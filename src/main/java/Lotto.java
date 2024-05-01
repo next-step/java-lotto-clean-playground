ffec0c5138f43de44eb045ec831c38ee6199bd78
jieun4510@sju.ac.kr <jieun4510@sju.ac.kr>
2024-05-01 21:13:42 +0900

    feat: 로또 검증 로직 추가

---
+++ src/main/java/Lotto.java
@@ -0,0 +1,32 @@
+import java.util.List;
+
+public class Lotto {
+
+    public static final int LOTTO_SIZE = 6;
+
+    private final List<LottoNumber> numbers;
+
+    public Lotto(List<LottoNumber> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<LottoNumber> numbers) {
+        validateSize(numbers);
+        validateDuplication(numbers);
+    }
+
+    private void validateSize(List<LottoNumber> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplication(List<LottoNumber> numbers) {
+        long duplicatedSize = numbers.stream().distinct().count();
+        if (numbers.size() != duplicatedSize) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+}
