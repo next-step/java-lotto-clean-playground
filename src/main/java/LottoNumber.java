2e22419c20c6ba0abc03909e97a3a578ff4201ff
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-05-02 10:10:06 +0900

    fix: 로또 번호 범위 수정

--- src/main/java/LottoNumber.java
+++ src/main/java/LottoNumber.java
@@ -1,6 +1,6 @@
 public class LottoNumber {
 
-    public static final int MIN_VALUE = 0;
+    public static final int MIN_VALUE = 1;
     public static final int MAX_VALUE = 45;
 
     private final int number;
