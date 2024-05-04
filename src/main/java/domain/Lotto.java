16d1146466408b19cf90c5a53617b14aded386f0
gogo1414 <wnstn6945@gmail.com>
2024-05-04 15:01:52 +0900

    [1단계] - 로또 자동 구매 : 랜덤 함수 제거 후 컬렉션의 셔플 적용

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,47 +1,55 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 public class Lotto {
     private final int LOTTO_PRICE = 1000;
+    private final int FIRST_LOTTO_POSITION = 0;
     private final int LAST_LOTTO_POSITION = 6;
     private final int MIN_LOTTO_NUMBER = 1;
     private final int MAX_LOTTO_NUMBER = 45;
 
     private int lottoCount;
+    private List<Integer> lottoNumberList;
     private List<List<Integer>> lottoNumberTickets;
 
     public Lotto(int money) {
         this.lottoCount = (int) money / LOTTO_PRICE;
-        lottoNumberTickets = new ArrayList<>();
+        makeLottoNumberList();
         makeLottoNumberTickets(lottoCount);
     }
 
-    public void makeLottoNumberTickets(int lottoCount) {
-        for(int i = 0;i < lottoCount; i++) {
-            lottoNumberTickets.add(generateLottoNumber());
+    public void makeLottoNumberList() {
+        lottoNumberList = new ArrayList<>();
+        for(int i = MIN_LOTTO_NUMBER; i <= MAX_LOTTO_NUMBER; i++) {
+            lottoNumberList.add(i);
         }
     }
 
-    public List<Integer> generateLottoNumber() {
-        Random random = new Random();
+    public void makeLottoNumberTickets(int lottoCount) {
+        lottoNumberTickets = new ArrayList<>();
 
-        List<Integer> lottoTicket = new ArrayList<>();
+        List<Integer> lottoTicket;
 
-        while(lottoTicket.size() != LAST_LOTTO_POSITION) {
-            lottoTicket.add(random.nextInt(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER));
+        for(int i = 0;i < lottoCount; i++) {
+            Collections.shuffle(lottoNumberList);
+
+            lottoTicket = lottoNumberList.subList(FIRST_LOTTO_POSITION, LAST_LOTTO_POSITION);
+            Collections.sort(lottoTicket);
+            lottoNumberTickets.add(lottoTicket.stream().toList());
         }
-
-        Collections.sort(lottoTicket);
-
-        return lottoTicket;
     }
 
     public int getLottoCount() {
         return this.lottoCount;
+    }
+
+    public List<Integer> getLottoNumberList() {
+        return this.lottoNumberList;
     }
 
     public List<List<Integer>> getLottoNumberTickets() {
--- src/test/java/view/OutputViewTest.java
+++ src/test/java/view/OutputViewTest.java
@@ -1,13 +1,8 @@
 package view;
 
 import domain.Lotto;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.List;
-import java.util.Random;
-import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,11 +12,11 @@ public class OutputViewTest {
     @DisplayName("로또 생성 확인 메서드")
     public void a() {
         // given
-        Random random = new Random();
         int money = 10000;
 
         //when
-        Lotto lotto = new Lotto(10000);
+        Lotto lotto = new Lotto(money);
+        List<Integer> num = lotto.getLottoNumberList();
 
         //then
         new OutputView().outputLottoTickets(lotto);
