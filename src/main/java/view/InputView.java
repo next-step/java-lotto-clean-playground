9fe9d100f409224a4cc50e04459027626a2d7e78
kokodak <kokodakadokok@gmail.com>
2024-05-05 16:25:46 +0900

    fix: 당첨 로또 입력 오류 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -16,7 +16,7 @@ public class InputView {
     public List<String> readWinningNumbers() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         String input = SCANNER.nextLine();
-        return Stream.of(input.split(","))
+        return Stream.of(input.replace(" ", "").split(","))
                 .filter(it -> !it.isBlank())
                 .toList();
     }
