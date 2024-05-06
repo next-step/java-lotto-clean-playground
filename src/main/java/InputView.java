709f187ee1f8b796e4e1d39e2eb4a60920217dad
jieun4510@sju.ac.kr <jieun4510@sju.ac.kr>
2024-05-06 23:31:27 +0900

    feat: 로또 번호 입력

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -7,5 +9,18 @@ public class InputView {
     public int inputLottoPrice() {
         System.out.println("구입금액을 입력해 주세요.");
         return SCANNER.nextInt();
+    }
+
+    public Lotto inputLottoNumbers() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        return stringToLotto(SCANNER.nextLine());
+    }
+
+    public Lotto stringToLotto(String string) {
+        List<Integer> numbers = Arrays.stream(string.split(",")).map(Integer::parseInt).toList();
+        List<LottoNumber> lottoNumbers = numbers.stream()
+                .map(LottoNumber::new)
+                .toList();
+        return new Lotto(lottoNumbers);
     }
 }
