cfe53f4309e15a91ff89d837a842329afbea3c08
nyeroni <nir2y@naver.com>
2024-05-06 23:49:51 +0900

    feat: 투입 금액 및 로또 구매 개수 예외 처리

---
+++ src/main/java/model/UserCount.java
@@ -0,0 +1,48 @@
+package model;
+
+public class UserCount {
+
+    private static final int LOTTO_UNIT = 1000;
+
+    private final int count;
+    private final int price;
+
+    public UserCount(String price){
+        int priceNum = convertStringToInt(price);
+        validate(priceNum);
+        this.price = priceNum;
+        this.count = calculateCount(priceNum);
+    }
+    public int calculateCount(int price){
+        return price/1000;
+    }
+    public int getCount(){
+        return count;
+    }
+    public int getPrice(){
+        return price;
+    }
+    public int convertStringToInt(String price){
+        try {
+            return Integer.parseInt(price);
+        } catch (NumberFormatException e) {
+            System.err.println("[ERROR] 숫자로만 입력해주세요.");
+            throw e;
+        }
+    }
+
+    public void validate(int priceNum){
+        validateRange(priceNum);
+        validateUnit(priceNum);
+    }
+    public void validateRange(int priceNum){
+        if(priceNum<=0){
+            throw new IllegalArgumentException();
+        }
+    }
+    public void validateUnit(int priceNum){
+        if(priceNum % LOTTO_UNIT != 0){
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위로 입력되어야 합니다.");
+        }
+    }
+}
