9c2832071b9d0b30dcb46aa854c1981e703593bd
nyeroni <nir2y@naver.com>
2024-05-06 16:35:02 +0900

    feat: 출력 문구 수정 및 로또 출력 방식 변경

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,33 +1,37 @@
 package view;
 
+import model.Lotto;
+import model.LottoList;
+import model.WinningType;
+
 import java.util.List;
 
 public class OutputView {
 
-    private final static String BUY_MESSAGE = "개를 구매했습니다.";
-    private final static String THREE_MATCHES = "3개 일치 (5000원)- ";
-    private final static String FOUR_MATCHES = "4개 일치 (50000원)- ";
-    private final static String FIVE_MATCHES = "5개 일치 (1500000원)- ";
-    private final static String SIX_MATCHES = "6개 일치 (2000000000원)- ";
+    private final static String BUY_MESSAGE = "수동으로 %d장, 자동으로 %d개를 구매했습니다.";
     private final static String COUNT_SUFFIX = "개";
 
-    public void printTicketCount (int count){
-        System.out.println("\n" + count + BUY_MESSAGE);
 
-    }
-    public void printLottoNumbers(List<Integer> numbers) {
-        System.out.println(numbers);
+    public void printTicketCount (int autoCount, int manualCount){
+        System.out.println();
+        String message = String.format(BUY_MESSAGE, manualCount, autoCount);
+        System.out.println(message);    }
+    public void printLottoNumbers(LottoList lottoList) {
+        List<Lotto> lottos = lottoList.getLottoList();
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
     }
 
     public void printResults(int [] arr, double rate){
         System.out.println();
         System.out.println("당첨 통계");
         System.out.println("---------");
-        System.out.println(THREE_MATCHES + arr[0] + COUNT_SUFFIX);
-        System.out.println(FOUR_MATCHES + arr[1] + COUNT_SUFFIX);
-        System.out.println(FIVE_MATCHES + arr[2] + COUNT_SUFFIX);
-        System.out.println(SIX_MATCHES + arr[3] + COUNT_SUFFIX);
-
-        System.out.println("총 수익률은 " + rate + " 입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
+        System.out.println(WinningType.FIFTH.getSameCountMessage() + arr[0] + COUNT_SUFFIX);
+        System.out.println(WinningType.FOURTH.getSameCountMessage() + arr[1] + COUNT_SUFFIX);
+        System.out.println(WinningType.THIRD.getSameCountMessage() + arr[2] + COUNT_SUFFIX);
+        System.out.println(WinningType.SECOND.getSameCountMessage() + arr[3] + COUNT_SUFFIX);
+        System.out.println(WinningType.FIRST.getSameCountMessage() + arr[4] + COUNT_SUFFIX);
+        System.out.printf("총 수익률은 %.2f 입니다. (기준이 1이기 때문에 결과적으로 손해라는 의미임)%n", rate);
     }
 }
