c8c32ac0bd49010f5d9e0ec8621adf9c60c11c74
Mint <audwl03071@gmail.com>
2024-05-06 21:02:56 +0900

    feat: Controller, Application 구현

--- src/main/java/org/duckstudy/controller/LottoController.java
+++ src/main/java/org/duckstudy/controller/LottoController.java
@@ -1,10 +1,10 @@
 package org.duckstudy.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
+import org.duckstudy.model.Lotto;
 import org.duckstudy.model.Price;
-import org.duckstudy.model.lotto.Lotto;
 import org.duckstudy.view.InputView;
 import org.duckstudy.view.OutputView;
 
@@ -20,22 +20,25 @@ public class LottoController {
 
     public void run() throws IOException {
         Price price = createPrice();
-        List<Lotto> lottos = createLottos(price.calculateLottoCount());
-        outputView.printLottos(lottos);
+        createLottos(price.calculateLottoCount());
     }
 
     private Price createPrice() throws IOException {
         try {
             return new Price(inputView.inputPrice());
-        } catch (IllegalArgumentException e) {
+        } catch (NumberFormatException e) {
             outputView.printException(e);
             return createPrice();
         }
     }
 
-    private List<Lotto> createLottos(int lottoCount) {
-        return Stream.generate(Lotto::createRandomLotto)
-                .limit(lottoCount)
-                .toList();
+    private void createLottos(int lottoCount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            lottos.add(new Lotto());
+        }
+
+        outputView.printLottoCount(lottoCount);
+        outputView.printLottos(lottos);
     }
 }
