0e387fb5c897921d0d85b5d7410402aa61cd8c98
nyeroni <nir2y@naver.com>
2024-05-06 23:49:21 +0900

    feat: enum타입 수정

--- src/main/java/model/WinningType.java
+++ src/main/java/model/WinningType.java
@@ -1,19 +1,27 @@
 package model;
 
 public enum WinningType {
-    FIRST("6개 일치 (2000000000원)"),
-    SECOND("5개 일치, 보너스 볼 일치 (30000000원)"),
-    THIRD("5개 일치 (1500000원)"),
-    FOURTH("4개 일치 (50000원)"),
-    FIFTH("3개 일치 (5000원)");
+    FIFTH(3, 5000, "3개 일치 (5000원)- "),
+    FOURTH(4, 50000, "4개 일치 (50000원)- "),
+    THIRD(5, 150000, "5개 일치 (1500000원)- "),
+    SECOND(5, 30000000, "5개 일치, 보너스 볼 일치(30000000원)- "),
+    FIRTH(6, 2000000000, "6개 일치 (2000000000원)- ");
 
-    private String sameCountMessage;
+    private static final int WINNING_MIN_COUNT = 3;
+    private static final String ERROR_MESSAGE = "[ERROR]";
 
-    WinningType(String sameCountMessage) {
-        this.sameCountMessage = sameCountMessage;
+    private int countOfCorrect;
+    private int reward;
+    private String message;
+
+    WinningType(int countOfCorrect, int reward, String message) {
+        this.countOfCorrect = countOfCorrect;
+        this.reward = reward;
+        this.message = message;
     }
 
-    public String getSameCountMessage() {
-        return sameCountMessage;
+    public String getMessage(){
+        return message;
     }
+
 }
