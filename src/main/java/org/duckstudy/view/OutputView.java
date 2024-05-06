d82470b9bd968b35159b8f9f9a542f37070dccb5
Mint <audwl03071@gmail.com>
2024-05-06 21:22:06 +0900

    feat: Lottos 생성 및 Lotto 출력 로직 캡슐화
    - List<Lotto>를 불변으로 만들어 보호하기 위해 Lottos 일급 컬렉션을 생성한다.
    - outputView에서 Lottos 정보를 출력하는 두 메서드를 하나로 합친다.

--- src/main/java/org/duckstudy/view/OutputView.java
+++ src/main/java/org/duckstudy/view/OutputView.java
@@ -9,15 +9,13 @@ public class OutputView {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    public void printLottoCount(int lottoCount) {
-        System.out.printf("\n%d개를 구매했습니다.\n", lottoCount);
-    }
-
     public void printException(Exception e) {
         System.out.println(e.getMessage());
     }
 
     public void printLottos(List<Lotto> lottos) {
+        System.out.printf("\n%d개를 구매했습니다.\n", lottos.size());
+
         for (Lotto lotto : lottos) {
             System.out.println(lotto.toList());
         }
