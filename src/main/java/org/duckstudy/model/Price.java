de869b84b0038d183d6f46a0de50a1c751290ba3
Mint <audwl03071@gmail.com>
2024-05-06 20:57:20 +0900

    feat: Price VO 구현
    - 구매 가격에 대한 값 객체(VO)를 생성하여 가격 관련 로직들을 캡슐화한다.

---
+++ src/main/java/org/duckstudy/model/Price.java
@@ -0,0 +1,21 @@
+package org.duckstudy.model;
+
+public record Price(int value) {
+
+    public static final int INCLUSIVE_MIN_PRICE = 0;
+    public static final int PER_PRICE = 1000;
+
+    public Price {
+        validatePrice(value);
+    }
+
+    private void validatePrice(int price) {
+        if (price < INCLUSIVE_MIN_PRICE) {
+            throw new IllegalArgumentException(String.format("가격은 %d보다 작을 수 없습니다.", INCLUSIVE_MIN_PRICE));
+        }
+    }
+
+    public int calculateLottoCount() {
+        return value / PER_PRICE;
+    }
+}
