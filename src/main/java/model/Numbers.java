8bebe60d9bc3a8aa651eb44e729745b9066f5da2
whxogus215 <whxogus215@naver.com>
2024-05-06 21:10:41 +0900

    refactor: 방어적 복사 적용

--- src/main/java/model/Numbers.java
+++ src/main/java/model/Numbers.java
@@ -14,7 +14,7 @@ public class Numbers {
 
     public Numbers(final List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = new ArrayList<>(numbers);
     }
 
     private void validate(final List<Integer> numbers) {
