476d6e6b4ff89adb367c89af12c194640e746d57
승엽 <tmdduqflfl@naver.com>
2024-05-14 01:09:35 +0900

    feat: - Lotto 난수 추출 메서드

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,14 +1,22 @@
 package domain;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Lotto {
-    private static final int moneyNumber = 1000;
+    private static final int LOTTO_NUM = 1000;
+    private static final int LOTTO_RANGE = 45;
 
-
-    private int LottoNum(int money) {
-        return money / moneyNumber;
+    private int lottoNum(int money) {
+        return money / LOTTO_NUM;
     }
 
     public int getLottoNum(int money) {
-        return LottoNum(money);
+        return lottoNum(money);
     }
+
+    private int lottoRandomNum() {
+        return (int) (Math.random() * LOTTO_RANGE);
+    }
+
 }
