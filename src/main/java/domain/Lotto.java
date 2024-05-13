064002e401c50fe345cc9fe9532f9c256cc7fbad
승엽 <tmdduqflfl@naver.com>
2024-05-14 01:37:11 +0900

    feat: - 로또 구매 개수 메서드 구현

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,11 +1,8 @@
 package domain;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class Lotto {
+
     private static final int LOTTO_NUM = 1000;
-    private static final int LOTTO_RANGE = 45;
 
     private int lottoNum(int money) {
         return money / LOTTO_NUM;
@@ -14,9 +11,4 @@ public class Lotto {
     public int getLottoNum(int money) {
         return lottoNum(money);
     }
-
-    private int lottoRandomNum() {
-        return (int) (Math.random() * LOTTO_RANGE);
-    }
-
 }
