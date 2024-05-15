7edb51548da218807e8f21befb2efbe6a30758fb
승엽 <tmdduqflfl@naver.com>
2024-05-15 21:39:19 +0900

    Fix : - OutputView 에서 사용하던 반복문 Controller로 재구현했습니다.

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,22 +1,27 @@
 package controller;
 
-import domain.Lotto;
-import domain.LottoRandom;
+import domain.CountLotto;
+import domain.GenerateRandomNumber;
 import view.InputView;
 import view.OutputView;
 
 import java.util.List;
 
 public class LottoController {
+    private static final int INITIAL_NUM = 0;
+
     public static void main(String[] args) {
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
-        LottoRandom lottoRandom = new LottoRandom();
-        Lotto lotto = new Lotto();
+        CountLotto countLotto = new CountLotto();
+        GenerateRandomNumber generateRandomNumber = new GenerateRandomNumber();
 
-        List<Integer> lottoPaper = lottoRandom.getLottoRandomNum();
         int money = inputView.getMoney();
-        int num = lotto.getLottoNum(money);
-        outputView.repeatLotto(num, lottoPaper);
+        int paper = countLotto.getCountLotto(money);
+
+        for (int i = INITIAL_NUM; i < paper; i++) {
+            List<Integer> lottoList = generateRandomNumber.generateNumber();
+            outputView.lotto(lottoList);
+        }
     }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,16 +1,10 @@
 package view;
 
-
 import java.util.List;
 
 public class OutputView {
 
-    private int i;
-    private final int INITIAL_NUM = 0;
-
-    public void repeatLotto(int num, List<Integer> lottoNumbers) {
-        for (i = INITIAL_NUM; i <= num; i++) {
-            System.out.println(lottoNumbers);
-        }
+    public void lotto(List<Integer> lottoNumbers) {
+        System.out.println(lottoNumbers);
     }
 }
