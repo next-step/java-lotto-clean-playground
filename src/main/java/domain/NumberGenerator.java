ed0aafe3b8aad12e3758471f2526ec76c85e6ab0
DaYeong <day0_ng@naver.com>
2024-05-16 01:00:23 +0900

    fix: indentdepth 수정

--- src/main/java/domain/NumberGenerator.java
+++ src/main/java/domain/NumberGenerator.java
@@ -9,7 +9,7 @@ public class NumberGenerator {
     public List<Integer> generateRandomNumbers() {
         Set<Integer> numbers = new HashSet<>();
         while (numbers.size() < 6) {
-            int num = (int) (Math.random() * 45) + 1; // 1부터 45 사이의 난수 생성
+            int num = (int) (Math.random() * 45) + 1; 
             numbers.add(num);
         }
         return new ArrayList<>(numbers);
