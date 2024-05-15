ff78a0c29034a408aaa633c5a0824c400f8d8283
승엽 <tmdduqflfl@naver.com>
2024-05-15 21:44:39 +0900

    Test : - 로또 난수 범위가 1~45 까지 인지 확인 했습니다.
    - 숫자를 고정 후 알맞게 잘 진행 되는지 테스트

---
+++ src/test/java/domain/GenerateRandomNumberTest.java
@@ -0,0 +1,33 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GenerateRandomNumberTest {
+    @Test
+    void 로또_난수_범위_테스트() {
+        // given
+        GenerateRandomNumber lottoRandom = new GenerateRandomNumber();
+        List<Integer> lottoNum = lottoRandom.generateNumber();
+        // when&then
+        for (int num : lottoNum) {
+            assertTrue(1 <= num && num <= 45, "범위오버");
+        }
+    }
+
+    @Test
+    void 무작위_숫자_테스트() {
+        // given
+        FakeGenerateRandomNumber fakeGenerateRandomNumber = new FakeGenerateRandomNumber();
+        // when
+        List<Integer> lottoNum = fakeGenerateRandomNumber.generateNumber();
+        // then
+        assertEquals(6, lottoNum.size());
+        assertEquals(List.of(11, 23, 32, 44, 5, 16), lottoNum);
+    }
+
+}
