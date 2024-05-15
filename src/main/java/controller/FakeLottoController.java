27755cf35622713759b8d5d83e01f9973efd279e
승엽 <tmdduqflfl@naver.com>
2024-05-15 21:39:55 +0900

    Fix : - 랜덤 숫자를 테스트 하기위한 가상 로또 컨트롤러 입니다.

---
+++ src/main/java/controller/FakeLottoController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import domain.CountLotto;
+import domain.FakeGenerateRandomNumber;
+import domain.GenerateRandomNumber;
+import view.InputView;
+import view.OutputView;
+
+import java.util.List;
+
+public class FakeLottoController {
+
+    private static final int INITIAL_NUM = 0;
+
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        CountLotto countLotto = new CountLotto();
+        FakeGenerateRandomNumber generateRandomNumber = new FakeGenerateRandomNumber();
+
+        int money = inputView.getMoney();
+        int paper = countLotto.getCountLotto(money);
+
+        for (int i = INITIAL_NUM; i < paper; i++) {
+            List<Integer> lottoList = generateRandomNumber.generateNumber();
+            outputView.lotto(lottoList);
+        }
+    }
+}
