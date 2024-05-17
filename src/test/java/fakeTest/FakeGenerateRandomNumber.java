7831a7af002b22ad579655d16484105c25f1c9ca
승엽 <tmdduqflfl@naver.com>
2024-05-17 17:18:10 +0900

    Refactor: - 로또 테스트를 위한 가상 로또 값생성

--- src/test/java/fakeTest/FakeGenerateRandomNumber.java
+++ src/test/java/fakeTest/FakeGenerateRandomNumber.java
@@ -1,12 +1,13 @@
 package fakeTest;
 
 import domain.RandomNumber;
+
 import java.util.List;
 
 public class FakeGenerateRandomNumber implements RandomNumber {
 
     @Override
-    public List<Integer> generateNumber() {
+    public List<Integer> generateRandomNumber() {
         return List.of(11, 23, 32, 44, 5, 16);
     }
 }
