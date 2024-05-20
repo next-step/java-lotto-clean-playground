8039cfab59941b0c0422cd1d2e70f2d739099ac7
승엽 <tmdduqflfl@naver.com>
2024-05-20 19:13:29 +0900

    Refactor: - 실행 할 수 있게 구현

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,26 +1,40 @@
 package controller;
 
 import domain.LottoTicket;
+import domain.LottoTicketStore;
+import domain.NumberGenerator;
+import domain.RandomNumberGenerator;
 import view.InputView;
 import view.OutputView;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoController {
 
     private final InputView inputView;
     private final OutputView outputView;
+    private final NumberGenerator numberGenerator;
 
     public LottoController() {
+        this.numberGenerator = new RandomNumberGenerator();
         this.inputView = new InputView();
         this.outputView = new OutputView();
     }
 
     public void run() {
         int money = inputView.getMoney();
-        LottoTicket lottoTicket = new LottoTicket(money);
-        outputView.displayLottoTickets(lottoTicket.getLottoTicket());
+        purchasedLotto(money);
     }
 
     public static void main(String[] args) {
         new LottoController().run();
+    }
+
+    private void purchasedLotto(int money) {
+        LottoTicket lottoTicket = new LottoTicket(money, numberGenerator);
+        List<List<Integer>> tickets = lottoTicket.getLottoTicket().stream()
+                .map(LottoTicketStore::getLottoNumber)
+                .collect(Collectors.toList());
+        outputView.displayLottoTickets(tickets);
     }
 }
