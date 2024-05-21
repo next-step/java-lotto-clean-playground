71d84dd4ad1e922056172e6a1dc7778e624fedee
승엽 <tmdduqflfl@naver.com>
2024-05-21 15:42:35 +0900

    Refactor: - 로또 컨트롤러 합체

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,40 +1,42 @@
 package controller;
 
-import domain.LottoTicket;
-import domain.LottoTicketStore;
-import domain.NumberGenerator;
-import domain.RandomNumberGenerator;
+import domain.*;
 import view.InputView;
 import view.OutputView;
+
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class LottoController {
 
     private final InputView inputView;
     private final OutputView outputView;
-    private final NumberGenerator numberGenerator;
 
     public LottoController() {
-        this.numberGenerator = new RandomNumberGenerator();
         this.inputView = new InputView();
         this.outputView = new OutputView();
     }
 
     public void run() {
         int money = inputView.getMoney();
-        purchasedLotto(money);
+        TicketCount ticketCount = new TicketCount(money);
+        LottoTickets lottoTickets = new LottoTickets(new RandomNumberGenerator());
+        List<LottoTicket> tickets = lottoTickets.generateLottoTickets(ticketCount.getCount());
+        outputView.displayLottoTickets(tickets);
+        List<Integer> winNumbers = inputView.getWinNumbers();
+        WinningLotto winningLotto = new WinningLotto(winNumbers);
+        winning(tickets, winningLotto);
+        outputView.winningLottoRateOfResult(money);
     }
+
 
     public static void main(String[] args) {
         new LottoController().run();
     }
 
-    private void purchasedLotto(int money) {
-        LottoTicket lottoTicket = new LottoTicket(money, numberGenerator);
-        List<List<Integer>> tickets = lottoTicket.getLottoTicket().stream()
-                .map(LottoTicketStore::getLottoNumber)
-                .collect(Collectors.toList());
-        outputView.displayLottoTickets(tickets);
+    private void winning(List<LottoTicket> tickets, WinningLotto winningLotto) {
+        for (LottoTicket ticket : tickets) {
+            int matchedNumbers = winningLotto.getCount(ticket.getNumbers());
+            outputView.numberOfWinning(matchedNumbers);
+        }
     }
 }
