a16390d1ab0a4ed079868c9f310b560c2031c59a
승엽 <tmdduqflfl@naver.com>
2024-05-21 15:40:45 +0900

    Refactor: -  outputView에서 도메인을 의존하는 방식을 끊기 위한 Dto 생성 시도중

---
+++ src/main/java/domain/LottoTicketDto.java
@@ -0,0 +1,17 @@
+
+package domain;
+
+import java.util.List;
+
+public class LottoTicketDto {
+
+    private final List<Integer> numbers;
+
+    public LottoTicketDto(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}
