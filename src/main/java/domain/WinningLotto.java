4ccd643a9880673dbdc24263defd62c6bfa635a5
승엽 <tmdduqflfl@naver.com>
2024-05-21 15:39:23 +0900

    Refactor: - 네이밍 변경 - 일급컬렉션 특성상 멤버변수는 하나여야하는데 두개를 유지하고 있어 분리

--- src/main/java/domain/WinningLotto.java
+++ src/main/java/domain/WinningLotto.java
@@ -4,20 +4,21 @@ import java.util.List;
 
 public class WinningLotto {
 
-    private List<Integer> winNumbers;
+    private static final int INITIAL_NUMBER = 0;
 
-    public int count = 0;
+    private final List<Integer> winNumbers;
 
     public WinningLotto(List<Integer> winNumbers) {
         this.winNumbers = winNumbers;
-        checkSameLottoNumber();
     }
 
-    private void checkSameLottoNumber() {
-        for (Integer number : winNumbers) {
+    public int getCount(List<Integer> ticketNumbers) {
+        int count = INITIAL_NUMBER;
+        for (int number : ticketNumbers) {
             if (winNumbers.contains(number)) {
                 count++;
             }
         }
+        return count;
     }
 }
