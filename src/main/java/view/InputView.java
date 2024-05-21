fe2fbcd717a13dcb4cbb1a5410e10a023afe27a6
승엽 <tmdduqflfl@naver.com>
2024-05-21 15:41:51 +0900

    Refactor: - 입력값을 받기 위한 메서드 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,6 +7,8 @@ import java.util.stream.Collectors;
 
 public class InputView {
 
+    private static final String DISUNITE_COMMA = ",";
+
     private final Scanner scanner = new Scanner(System.in);
 
     public int getMoney() {
@@ -15,9 +17,9 @@ public class InputView {
     }
 
     public List<Integer> getWinNumbers() {
-        System.out.println("지난 주 당첨 번호를 입력하시오:");
+        System.out.println("지난 주 당첨 번호를 입력하시오 (쉼표로 구분):");
         String winNumbers = scanner.next();
-        return Arrays.stream(winNumbers.split(","))
+        return Arrays.stream(winNumbers.split(DISUNITE_COMMA))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
     }
