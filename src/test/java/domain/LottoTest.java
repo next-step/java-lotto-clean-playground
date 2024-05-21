a781c78112b02d56d565e833c3bab542df94ec0c
승엽 <tmdduqflfl@naver.com>
2024-05-21 15:42:26 +0900

    Refactor: - 로또 테스트 네이밍 바뀌는 문제등 해결

---
+++ src/test/java/domain/LottoTest.java
@@ -0,0 +1,47 @@
+package domain;
+
+import fakeTest.FakeGenerateNumberGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class LottoTest {
+
+    @Test
+    void 로또_가상값_테스트() {
+
+        // given
+        FakeGenerateNumberGenerator fakeGenerateRandomNumber = new FakeGenerateNumberGenerator();
+        LottoTickets lottos = new LottoTickets(fakeGenerateRandomNumber);
+
+        // when
+        List<LottoTicket> tickets = lottos.generateLottoTickets(1);
+        List<Integer> generatedNumbers = tickets.get(0).getNumbers();
+
+        // then
+        List<Integer> expectedValue = fakeGenerateRandomNumber.generateRandomNumber();
+        assertEquals(expectedValue, generatedNumbers);
+        assertEquals(6, generatedNumbers.size());
+        assertEquals(List.of(5, 16, 11, 23, 32, 44), generatedNumbers);
+    }
+
+    @Test
+    void 로또_범위_테스트() {
+
+        // given
+        NumberGenerator numberGenerator = new RandomNumberGenerator();
+        LottoTickets lottos = new LottoTickets(numberGenerator);
+
+        // when
+        List<LottoTicket> tickets = lottos.generateLottoTickets(1);
+        List<Integer> generatedNumbers = tickets.get(0).getNumbers();
+
+        // then
+        for (int num : generatedNumbers) {
+            assertTrue(1 <= num && num <= 45, "로또범위");
+        }
+    }
+}
