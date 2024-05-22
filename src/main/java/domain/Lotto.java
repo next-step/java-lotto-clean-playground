bf3c29fd382ba3a99d6320bd6279d7f3b9b40b4e
Hheojiwon <0618amy@naver.com>
2024-05-22 16:45:08 +0900

    refactor: 일급 컬렉션에 맞게 lotto class 수정

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,36 +1,17 @@
 package domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
 
-    private static final int LOTTO_PRICE = 1000;
-    private static final int LIMIT_NUMBER = 45;
-    private final int price;
+    private List<Integer> lottoNumbers;
 
-    public Lotto(int price) {
-        this.price = price;
+    public Lotto(List<Integer> lottoNumbers) {
+        this.lottoNumbers=lottoNumbers;
     }
 
-    private int getLottoTickets() {
-        return price / LOTTO_PRICE;
+    public List<Integer> getLottoNumbers(){
+        return lottoNumbers;
     }
 
-    private List<Integer> getLottoNumbers() {
-        List<Integer> lotto = new ArrayList<>();
-        for (int i = 0; i < LIMIT_NUMBER; i++) {
-            lotto.add(i + 1);
-        }
-        Collections.shuffle(lotto);
-        return lotto.subList(0, 6);
-    }
-
-    public void repeatLotto() {
-        int lottoTickets = getLottoTickets();
-        for (int i = 0; i < lottoTickets; i++) {
-            getLottoNumbers();
-        }
-    }
 }
