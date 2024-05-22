d94f56ac69d402503f455ac87b7bd02dfaa2555d
승엽 <tmdduqflfl@naver.com>
2024-05-22 15:37:20 +0900

    Refactor: - 컨트롤러 다시 작성

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -16,27 +16,34 @@ public class LottoController {
         this.outputView = new OutputView();
     }
 
+    public static void main(String[] args) {
+        new LottoController().run();
+    }
+
     public void run() {
         int money = inputView.getMoney();
         TicketCount ticketCount = new TicketCount(money);
         LottoTickets lottoTickets = new LottoTickets(new RandomNumberGenerator());
         List<LottoTicket> tickets = lottoTickets.generateLottoTickets(ticketCount.getCount());
         outputView.displayLottoTickets(tickets);
+        winningLotto(tickets, money);
+    }
+
+    private void winningLotto(List<LottoTicket> tickets, int money) {
         List<Integer> winNumbers = inputView.getWinNumbers();
         WinningLotto winningLotto = new WinningLotto(winNumbers);
         winning(tickets, winningLotto);
-        outputView.winningLottoRateOfResult(money);
-    }
-
-
-    public static void main(String[] args) {
-        new LottoController().run();
+        LottoRate lottoRate = new LottoRate();
+        double rateOfReturn = lottoRate.LottoRate(money);
+        outputView.winningLottoRateOfResult(rateOfReturn);
     }
 
     private void winning(List<LottoTicket> tickets, WinningLotto winningLotto) {
         for (LottoTicket ticket : tickets) {
             int matchedNumbers = winningLotto.getCount(ticket.getNumbers());
-            outputView.numberOfWinning(matchedNumbers);
+            WinningsMoney winningsMoney = new WinningsMoney();
+            List<Integer> matchesMoney = winningsMoney.matchesMoney();
+            outputView.numberOfWinning(matchedNumbers, matchesMoney);
         }
     }
 }
