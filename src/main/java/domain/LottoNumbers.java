f90da77f33438ba0b615430de03f49954470e580
Hheojiwon <0618amy@naver.com>
2024-05-25 04:45:10 +0900

    docs: 변수 이름 변경

--- src/main/java/domain/LottoNumbers.java
+++ src/main/java/domain/LottoNumbers.java
@@ -6,16 +6,16 @@ import java.util.List;
 
 public class LottoNumbers {
 
-    private static final int MAX_NUMBER = 45;
-    private static final int NUMBERS_PER_TICKET = 6;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int LOTTO_SIZE = 6;
 
     public List<Integer> generateLottoNumber() {
         List<Integer> numbers = new ArrayList<>();
-        for (int number = 0; number < MAX_NUMBER; number++) {
+        for (int number = 0; number < MAX_LOTTO_NUMBER; number++) {
             numbers.add(number + 1);
         }
 
         Collections.shuffle(numbers);
-        return numbers.subList(0, NUMBERS_PER_TICKET);
+        return numbers.subList(0, LOTTO_SIZE);
     }
 }
