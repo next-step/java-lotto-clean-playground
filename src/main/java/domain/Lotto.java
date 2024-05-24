4255b08df6dd57ab5377824410e8dac1beff6588
Hheojiwon <0618amy@naver.com>
2024-05-25 04:39:47 +0900

    docs: 개행 수정

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -7,11 +7,10 @@ public class Lotto {
     private List<Integer> lottoNumbers;
 
     public Lotto(List<Integer> lottoNumbers) {
-        this.lottoNumbers=lottoNumbers;
+        this.lottoNumbers = lottoNumbers;
     }
 
-    public List<Integer> getLottoNumbers(){
+    public List<Integer> getLottoNumbers() {
         return lottoNumbers;
     }
-
 }
