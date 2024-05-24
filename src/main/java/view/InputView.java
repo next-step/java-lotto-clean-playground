b75ef127745179aae1f7419f1f37e7a7b1908ca0
승엽 <tmdduqflfl@naver.com>
2024-05-25 02:46:10 +0900

    Feat: - 보너스볼 입력 메서드 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 public class InputView {
 
     private final Scanner scanner = new Scanner(System.in);
+    public static final String COMMA = ",";
 
     public int getMoney() {
         System.out.println("금액을 입력하세요:");
@@ -15,9 +16,9 @@ public class InputView {
     }
 
     public List<Integer> getWinNumbers() {
-        System.out.println("지난 주 당첨 번호를 입력하시오 (쉼표로 구분):");
+        System.out.println("지난 주 당첨 번호를 입력하시오 (쉼표로 구분)");
         String winNumbers = scanner.next();
-        return Arrays.stream(winNumbers.split(","))
+        return Arrays.stream(winNumbers.split(COMMA))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
     }
