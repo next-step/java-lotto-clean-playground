be4dc8552f4d17ca8b8e7922fa9bd69d1bd5c470
승엽 <tmdduqflfl@naver.com>
2024-05-25 02:47:32 +0900

    Refactor: - 전체적인 코드 수정이 있었다 . - Enum을 사용함에 따라 새로운 메서드로 구현을했다 .

--- src/main/java/domain/LottoRate.java
+++ src/main/java/domain/LottoRate.java
@@ -4,20 +4,31 @@ import java.util.List;
 
 public class LottoRate {
 
-    private static final int INITIAL_NUMBER_ZERO = 0;
-    private static final int THREE_MATCHES = 5000;
-    private static final int FOUR_MATCHES = 50000;
-    private static final int FIVE_MATCHES = 150000;
-    private static final int SIX_MATCHES = 20000000;
-    private int sum = 0;
+    private final List<LottoTicket> tickets;
+    private final WinningLotto winningLotto;
 
-    public List<Integer> matchesMoney() {
-        return List.of(THREE_MATCHES, FOUR_MATCHES, FIVE_MATCHES, SIX_MATCHES);
+    public LottoRate(List<LottoTicket> tickets, WinningLotto winningLotto) {
+        this.tickets = tickets;
+        this.winningLotto = winningLotto;
     }
 
-    public double LottoRate(int payMoney) {
-        for (int i = INITIAL_NUMBER_ZERO; i < matchesMoney().size(); i++)
-            sum += matchesMoney().get(i);
-        return (double) sum / payMoney;
+    public List<Integer> matchesMoney() {
+        return List.of(
+                Ranking.FIRST.getPrice(),
+                Ranking.SECOND.getPrice(),
+                Ranking.THIRD.getPrice(),
+                Ranking.FOURTH.getPrice(),
+                Ranking.FIFTH.getPrice()
+        );
+    }
+
+    public double calculateRateOfReturn(int payMoney, int lottoBonusNumber) {
+        int totalWinnings = tickets.stream()
+                .mapToInt(ticket -> {
+                    int matchedNumbers = winningLotto.getCount(ticket.getNumbers());
+                    int matchedBonusNumbers = winningLotto.getBonusCount(ticket.getNumbers(), lottoBonusNumber);
+                    return Ranking.valueOfCount(matchedNumbers, matchedBonusNumbers).getPrice();
+                }).sum();
+        return (double) totalWinnings / payMoney;
     }
 }
