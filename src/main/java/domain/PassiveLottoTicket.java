e7b4f653ef0ea907d84a005fd12da4b0668b757e
승엽 <tmdduqflfl@naver.com>
2024-05-26 18:03:35 +0900

    Refator: - 처음 접근했던 수동로또 일급컬렉션 삭제

--- src/main/java/domain/PassiveLottoTicket.java
+++ src/main/java/domain/PassiveLottoTicket.java
@@ -1,15 +1,11 @@
-package domain;
-
-import java.util.List;
-
-public class PassiveLottoTicket {
-    private final List<Integer> numbers;
-
-    public PassiveLottoTicket(List<Integer> numbers) {
-        this.numbers = numbers;
-    }
-
-    public List<Integer> getNumbers() {
-        return numbers;
-    }
-}
+//package domain;
+//
+//import java.util.List;
+//
+//public class PassiveLottoTicket {
+//    private final List<Integer> numbers;
+//
+//    public PassiveLottoTicket(List<Integer> numbers) {
+//        this.numbers = numbers;
+//    }
+//}
