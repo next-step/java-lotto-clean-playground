f29e19892727a8e04c175e5b33c999f0e575ab0e
승엽 <tmdduqflfl@naver.com>
2024-05-26 18:03:16 +0900

    Refator: - 수동로또 메서드 구현

--- src/main/java/domain/LottoTickets.java
+++ src/main/java/domain/LottoTickets.java
@@ -7,13 +7,20 @@ public class LottoTickets {
 
     private static final int INITIAL_NUMBER = 0;
     private final NumberGenerator numberGenerator;
+    private final List<LottoTicket> tickets;
 
     public LottoTickets(NumberGenerator numberGenerator) {
         this.numberGenerator = numberGenerator;
+        this.tickets = new ArrayList<>();
     }
 
-    public List<LottoTicket> generateLottoTicket(int ticketCount) {
-        List<LottoTicket> tickets = new ArrayList<>();
+    public void addPassiveTickets(List<List<Integer>> passiveNumbers) {
+        for (List<Integer> numbers : passiveNumbers) {
+            tickets.add(new LottoTicket(numbers));
+        }
+    }
+
+    public List<LottoTicket> generateLottoTickets(int ticketCount) {
         generateLottoTickets(ticketCount, tickets);
         return tickets;
     }
