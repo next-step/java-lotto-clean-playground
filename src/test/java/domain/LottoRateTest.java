ba7f8993f81b2f111a43f85b9353f5fdb09650c8
승엽 <tmdduqflfl@naver.com>
2024-05-27 16:54:51 +0900

    Fix: - 로또 테스트

---
+++ src/test/java/domain/LottoRateTest.java
@@ -0,0 +1,17 @@
+package domain;
+
+
+import org.junit.jupiter.api.Test;
+
+class LottoRateTest {
+
+    @Test
+    void 로또당첨계산() {
+
+        // given
+
+        // when
+
+        // then
+    }
+}
