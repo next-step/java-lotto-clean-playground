c39317f411bad4e453eaf9286ed7798c547aade9
승엽 <tmdduqflfl@naver.com>
2024-05-27 17:33:31 +0900

    Feat: - 당첨금 테스트 - 수익률 테스트 구현

--- src/test/java/domain/LottoRateTest.java
+++ src/test/java/domain/LottoRateTest.java
@@ -1,17 +1,41 @@
 package domain;
 
-
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottoRateTest {
 
     @Test
-    void 로또당첨계산() {
+    void 로또상금_테스트() {
 
         // given
+        LottoRate lottoRate = new LottoRate(null, null);
 
         // when
+        List<Integer> matchesMoney = lottoRate.matchesMoney();
 
         // then
+        assertEquals(List.of(2000000000, 30000000, 1500000, 50000, 5000), matchesMoney);
+    }
+
+    @Test
+    void 수익률_테스트() {
+
+        // given
+        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6));
+        LottoTicket ticket1 = new LottoTicket(List.of(1, 2, 3, 4, 5, 6));
+        LottoTicket ticket2 = new LottoTicket(List.of(11, 12, 13, 14, 15, 16));
+        List<LottoTicket> tickets = List.of(ticket1, ticket2);
+
+        LottoRate lottoRate = new LottoRate(tickets, winningLotto);
+
+        // when
+        double rateOfReturn = lottoRate.calculateRateOfReturn(5000, 7);
+
+        // then
+        assertEquals(400000.0, rateOfReturn); // No winnings, so rate of return should be 0
     }
 }
