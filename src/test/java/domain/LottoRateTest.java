9321a4f5cb4824a1d4fbd2adb1ede88f6a647430
승엽 <tmdduqflfl@naver.com>
2024-05-29 22:12:37 +0900

    Feat: -  Null이 나오지않게 수정했습니다. Collections.singletonList를 인텔리제이에선 지향한다고 하길래 찾아서 적용해봤습니다. [URL](https://www.korecmblog.com/blog/arrays-asList-alternative-Collections-singletonList) 근데 아직 이해가 되진않습니다. 이것이 빅오n관련 내용일까요>

--- src/test/java/domain/LottoRateTest.java
+++ src/test/java/domain/LottoRateTest.java
@@ -2,24 +2,31 @@ package domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottoRateTest {
-
     @Test
     void 로또상금_테스트() {
 
         // given
-        LottoRate lottoRate = new LottoRate(null, null);
+        List<Integer> ticketNumbers = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> winNumbers = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> winningMoney = List.of(2000000000, 30000000, 1500000, 50000, 5000);
+        LottoTicket lottoTicket = new LottoTicket(ticketNumbers);
+        WinningLotto winningLotto = new WinningLotto(winNumbers);
+        List<LottoTicket> tickets = Collections.singletonList(lottoTicket);
 
         // when
+        LottoRate lottoRate = new LottoRate(tickets, winningLotto);
         List<Integer> matchesMoney = lottoRate.matchesMoney();
 
         // then
-        assertEquals(List.of(2000000000, 30000000, 1500000, 50000, 5000), matchesMoney);
+        assertEquals(winningMoney, matchesMoney);
     }
+
 
     @Test
     void 수익률_테스트() {
@@ -28,14 +35,17 @@ class LottoRateTest {
         WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6));
         LottoTicket ticket1 = new LottoTicket(List.of(1, 2, 3, 4, 5, 6));
         LottoTicket ticket2 = new LottoTicket(List.of(11, 12, 13, 14, 15, 16));
-        List<LottoTicket> tickets = List.of(ticket1, ticket2);
+        int payMoney = 5000;
+        int bonusNumber = 7;
 
+        List<LottoTicket> tickets = List.of(ticket1, ticket2);
         LottoRate lottoRate = new LottoRate(tickets, winningLotto);
 
+
         // when
-        double rateOfReturn = lottoRate.calculateRateOfReturn(5000, 7);
+        double rateOfReturn = lottoRate.calculateRateOfReturn(payMoney, bonusNumber);
 
         // then
-        assertEquals(400000.0, rateOfReturn); // No winnings, so rate of return should be 0
+        assertEquals(400000.0, rateOfReturn);
     }
 }
