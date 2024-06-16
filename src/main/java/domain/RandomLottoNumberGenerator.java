8d4a90426bb86ac54e17446a2c383337bc11c8e6
JoungMinJu <alswn9938@gmail.com>
2024-06-17 00:18:48 +0900

    add : 로또 숫자 생성기

---
+++ src/main/java/domain/RandomLottoNumberGenerator.java
@@ -0,0 +1,14 @@
+package domain;
+
+import java.util.Random;
+
+public class RandomLottoNumberGenerator implements NumberGenerator{
+
+    private final static int LOTTO_MAX_NUMBER = 45;
+
+    @Override
+    public int generate() {
+        Random random = new Random();
+        return random.nextInt(LOTTO_MAX_NUMBER);
+    }
+}
