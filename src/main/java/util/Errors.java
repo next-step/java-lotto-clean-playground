776137fd36b0820f6a6b562648d577e04ce178f5
JoungMinJu <alswn9938@gmail.com>
2024-06-17 00:19:23 +0900

    add : 출력하는 outputview

---
+++ src/main/java/util/Errors.java
@@ -0,0 +1,7 @@
+package util;
+
+public class Errors {
+
+    public static final String INPUT_IS_NOT_INTEGER = "시도 횟수로는 정수가 입력되어야 합니다.";
+
+}
---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,32 @@
+package view;
+
+import domain.Lottos;
+import java.util.List;
+import java.util.StringJoiner;
+
+public class OutputView {
+
+    private static final String DELIMITER_OF_LOTTOS_STATUS = "\n";
+
+    public void printInputPriceGuide() {
+        System.out.println("구입 금액을 입력해 주세요.");
+    }
+
+    public void printNumberOfLotto(int numberOfLotto) {
+        printEmptyLine();
+        System.out.printf("%d개를 구매했습니다.%n", numberOfLotto);
+    }
+
+    public void printStatusOfLottos(List<String> statusOfLottos) {
+        StringJoiner joiner = new StringJoiner(DELIMITER_OF_LOTTOS_STATUS);
+        for (String statusOfLotto : statusOfLottos) {
+            joiner.add(statusOfLotto);
+        }
+        System.out.println(joiner);
+    }
+
+    private void printEmptyLine() {
+        System.out.println();
+    }
+
+}
