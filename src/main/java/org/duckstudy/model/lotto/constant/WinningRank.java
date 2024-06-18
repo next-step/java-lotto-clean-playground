685ce55bc94f412eeb6d7ae2ddb75a08b100721e
Mint <audwl03071@gmail.com>
2024-06-18 16:52:00 +0900

    fix: 우승자 예외 처리 버그 수정
    - 매칭되지 않은 경우를 이미 filter에서 확인하고 있으므로 그 외의 경우 예외를 발생시키도록 수정한다(orElseThrow()).

--- src/main/java/org/duckstudy/model/lotto/constant/WinningRank.java
+++ src/main/java/org/duckstudy/model/lotto/constant/WinningRank.java
@@ -5,11 +5,11 @@ import java.util.stream.Stream;
 public enum WinningRank {
 
     NONE(0, 0, 0),
-    FIFTH(3, 5000, 3),
-    FOURTH(4, 50000, 4),
-    THIRD(5, 1500000, 5),
-    SECOND(5, 30000000, -5),
-    FIRST(6, 2000000000, 6);
+    FIFTH(3, 5_000, 3),
+    FOURTH(4, 50_000, 4),
+    THIRD(5, 1_500_000, 5),
+    SECOND(5, 30_000_000, -5),
+    FIRST(6, 2_000_000_000, 6);
 
     private final int matchCount;
     private final int price;
@@ -22,13 +22,20 @@ public enum WinningRank {
     }
 
     public static WinningRank findByMatchCountAndBonus(int matchCount, boolean matchBonus) {
-        if (matchCount == SECOND.getMatchCount() && matchBonus) {
-            return SECOND;
-        }
         return Stream.of(values())
-                .filter(winningLank -> winningLank.getMatchCount() == matchCount)
+                .filter(winningLank -> winningLank.isMatch(matchCount, matchBonus))
                 .findFirst()
-                .orElse(NONE);
+                .orElseThrow(() -> new IllegalArgumentException("적절한 당첨 등수를 찾을 수 없습니다."));
+    }
+
+    private boolean isMatch(int matchCount, boolean matchBonus) {
+        if (this == SECOND) {
+            return matchCount == 5 && matchBonus;
+        }
+        if (this == THIRD) {
+            return matchCount == 5 && !matchBonus;
+        }
+        return this.matchCount == matchCount;
     }
 
     public int getMatchCount() {
