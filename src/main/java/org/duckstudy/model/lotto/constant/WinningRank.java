0f84cc294e848237879c1a65af44a392d0739a34
Mint <audwl03071@gmail.com>
2024-06-18 17:10:12 +0900

    refactor: 함수형 인터페이스 활용
    - BiPredicate를 사용하여 매칭 여부를 판단하도록 작성한다.
    - enum 객체 내에 함수를 정의하여 캡슐화한다.

--- src/main/java/org/duckstudy/model/lotto/constant/WinningRank.java
+++ src/main/java/org/duckstudy/model/lotto/constant/WinningRank.java
@@ -1,24 +1,27 @@
 package org.duckstudy.model.lotto.constant;
 
+import java.util.function.BiPredicate;
 import java.util.stream.Stream;
 
 public enum WinningRank {
 
-    NONE(0, 0, 0),
-    FIFTH(3, 5_000, 3),
-    FOURTH(4, 50_000, 4),
-    THIRD(5, 1_500_000, 5),
-    SECOND(5, 30_000_000, -5),
-    FIRST(6, 2_000_000_000, 6);
+    NONE(0, 0, 0, (matchCount, matchBonus) -> matchCount < 3),
+    FIFTH(3, 5_000, 3, (matchCount, matchBonus) -> matchCount == 3),
+    FOURTH(4, 50_000, 4, (matchCount, matchBonus) -> matchCount == 4),
+    THIRD(5, 1_500_000, 5, (matchCount, matchBonus) -> matchCount == 5 && !matchBonus),
+    SECOND(5, 30_000_000, -5, (matchCount, matchBonus) -> matchCount == 5 && matchBonus),
+    FIRST(6, 2_000_000_000, 6, (matchCount, matchBonus) -> matchCount == 6);
 
     private final int matchCount;
     private final int price;
     private final int key;
+    private final BiPredicate<Integer, Boolean> isMatchPredicate;
 
-    WinningRank(int matchCount, int price, int key) {
+    WinningRank(int matchCount, int price, int key, BiPredicate<Integer, Boolean> isMatchPredicate) {
         this.matchCount = matchCount;
         this.price = price;
         this.key = key;
+        this.isMatchPredicate = isMatchPredicate;
     }
 
     public static WinningRank findByMatchCountAndBonus(int matchCount, boolean matchBonus) {
@@ -28,14 +31,8 @@ public enum WinningRank {
                 .orElseThrow(() -> new IllegalArgumentException("적절한 당첨 등수를 찾을 수 없습니다."));
     }
 
-    private boolean isMatch(int matchCount, boolean matchBonus) {
-        if (this == SECOND) {
-            return matchCount == 5 && matchBonus;
-        }
-        if (this == THIRD) {
-            return matchCount == 5 && !matchBonus;
-        }
-        return this.matchCount == matchCount;
+    public boolean isMatch(int matchCount, boolean matchBonus) {
+        return isMatchPredicate.test(matchCount, matchBonus);
     }
 
     public int getMatchCount() {
