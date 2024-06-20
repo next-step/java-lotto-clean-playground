4cc45b0ddc74c8f4dc911ba0c14dd150f1dd4fe0
Hheojiwon <0618amy@naver.com>
2024-06-20 10:47:31 +0900

    refactor: lottomarket 수정

--- src/main/java/domain/LottoMarket.java
+++ src/main/java/domain/LottoMarket.java
@@ -1,8 +1,5 @@
 package domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class LottoMarket {
 
     private final int price;
@@ -14,17 +11,6 @@ public class LottoMarket {
     public Lottos generateTickets() {
         LottoTicket lottoTicket = new LottoTicket(price);
         int numberOfTickets = lottoTicket.getLottoTickets();
-        List<Lotto> lottoList = new ArrayList<>();
-
-        generateLotto(numberOfTickets, lottoList);
-
-        return new Lottos(lottoList);
-    }
-
-    private void generateLotto(int numberOfTickets, List<Lotto> lotto) {
-        for (int count = 0; count < numberOfTickets; count++) {
-            LottoNumbers lottoNumbers = new LottoNumbers();
-            lotto.add(new Lotto(lottoNumbers.generateLottoNumber()));
-        }
+        return Lottos.generateLottos(numberOfTickets);
     }
 }
