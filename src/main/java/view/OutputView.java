9f55a86ee98b1d60e011229c91948b4c6a5dda54
Hheojiwon <0618amy@naver.com>
2024-06-20 10:53:31 +0900

    refactor: outputView 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,26 +4,26 @@ import java.util.List;
 
 public class OutputView {
 
-    public void printLottoTickets(int LottoTickets) {
-        System.out.println(LottoTickets + "개를 구매했습니다.");
+    public void printLottoTickets(int lottoTickets) {
+        System.out.println(lottoTickets + "개를 구매했습니다.");
     }
 
     public void printLotto(List<Integer> LottoList) {
         System.out.println(LottoList);
     }
 
-    public void printWinningResult(int[] rankCounts, List<int[]> Ranks) {
+    public void printWinningResult(List<Integer> rankCounts, List<int[]> ranks) {
         System.out.println("당첨 통계");
         System.out.println("--------");
-        for (int i = 0; i < rankCounts.length; i++) {
-            int[] rank = Ranks.get(i);
-            System.out.println(rank[0] + "개 일치 " + rank[1] + "원- " + rankCounts[i]);
+        for (int i = 0; i < rankCounts.size(); i++) {
+            int[] rank = ranks.get(i);
+            System.out.println(rank[0] + "개 일치 " + rank[1] + "원- " + rankCounts.get(i));
         }
     }
+
 
     public void printProfit(double profit) {
         System.out.println("총 수익률은 " + profit + "입니다.");
     }
-
 
 }
