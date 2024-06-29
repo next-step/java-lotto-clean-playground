11dc433a6c4af3bf328760edef79e684602ed2a8
JoungMinJu <alswn9938@gmail.com>
2024-06-30 02:44:19 +0900

    refactor : Lotto 클래스에서 각 숫자가 유효한 범위인지 검증하도록 수정

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -7,12 +7,32 @@ import util.Errors;
 public class Lotto {
 
     public final static int SIZE = 6;
+    public final static int MIN_NUMBER = 1;
+    public final static int MAX_NUMBER = 45;
+    public final static int PRICE = 1000;
 
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validateLottoNumbersSize(numbers);
+        validateNumbers(numbers);
         this.numbers = numbers;
+    }
+
+    private void validateNumbers(List<Integer> numbers) {
+        validateNumbersRange(numbers);
+        validateLottoNumbersSize(numbers);
+    }
+
+    private void validateNumbersRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (!isNumberInRange(number)) {
+                throw new IllegalArgumentException(Errors.NUMBER_IS_NOT_IN_VALID_RANGE);
+            }
+        }
+    }
+
+    private boolean isNumberInRange(int number) {
+        return number >= MIN_NUMBER && number <= MAX_NUMBER;
     }
 
     private void validateLottoNumbersSize(List<Integer> numbers) {
--- src/main/java/util/Errors.java
+++ src/main/java/util/Errors.java
@@ -7,4 +7,6 @@ public class Errors {
     public static final String INPUT_IS_NOT_INTEGER = "시도 횟수로는 정수가 입력되어야 합니다.";
     public static final String INPUT_PRICE_IS_NEGATIVE = "구입금액은 음수일 수 없습니다.";
     public static final String WRONG_LOTTO_SIZE = String.format("로또 숫자의 개수는 %d개여야 합니다.", Lotto.SIZE);
+    public static final String NUMBER_IS_NOT_IN_VALID_RANGE = String.format("로또 각 숫자는 %d~%d사이의 숫자입니다.",
+                                                                            Lotto.MIN_NUMBER, Lotto.MAX_NUMBER);
 }
