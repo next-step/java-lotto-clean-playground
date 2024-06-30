370e8518bb9fc9cac85a4d558cd4085e85d4b702
Hheojiwon <0618amy@naver.com>
2024-07-01 01:24:12 +0900

    feat: 보너스 볼 당첨번호 생성하는 메서드

--- src/main/java/domain/LottoNumbers.java
+++ src/main/java/domain/LottoNumbers.java
@@ -3,12 +3,14 @@ package domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
-public class LottoNumbers {
+public class LottoNumbers implements LottoNumberGenerate {
 
     private static final int MAX_LOTTO_NUMBER = 45;
     private static final int LOTTO_SIZE = 6;
 
+    @Override
     public List<Integer> generateLottoNumber() {
         List<Integer> numbers = new ArrayList<>();
         for (int number = 0; number < MAX_LOTTO_NUMBER; number++) {
@@ -17,5 +19,11 @@ public class LottoNumbers {
 
         Collections.shuffle(numbers);
         return numbers.subList(0, LOTTO_SIZE);
+    }
+
+    @Override
+    public int generateBonusNumber() {
+        Random random = new Random();
+        return random.nextInt(MAX_LOTTO_NUMBER);
     }
 }
