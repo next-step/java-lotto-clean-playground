1578cdcf8c65ff421288e31a9cd32717c28e12b4
Hheojiwon <0618amy@naver.com>
2024-07-01 01:28:18 +0900

    feat: enum 수정, 2등 당첨 추가

--- src/main/java/domain/LottoRank.java
+++ src/main/java/domain/LottoRank.java
@@ -3,16 +3,27 @@ package domain;
 public enum LottoRank {
 
     FIRST(6, 2000000000),
+    BONUS(5,30000000, true),
     SECOND(5, 1500000),
     THIRD(4, 50000),
-    FOURTH(3, 5000);
+    FOURTH(3, 5000),
+    NONE(0,0);
+
 
     private final int count;
     private final int prize;
+    private final boolean bonusMatch;
 
     LottoRank(int count, int prize) {
         this.count = count;
         this.prize = prize;
+        bonusMatch = false;
+    }
+
+    LottoRank(int count, int prize, boolean bonusMatch) {
+        this.count = count;
+        this.prize = prize;
+        this.bonusMatch = bonusMatch;
     }
 
     public int getCount() {
@@ -21,5 +32,9 @@ public enum LottoRank {
 
     public int getPrize() {
         return prize;
+    }
+
+    public boolean isBonusMatch() {
+        return bonusMatch;
     }
 }
