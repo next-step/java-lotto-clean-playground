ebe74bb81784a1e80b97b7237f9b33c4ec866ad2
Hheojiwon <0618amy@naver.com>
2024-07-01 01:37:08 +0900

    feat: 2등 당첨 입출력 메서드 추가, 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,6 +1,9 @@
 package view;
 
+import domain.LottoRank;
+
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
 
@@ -12,15 +15,16 @@ public class OutputView {
         System.out.println(LottoList);
     }
 
-    public void printWinningResult(List<Integer> rankCounts, List<int[]> ranks) {
+    public void printWinningResult(Map<LottoRank, Integer> rankCounts) {
         System.out.println("당첨 통계");
         System.out.println("--------");
-        for (int i = 0; i < rankCounts.size(); i++) {
-            int[] rank = ranks.get(i);
-            System.out.println(rank[0] + "개 일치 " + rank[1] + "원- " + rankCounts.get(i));
+        for (LottoRank rank : LottoRank.values()) {
+            if (rank == LottoRank.NONE) {
+                continue;
+            }
+            System.out.println(rank.getCount() + "개 일치 (" + (rank.isBonusMatch() ? "보너스 볼 일치" : "") + ") " + rank.getPrize() + "원 - " + rankCounts.getOrDefault(rank, 0) + "개");
         }
     }
-
 
     public void printProfit(double profit) {
         System.out.println("총 수익률은 " + profit + "입니다.");
