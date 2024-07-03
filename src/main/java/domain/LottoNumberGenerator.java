eea372811c5650610d53e154ba50b99922886c22
JoungMinJu <alswn9938@gmail.com>
2024-07-03 23:05:52 +0900

    refactor : 로또 생성 로직 수정

--- src/main/java/domain/LottoNumberGenerator.java
+++ src/main/java/domain/LottoNumberGenerator.java
@@ -3,18 +3,18 @@ package domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LottoNumberGenerator {
 
     private static final List<Integer> NUMBERS = IntStream.rangeClosed(Lotto.MIN_NUMBER, Lotto.MAX_NUMBER)
         .boxed()
-        .toList();
+        .collect(Collectors.toList());
 
     public List<Integer> generate() {
-        List<Integer> lottoNumbers = new ArrayList<>(NUMBERS);
-        Collections.shuffle(lottoNumbers);
-        return lottoNumbers.subList(0, Lotto.SIZE);
+        Collections.shuffle(NUMBERS);
+        return NUMBERS.subList(0, Lotto.SIZE);
     }
 
 }
