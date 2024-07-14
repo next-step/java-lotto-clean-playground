77bea885cfcfb1b673104c87e10b2a42067e719f
JoungMinJu <alswn9938@gmail.com>
2024-07-14 17:14:52 +0900

    refactor : inputView가 domain을 모르도록 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,6 +1,5 @@
 package view;
 
-import domain.BonusBall;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -47,10 +46,5 @@ public class InputView {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(Errors.INPUT_NUMBER_IS_NOT_INTEGER);
         }
-    }
-
-    public BonusBall getBonusNumber() {
-        final int userIntegerInput = getUserIntegerInput();
-        return new BonusBall(userIntegerInput);
     }
 }
