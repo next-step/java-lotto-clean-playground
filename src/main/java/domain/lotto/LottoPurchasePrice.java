333dcfada9ee2f29c712548763666d30de8c8d72
Seokju Hong <iamseokjuhong@gmail.com>
2024-08-05 00:26:06 +0900

    로또 구매개수 관리하는 객체 분리 테스트 코드 작성

--- src/main/java/domain/lotto/LottoPurchasePrice.java
+++ src/main/java/domain/lotto/LottoPurchasePrice.java
@@ -4,16 +4,24 @@ import domain.common.Money;
 
 public class LottoPurchasePrice {
 
+    private static final String NEGATIVE_NUMBER = "음수 입력을 불가능합니다.";
     private static final int LOTTO_PRICE = 1000;
 
     private final int count;
 
     public LottoPurchasePrice(Money money) {
-        validate(money);
+        validateNegativeNumber(money);
+        validateMoneyUnit(money);
         this.count = getCount(money);
     }
 
-    private static void validate(Money money) {
+    private void validateNegativeNumber(Money money) {
+        if (money.getAmount() < 0) {
+            throw new IllegalArgumentException(NEGATIVE_NUMBER);
+        }
+    }
+
+    private static void validateMoneyUnit(Money money) {
         if (money.getAmount() % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("1000원 단위의 돈만 받음");
         }
