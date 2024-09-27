59e65d07bc8cadd70abd2c012f6fe53ed1149bf2
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2024-09-27 20:48:39 +0900

    [FEAT] : 1단계 - 구입금액 입력 구현

---
+++ src/main/java/util/InputFromUser.java
@@ -0,0 +1,12 @@
+package util;
+
+import java.util.Scanner;
+
+public class InputFromUser {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static int inputBuyingCosts() {
+        return Integer.parseInt(scanner.nextLine());
+    }
+}
