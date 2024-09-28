98f550e5985f69acd039c875c385bbe8efad35e9
davidolleh <asherolleh@gmail.com>
2024-09-28 20:52:08 +0900

    <FEAT> 로또 숫자 중복시 예외처리 기능 추가

--- src/main/java/domain/LottoNumbers.java
+++ src/main/java/domain/LottoNumbers.java
@@ -8,13 +8,26 @@ public class LottoNumbers {
     private final List<LottoNumber> numbers;
 
     public LottoNumbers(List<LottoNumber> numbers) {
-        validateSize(numbers);
+        validateNumber(numbers);
         this.numbers = numbers;
     }
 
-    public void validateSize(List<LottoNumber> numbers) {
-        if (numbers.size() > SIZE) {
-            throw new IllegalArgumentException();
+    private void validateNumber(List<LottoNumber> numbers) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+    }
+
+    private void validateSize(List<LottoNumber> numbers) {
+        if (numbers.size() != SIZE) {
+            throw new IllegalArgumentException("로또 번호의 갯수는 6이여야 합니다.");
+        }
+    }
+
+    private void validateDuplicate(List<LottoNumber> numbers) {
+        Set<LottoNumber> setNumbers = new HashSet<LottoNumber>(numbers);
+
+        if (setNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("로또 번호는 중복이 없어야 합니다");
         }
     }
 }
