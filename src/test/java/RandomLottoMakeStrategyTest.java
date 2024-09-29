c4011555167c5d15c94d4b9413204a80cda56f58
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2024-09-29 18:54:44 +0900

    [FEAT] : 1단계 - 랜덤 로또 생성 구현

---
+++ src/test/java/RandomLottoMakeStrategyTest.java
@@ -0,0 +1,43 @@
+import domain.LottoMakeStrategy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@DisplayName("로또 생성 테스트")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class RandomLottoMakeStrategyTest {
+
+    @DisplayName("6개의 번호를 갖는 로또를 만들 수 있다.")
+    @ParameterizedTest
+    @CsvSource({
+            "5, true",
+            "14, true",
+            "16, true",
+            "17, false",
+            "35, true",
+            "37, false",
+            "38, true",
+            "44, true",
+    })
+    public void makeLotto(final int lottoNumber, final boolean expected) {
+        //given
+        LottoMakeStrategy lottoMakeStrategy = new TestMakeLottoStrategy();
+
+        //when
+        List<Integer> result = lottoMakeStrategy.makeLotto();
+
+        //then
+        Assertions.assertThat(result.contains(lottoNumber)).isEqualTo(expected);
+    }
+
+    static class TestMakeLottoStrategy implements LottoMakeStrategy {
+        @Override
+        public List<Integer> makeLotto() {
+            return new ArrayList<Integer>(List.of(5, 14, 16, 35, 38, 44));
+        }
+    }
+}
