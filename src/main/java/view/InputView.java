c9a879c07949439f321f9ce4b2ed86bebcb623e2
davidolleh <asherolleh@gmail.com>
2024-09-30 22:24:15 +0900

    <FEAT> 공백 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -25,13 +25,13 @@ public class InputView {
     }
 
     public int readManualCount() {
-        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        System.out.println("\n수동으로 구매할 로또 수를 입력해 주세요.");
 
         return Integer.parseInt(sc.nextLine());
     }
 
     public LottoNumbers readManualLottoNumbers() {
-        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        System.out.println("\n수동으로 구매할 번호를 입력해 주세요.");
 
         String[] numbers = sc.nextLine().replaceAll(" ", "").split(",");
 
