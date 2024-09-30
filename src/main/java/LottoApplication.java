62aa8ee1b1885a66fecbb74090446d5c8405fb01
davidolleh <asherolleh@gmail.com>
2024-09-30 22:23:27 +0900

    <FEAT> controller로 게임 실행 기능 추가

--- src/main/java/LottoApplication.java
+++ src/main/java/LottoApplication.java
@@ -1,5 +1,10 @@
+import view.InputView;
+import view.OutputView;
+
 public class LottoApplication {
     public static void main(String[] args) {
+        LottoGameController lottoGameController = new LottoGameController(new InputView(), new OutputView());
 
+        lottoGameController.run();
     }
 }
