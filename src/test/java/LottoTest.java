ca64b57658c5899a89812ce73291015135f2451a
davidolleh <asherolleh@gmail.com>
2024-10-02 00:07:56 +0900

    <DELETE> 사용안하는 테스트 파일 삭제

--- src/test/java/LottoTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-@SuppressWarnings("NonAsciiCharacters")
-public class LottoTest {
-}
