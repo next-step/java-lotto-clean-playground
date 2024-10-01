15bff153698fe708a77d5ea7c83bebd2190b5a1e
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2024-10-01 23:53:54 +0900

    [FEAT] Lottos 클래스 필드 구현

---
+++ src/main/java/domain/Lottos.java
@@ -0,0 +1,12 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+    private final List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+}
