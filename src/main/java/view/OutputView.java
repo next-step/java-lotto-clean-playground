42afbe25103d8955c50effb29c38baa801c9cd52
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2024-10-02 03:17:29 +0900

    [FEAT] 당첨 결과 출력 메서드 구현 (2단계)

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -3,11 +3,23 @@ package view;
 import domain.Lotto;
 import domain.Lottos;
 
+import java.util.Map;
+
 public class OutputView {
 
     public static void printLottos(Lottos lottos){
         for(Lotto lotto : lottos.getLottos()){
             System.out.println(lotto);
         }
+    }
+
+    public static void printWinningResult(Map<Integer,Integer> resultMap){
+        System.out.println("당첨 통계");
+        System.out.println("_________");
+        System.out.println("3개 일치 (5000원) - "+resultMap.get("3")+"개");
+        System.out.println("4개 일치 (50000원) - "+resultMap.get("4")+"개");
+        System.out.println("5개 일치 (1500000원) - "+resultMap.get("5")+"개");
+        System.out.println("6개 일치 (2000000000원) - "+resultMap.get("6")+"개");
+
     }
 }
