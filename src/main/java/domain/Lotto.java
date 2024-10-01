82e0eb6c75a6cb405dcd73cd77c9bbba4b37a291
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2024-10-01 21:44:23 +0900

    [FEAT] Lotto 필드, 생성자 구현

---
+++ src/main/java/domain/Lotto.java
@@ -0,0 +1,12 @@
+package domain;
+
+import java.util.List;
+import java.util.Set;
+
+public class Lotto{
+    private final Set<Integer> lottoNumbers;
+
+    public Lotto(Set<Integer> lottoNumbers) {
+        this.lottoNumbers = lottoNumbers;
+    }
+}
