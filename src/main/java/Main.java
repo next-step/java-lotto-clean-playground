8ead40154c1b138c26afc14af587ffd8acd71aef
goldm0ng <jujs45177@gmail.com>
2024-10-02 19:02:56 +0900

    <ADD> Main 클래스 추가

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import controller.LottoController;
+import view.InputView;
+import view.ResultView;
+
+public class Main {
+    public static void main(String[] args) {
+        LottoController lottoController = new LottoController(
+                new InputView(),new ResultView());
+
+        lottoController.RunLottoApplication();
+    }
+}
