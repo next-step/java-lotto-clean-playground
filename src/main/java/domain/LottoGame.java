c4dc932804c9a3a03c73f3355f6a215fa3e3d98d
jermany17 <leejm5050@naver.com>
2024-10-02 13:50:39 +0900

    Domain - LottoGame 구현

---
+++ src/main/java/domain/LottoGame.java
@@ -0,0 +1,25 @@
+package domain;
+
+import java.util.List;
+
+public class LottoGame {
+
+    public static int matchNum(List<String> Lotto, List<String> LastWeekLotto){
+        int matchNum = 0;
+        for (String n : Lotto) {
+            if (LastWeekLotto.contains(n)) {
+                    matchNum++;
+            }
+        }
+        return matchNum;
+    }
+
+    public static int matchBonus(List<String> Lotto, String BonusBall){
+        for (String n : Lotto) {
+            if (n.equals(BonusBall)) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
