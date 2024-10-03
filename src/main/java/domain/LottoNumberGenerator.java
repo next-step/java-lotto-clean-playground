0561539cc43eca7390e0f8c9fcb05027cd273e67
davidolleh <asherolleh@gmail.com>
2024-10-04 00:27:31 +0900

    <REFACTOR> 한번에 램덤 숫자 6개로 로또 만들도록 설정

--- src/main/java/domain/LottoNumberGenerator.java
+++ src/main/java/domain/LottoNumberGenerator.java
@@ -6,17 +6,15 @@ import java.util.Collections;
 import java.util.List;
 
 public class LottoNumberGenerator {
-//    private final List<Integer> capableNumber = List.of(
-//        1, 2, 3, 4, 5, 6, 7, 8, 9,
-//        11, 12, 13, 14, 15, 16, 17, 18, 19,
-//        20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-//        30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-//        40, 41, 42, 43, 44, 45
-//    );
+    private final List<Integer> capableNumber = new ArrayList<>(Arrays.asList(
+            1, 2, 3, 4, 5, 6, 7, 8, 9,
+            11, 12, 13, 14, 15, 16, 17, 18, 19,
+            20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+            30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+            40, 41, 42, 43, 44, 45
+    ));
 
-    private final int RANDOM_INDEX = 0;
-    public LottoNumberGenerator() {
-    }
+    private static final int RANDOM_INDEX = 0;
     public LottoNumbers generateLottoNumbers() {
         // 이걸로 배열 등록하면 Collections shuffle 에서 터짐 왜?
 //        List<Integer> capableNumber = List.of(
@@ -27,40 +25,13 @@ public class LottoNumberGenerator {
 //                40, 41, 42, 43, 44, 45
 //        );
 
-        List<Integer> capableNumber = new ArrayList<>(Arrays.asList(
-                1, 2, 3, 4, 5, 6, 7, 8, 9,
-                11, 12, 13, 14, 15, 16, 17, 18, 19,
-                20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-                30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-                40, 41, 42, 43, 44, 45
-        ));
-
-        List<LottoNumber> value = new ArrayList<>();
-        for (int i = 0; i < LottoNumbers.SIZE; i++) {
-            LottoNumber lottoNumber = generateLottoNumber(capableNumber);
-            value.add(lottoNumber);
-        }
-
-        Collections.sort(value);
-
-        return new LottoNumbers(value);
-    }
-
-    private LottoNumber generateLottoNumber(List<Integer> capableNumber) {
-//        Integer[] capableNumber = {
-//                1, 2, 3, 4, 5, 6, 7, 8, 9,
-//                11, 12, 13, 14, 15, 16, 17, 18, 19,
-//                20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-//                30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-//                40, 41, 42, 43, 44, 45
-//        };
-
         Collections.shuffle(capableNumber);
-
-        int randomNumber = capableNumber.get(RANDOM_INDEX);
-
-        capableNumber.remove(RANDOM_INDEX);
-
-        return new LottoNumber(randomNumber);
+        return new LottoNumbers(
+                capableNumber.subList(0, LottoNumbers.SIZE)
+                        .stream()
+                        .sorted()
+                        .map(LottoNumber::new)
+                        .toList()
+        );
     }
 }
