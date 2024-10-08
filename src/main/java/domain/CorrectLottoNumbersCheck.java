b1d7890cd5f739de6d541a3d4b6b61c468b78e89
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2024-10-09 02:42:14 +0900

    [REFACTOR] : 5단계 - 필요없는 상수 제거

--- src/main/java/domain/CorrectLottoNumbersCheck.java
+++ src/main/java/domain/CorrectLottoNumbersCheck.java
@@ -2,9 +2,6 @@ package domain;
 
 public class CorrectLottoNumbersCheck {
 
-    private static final int IS_CORRECT_LOTTO_NUMBER = 1;
-    private static final int IS_NOT_CORRECT_LOTTO_NUMBER = 0;
-
     public int checkCorrectLottoNumbers(final WinningLotto winningLotto, final Lotto lotto) {
         for (final int winningLottoNumber : winningLotto.getLottoNumber()) {
             lotto.checkContainedWinningLottoNumbers(winningLottoNumber);
