34563edb38d70df5eb8081b588033501f9f59fd9
sinji <tobeabestns@naver.com>
2024-10-13 17:50:15 +0900

    refactor: separate main method

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -9,25 +9,40 @@ import view.OutputView;
 import static domain.Lotto.multipleLottoGenerator;
 
 public class LottoController {
-    public static void main(String[] args) {
-        int totalInvestment = InputView.inputToBuyLotto(); // 구입금액을 입력해주세요. (입력)
-        int numberOfManualLottos = InputView.inputToBuyManualLotto(); // 수동으로 구매할 로또 수를 입력해주세요. (입력)
-        int numberOfAutoLottos = totalInvestment / 1000 - numberOfManualLottos;
+    private final int totalInvestment; // 구입금액을 입력해주세요. (입력)
+    private final int numberOfManualLottos; // 수동으로 구매할 로또 수를 입력해주세요. (입력)
+    private final int numberOfAutoLottos; // 자동으로 구매할 로또 수(매개변수)
+    private List<List<Integer>> manualLottoInputs; // 수동 로또 리스트
+    private List<Integer> lastWeekWinningNumbers; // 지난주 당첨 번호
+    private int bonusWinningNumber; // 보너스 번호
 
-        List<List<Integer>> manualLottoInputs = InputView.inputToManualLotto(numberOfManualLottos); // 수동으로 구매할 번호를 입력해 주세요.
+    public LottoController() {
+        this.totalInvestment = InputView.inputToBuyLotto();
+        this.numberOfManualLottos = InputView.inputToBuyManualLotto();
+        this.numberOfAutoLottos = totalInvestment / 1000 - numberOfManualLottos;
+    }
 
-        OutputView.printResultOfBuying(numberOfManualLottos, numberOfAutoLottos); // 수동으로 장, 자동으로 개를 구매했습니다.
+    public void howManyOfLottos() {
+        manualLottoInputs = InputView.inputToManualLotto(numberOfManualLottos); // 수동으로 구매할 번호를 입력해 주세요.
+        OutputView.printResultOfBuying(numberOfManualLottos, numberOfAutoLottos); // 수동으로 n 장, 자동으로 k 개를 구매했습니다.
+    }
 
-        List<List<Integer>> lottoCollection = multipleLottoGenerator(numberOfAutoLottos);
-        manualLottoInputs.addAll(lottoCollection);
+    public void lottoGenerator() {
+        List<List<Integer>> lottoCollection = multipleLottoGenerator(numberOfAutoLottos); // 자동 로또 생성
+        manualLottoInputs.addAll(lottoCollection); // 수동 로또를 전체 로또 컬렉션에 추가
         OutputView.printResultOfAutoLotto(manualLottoInputs); // 로또 리스트 출력
+    }
 
-        List<Integer> lastWeekWinningNumbers = InputView.lastWeekWinningNumber(); // 지난주 당첨 번호를 입력해주세요. (입력)
-        int bonusWinningNumber = InputView.bonusWinningNumber(); // 보너스 볼을 입력해주세요. (입력)
+    public void winningNumberAndBonusNumber() {
+        lastWeekWinningNumbers = InputView.lastWeekWinningNumber(); // 지난주 당첨 번호 입력
+        bonusWinningNumber = InputView.bonusWinningNumber(); // 보너스 번호 입력
+    }
 
-        FindWinningLotto findWinningLotto = new FindWinningLotto();
+    public void findWinningLotto() {
+        FindWinningLotto findWinningLotto = new FindWinningLotto(); // 당첨 로또 찾기
+
         LottoDTO lottoDTO = findWinningLotto.countCollectNumber(manualLottoInputs, lastWeekWinningNumbers, bonusWinningNumber, totalInvestment);
 
-        OutputView.printLottoWinningStatistics(lottoDTO); // 당첨 통게 (출력)
+        OutputView.printLottoWinningStatistics(lottoDTO); // 당첨 통계 출력
     }
 }
