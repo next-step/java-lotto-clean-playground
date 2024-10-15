6714660dbcacffca86da0063754a62f54b876a8c
goldm0ng <jujs45177@gmail.com>
2024-10-15 17:03:38 +0900

    <Fix> 매직 넘버 제거 및 일급컬렉션화, 관련 역할 추가 부여

--- src/main/java/model/LottoResult.java
+++ src/main/java/model/LottoResult.java
@@ -1,22 +1,45 @@
 package model;
 
+import java.util.Collections;
 import java.util.Map;
 
 public class LottoResult {
 
+    private final int INITIAL_COUNT_BY_RANK = 0;
     private final Map<LottoRank, Integer> winningResult;
-    private final double earningRate;
 
-    public LottoResult(Map<LottoRank, Integer> winningResult, double earningRate) {
+    public LottoResult(Map<LottoRank, Integer> winningResult) {
+
+        for (LottoRank rank : LottoRank.values()) {
+            winningResult.put(rank,INITIAL_COUNT_BY_RANK);
+        }
+
         this.winningResult = winningResult;
-        this.earningRate = earningRate;
     }
 
     public Map<LottoRank, Integer> getWinningResult() {
-        return winningResult;
+        return Collections.unmodifiableMap(winningResult);
     }
 
-    public double getEarningRate() {
-        return earningRate;
+    public int calculateTotalPrize(Map<LottoRank, Integer> result) {
+
+        int  totalPrize = 0;
+
+        for (Map.Entry<LottoRank,Integer> entry : result.entrySet()) {
+            totalPrize += entry.getKey().getPrize() * entry.getValue();
+        }
+
+        return totalPrize;
+    }
+
+    public double calculateEarningRate(int totalPrize, int purchaseAmount) {
+        return (double) totalPrize / purchaseAmount;
+    }
+
+    public void operateLottoCheckMachine(LottoTickets lottoTickets,
+                                         LottoTicket winningNumbers, LottoNumber bonusNumber){
+
+        lottoTickets.checkWinningTickets(winningResult,winningNumbers,bonusNumber);
+
     }
 }
