8df412b1203839bf4e5567db04261a7328f159de
davidolleh <asherolleh@gmail.com>
2024-10-16 16:08:36 +0900

    <REFACTOR> 매번 생성되는 불필요한 리로스 전역 변수 설정으로 제거

--- src/main/java/domain/LottoNumberGenerator.java
+++ src/main/java/domain/LottoNumberGenerator.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class LottoNumberGenerator {
-    private final List<Integer> capableNumber = new ArrayList<>(Arrays.asList(
+    private static final List<Integer> capableNumber = new ArrayList<>(Arrays.asList(
             1, 2, 3, 4, 5, 6, 7, 8, 9,
             11, 12, 13, 14, 15, 16, 17, 18, 19,
             20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
@@ -14,17 +14,7 @@ public class LottoNumberGenerator {
             40, 41, 42, 43, 44, 45
     ));
 
-    private static final int RANDOM_INDEX = 0;
     public List<LottoNumber> generateLottoNumbers() {
-        // 이걸로 배열 등록하면 Collections shuffle 에서 터짐 왜?
-//        List<Integer> capableNumber = List.of(
-//                1, 2, 3, 4, 5, 6, 7, 8, 9,
-//                11, 12, 13, 14, 15, 16, 17, 18, 19,
-//                20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-//                30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-//                40, 41, 42, 43, 44, 45
-//        );
-
         Collections.shuffle(capableNumber);
         return capableNumber.subList(0, Lotto.SIZE)
                         .stream()
