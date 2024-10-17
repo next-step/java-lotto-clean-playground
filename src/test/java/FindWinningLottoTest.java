12b43073cfac15a8ce597fa9519a055e236bc079
sinji <tobeabestns@naver.com>
2024-10-17 18:18:12 +0900

    feat: add test code(winningResult_Five)

--- src/test/java/FindWinningLottoTest.java
+++ src/test/java/FindWinningLottoTest.java
@@ -27,7 +27,6 @@ public class FindWinningLottoTest {
         assertEquals(1, result.get(FindWinningLotto.LottoRank.SIX));  // 6개 일치
         assertEquals(0, result.get(FindWinningLotto.LottoRank.FIVE_WITH_BONUS));  // 보너스 번호 포함 5개 일치 없음
         assertEquals(1, result.get(FindWinningLotto.LottoRank.NONE));  // 나머지 번호는 일치하지 않음
-
     }
 
     @Test
@@ -49,6 +48,28 @@ public class FindWinningLottoTest {
         assertEquals(0, result.get(FindWinningLotto.LottoRank.SIX));
         assertEquals(1, result.get(FindWinningLotto.LottoRank.FIVE_WITH_BONUS));
         assertEquals(0, result.get(FindWinningLotto.LottoRank.FIVE));
+        assertEquals(1, result.get(FindWinningLotto.LottoRank.NONE));
+    }
+
+    @Test
+    public void testCalculateWinningResult_Five() {
+        // Given
+        List<List<Integer>> manualLottoInputs = List.of(
+                List.of(1, 2, 3, 4, 5, 7),
+                List.of(11, 12, 13, 14, 15, 16)
+        );
+        List<Integer> lastWeekWinningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        int bonusWinningNumber = 8;
+
+        FindWinningLotto findWinningLotto = new FindWinningLotto();
+
+        // When
+        Map<FindWinningLotto.LottoRank, Integer> result = findWinningLotto.calculateWinningResult(manualLottoInputs, lastWeekWinningNumbers, bonusWinningNumber);
+
+        // Then
+        assertEquals(0, result.get(FindWinningLotto.LottoRank.SIX));
+        assertEquals(0, result.get(FindWinningLotto.LottoRank.FIVE_WITH_BONUS));
+        assertEquals(1, result.get(FindWinningLotto.LottoRank.FIVE));
         assertEquals(1, result.get(FindWinningLotto.LottoRank.NONE));
     }
 }
