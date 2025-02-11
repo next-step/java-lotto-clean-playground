35760b2ae07332ceb575d82c860c78aa86ca84b2
dldb-chamchi <dladkfl49@gmail.com>
2025-02-12 00:04:31 +0900

    feat: inputview 완성

--- src/main/java/View.java
+++ src/main/java/View.java
@@ -1,4 +1,7 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class View {
     private static final Scanner in = new Scanner(System.in);
@@ -7,10 +10,28 @@ public class View {
         int amount;
         try{
             amount = in.nextInt();
-            if(amount <= 0) throw new RuntimeException("");
+            if(amount < 1000) throw new RuntimeException("");
+            if(amount % 1000 != 0) throw new RuntimeException("");
         }
-        catch (NumberFormatException e) throw new RuntimeException("");
+        catch (NumberFormatException e){
+            throw new RuntimeException("");
+        }
     }
 
+    public void intputWinningNums(){
+        in.nextLine();
+    }
 
+    private List<Integer> validateWiningNumber(String winningNums){
+        List<Integer> winingNumbers;
+        try{
+            winingNumbers = Arrays.stream(winningNums.split(","))
+                    .map(Integer::parseInt)
+                    .toList();
+        }
+        catch (NumberFormatException e){
+            throw new RuntimeException("");
+        }
+        return winingNumbers;
+    }
 }
