6e6c5a8dcb54841263fb8818e806a4fe04127736
dldb-chamchi <dladkfl49@gmail.com>
2025-02-12 21:42:04 +0900

    feat: Controller

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,2 +1,15 @@
 public class Controller {
+    private final LottoMarket market;
+    private final Statics statics;
+    private final Lotto lotto;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public Controller(LottoMarket market, Statics statics, Lotto lotto, InputView inputView, OutputView outputView){
+        this.market = market;
+        this.statics = statics;
+        this.lotto = lotto;
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
 }
