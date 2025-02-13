44fc795afd85a7725be894be12adf5a9b14fcd2d
nns503 <73779581+nns503@users.noreply.github.com>
2025-02-13 22:11:54 +0900

    refactor : BonusMask 상수화

--- src/main/java/domain/LottoRank.java
+++ src/main/java/domain/LottoRank.java
@@ -4,12 +4,12 @@ import java.util.stream.Stream;
 
 public enum LottoRank {
 
-    FIRST_PLACE(6, 2_000_000_000L, 3),
-    SECOND_PLACE(5, 30_000_000L, 2),
-    THIRD_PLACE(5, 1_500_000L, 1),
-    FOURTH_PLACE(4, 50_000L, 3),
-    FIFTH_PLACE(3, 5_000L, 3),
-    NO_PLACE(0, 0L, 3);
+    FIRST_PLACE(6, 2_000_000_000L, BonusMask.ANY),
+    SECOND_PLACE(5, 30_000_000L, BonusMask.HAS_BONUS),
+    THIRD_PLACE(5, 1_500_000L,      BonusMask.NO_BONUS),
+    FOURTH_PLACE(4, 50_000L, BonusMask.ANY),
+    FIFTH_PLACE(3, 5_000L, BonusMask.ANY),
+    NO_PLACE(0, 0L, BonusMask.ANY);
 
     private final int matchedCount;
     private final long prize;
@@ -24,16 +24,20 @@ public enum LottoRank {
     public static LottoRank of(int matchedCount, boolean isBonusNumber) {
         return Stream.of(values())
                 .filter(rank -> rank.matchedCount == matchedCount &&
-                        ((toBonusMask(isBonusNumber) & rank.bonusMask) != 0))
+                        isBonusMatch(isBonusNumber, rank.bonusMask))
                 .findAny()
                 .orElse(NO_PLACE);
     }
 
+    private static boolean isBonusMatch(boolean isBonusNumber, int bonusMask) {
+        return (toBonusMask(isBonusNumber) & bonusMask) != 0;
+    }
+
     private static int toBonusMask(boolean isBonusNumber) {
         if (isBonusNumber) {
-            return 2;
+            return BonusMask.HAS_BONUS;
         }
-        return 1;
+        return BonusMask.NO_BONUS;
     }
 
     public int getMatchedCount() {
@@ -45,6 +49,12 @@ public enum LottoRank {
     }
 
     public boolean isBonusNumber() {
-        return bonusMask == 2;
+        return bonusMask == BonusMask.HAS_BONUS;
+    }
+
+    private static class BonusMask {
+        public static final int NO_BONUS = 1;
+        public static final int HAS_BONUS = 2;
+        public static final int ANY = 3;
     }
 }
