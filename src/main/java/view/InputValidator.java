46b372da6da4891a0d2e0463b5c87255781ffa6b
chxghee <francis001021@gmail.com>
2025-03-26 11:31:18 +0900

    Test: 테스트 추가
    Utils와 InputValidator 클래스 테스트 추가

--- src/main/java/view/InputValidator.java
+++ src/main/java/view/InputValidator.java
@@ -6,7 +6,7 @@ public class InputValidator {
 
     public static void validateLottoNumbersInputPattern(String lottoNumbers) {
         if (!lottoNumbers.matches(LOTTO_NUMBERS_PATTERN)) {
-            throw new IllegalArgumentException("로또 번호는 총 6자이고, 입력시 쉼표로 구분되어야 합니다!");
+            throw new IllegalArgumentException("로또 번호 입력은 쉼표로 구분되어야 합니다!");
         }
     }
 }
---
+++ src/test/java/view/InputValidatorTest.java
@@ -0,0 +1,17 @@
+package view;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputValidatorTest {
+
+    @Test
+    void 구분자가_쉼표가_아니면_예외가_발생해야_한다() {
+        String string = "123,124,234,23&1,2";
+        Assertions.assertThatThrownBy(() -> InputValidator.validateLottoNumbersInputPattern(string))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 번호 입력은 쉼표로 구분되어야 합니다!");
+    }
+}
