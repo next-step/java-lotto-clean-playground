1490d603b6f19d91a55a1ff195a7ac2fc0966172
Jiwoo Kim <ziuoo0424@gmail.com>
2025-03-30 04:59:30 +0900

    refactor: 당첨 통계, 상금 계산 로직을 제외한 수익률 계산 로직만을 담도록 개선

--- src/main/java/domain/ProfitCalculator.java
+++ src/main/java/domain/ProfitCalculator.java
@@ -1,61 +1,17 @@
 package domain;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class ProfitCalculator {
 
-    private Integer totalPrize = 0;
     private final Integer purchaseAmount;
-    private final Map<Integer, Integer> matchCountMap = new HashMap<>(); // 당첨 개수 저장
+    private int totalPrize;
 
-    private static final Map<Integer, Integer> PRIZE_TABLE = Map.of(
-            3, 5000,
-            4, 50000,
-            5, 1500000,
-            6, 2000000000
-    );
-
-    public ProfitCalculator(Integer purchaseAmount) {
+    public ProfitCalculator(Integer purchaseAmount, int totalPrize) {
         this.purchaseAmount = purchaseAmount;
-    }
-
-    public void calculateWinningStatistics(LottoList lottoList, List<Integer> winningNumbers) {
-        matchCountMap.clear();
-        for (Lotto lotto : lottoList.getLottoList()) {
-            int matchCount = lotto.calculateMatchCount(winningNumbers);
-
-            if (matchCount >= 3 && matchCount <= 6) {
-                matchCountMap.put(matchCount, matchCountMap.getOrDefault(matchCount, 0) + 1);
-            }
-        }
-
-        calculateTotalPrize();
-    }
-
-    public void calculateTotalPrize() {
-        if (matchCountMap.isEmpty()) {
-            return;
-        }
-
-        for (Map.Entry<Integer, Integer> entry : matchCountMap.entrySet()) {
-            int matchCount = entry.getKey();
-            int count = entry.getValue();
-            totalPrize += count * PRIZE_TABLE.get(matchCount);
-        }
+        this.totalPrize = totalPrize;
     }
 
     public final Double getProfitRate() {
         Double profitRate = (double) totalPrize / purchaseAmount;
-        return Math.round(profitRate * 100) / 100.0; // 둘째 자리까지 반올림
-    }
-
-    public Map<Integer, Integer> getMatchCountMap() {
-        return matchCountMap;
-    }
-
-    public static int getPrize(int matchCount) {
-        return PRIZE_TABLE.getOrDefault(matchCount, 0); // PRIZE_TABLE에서 값 가져오기
+        return Math.floor(profitRate * 100) / 100.0;
     }
 }
