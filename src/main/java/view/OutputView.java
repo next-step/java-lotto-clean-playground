03c3afd91e62822e7de1c708f1e9a9264d3e419d
Jiwoo Kim <ziuoo0424@gmail.com>
2025-03-30 05:00:33 +0900

    refactor: 변경된 로직에 맞게 인자 수정, 개행 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,15 +1,13 @@
 package view;
 
-import domain.Lotto;
-import domain.LottoList;
-import domain.ProfitCalculator;
+import domain.*;
 
 import java.util.Map;
 
 public class OutputView {
 
     public static void printPurchaseResult(Integer lottoCount, LottoList lottoList) {
-        System.out.println(lottoCount + "개를 구매했습니다.");
+        System.out.printf("%n%d개를 구매했습니다.%n", lottoCount);
 
         for (Lotto lotto : lottoList.getLottoList()) {
             System.out.println(lotto.getNumbers());
@@ -22,18 +20,18 @@ public class OutputView {
         System.out.println("---------");
     }
 
-    public static void printWinningStatistics(ProfitCalculator profitCalculator) {
-        Map<Integer, Integer> matchCountMap = profitCalculator.getMatchCountMap();
+    public static void printWinningStatistics(LottoStatistics lottoStatistics, PrizeCalculator prizeCalculator) {
+        Map<Integer, Integer> matchCountMap = lottoStatistics.getMatchCountMap();
 
         // 당첨 통계 출력
         for (int i = 3; i <= 6; i++) {
             int count = matchCountMap.getOrDefault(i, 0);
-            int prize = ProfitCalculator.getPrize(i);
+            int prize = prizeCalculator.getPrize(i);
             System.out.printf("%d개 일치 (%d원)- %d개%n", i, prize, count);
         }
     }
 
     public static void printProfitRate(double profitRate) {
-        System.out.printf("총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)%n", profitRate);
+        System.out.printf("총 수익률은 %.2f입니다.%n", profitRate);
     }
 }
