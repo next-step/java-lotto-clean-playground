184c85f0b030b7064d70807ddb44f0ded5387f33
Jiwoo Kim <ziuoo0424@gmail.com>
2025-03-30 05:03:07 +0900

    refactor: 리뷰 반영(생성기 controller에 주입) & domain 변경에 따른 파라미터 조정

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,64 +1,58 @@
 package controller;
 
-import domain.LottoList;
-import domain.ProfitCalculator;
-import domain.RandomLottoNumberGenerator;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
+import domain.*;
 import view.InputView;
 import view.OutputView;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class LottoController {
 
-    private final static int PRICE_PER_LOTTO = 1000;
+    private final LottoNumberGenerator generator;
 
-    private final static RandomLottoNumberGenerator generator = new RandomLottoNumberGenerator();
+    public LottoController(LottoNumberGenerator generator) {
+        this.generator = generator;
+    }
 
     public void run() {
         final Integer purchaseAmount = InputView.readPurchaseAmount();
-        final LottoList lottoList = createLottoList(purchaseAmount);
+        final LottoList lottoList = new LottoList(purchaseAmount, generator);
 
-        OutputView.printPurchaseResult(purchaseAmount / PRICE_PER_LOTTO, lottoList);
+        OutputView.printPurchaseResult(lottoList.getLottoCount(), lottoList);
 
-        List<Integer> winningNumbers = getWinningNumbers();
+        List<LottoNumber> winningNumbers = getWinningNumbers();
 
-        ProfitCalculator profitCalculator = new ProfitCalculator(purchaseAmount);
-        printWinningStatistics(lottoList, winningNumbers, profitCalculator);
-        printProfitRate(profitCalculator);
-    }
-
-    // 로또 생성
-    private LottoList createLottoList(Integer purchaseAmount) {
-        int lottoCount = purchaseAmount / PRICE_PER_LOTTO;
-        return new LottoList(lottoCount, generator);
+        printWinningStatistics(lottoList, winningNumbers);
+        printProfitRate(purchaseAmount,lottoList.calculatePrize(winningNumbers));
     }
 
     // 당첨 번호 입력 및 변환
-    private List<Integer> getWinningNumbers() {
+    private List<LottoNumber> getWinningNumbers() {
         String winningNumbersInput = InputView.readWinningNumbers();
         return convertInputToNumbers(winningNumbersInput);
     }
 
-    // 당첨 통계 계산 및 출력
-    private void printWinningStatistics(LottoList lottoList, List<Integer> winningNumbers, ProfitCalculator profitCalculator) {
-        profitCalculator.calculateWinningStatistics(lottoList, winningNumbers);
+    private List<LottoNumber> convertInputToNumbers(String winningNumbersInput) {
+        return Stream.of(winningNumbersInput.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .map(LottoNumber::new)
+                .collect(Collectors.toList());
+    }
 
+    // 당첨 통계 계산 및 출력
+    private void printWinningStatistics(LottoList lottoList, List<LottoNumber> winningNumbers) {
         OutputView.printWinningStatisticsMessage();
-        OutputView.printWinningStatistics(profitCalculator);
+        PrizeCalculator PrizeCalculator = new PrizeCalculator();
+        OutputView.printWinningStatistics(lottoList.calculateStatistics(winningNumbers), PrizeCalculator);
     }
 
     // 수익률 출력
-    private void printProfitRate(ProfitCalculator profitCalculator) {
+    private void printProfitRate(Integer purchaseAmount, int totalPrize) {
+        ProfitCalculator profitCalculator = new ProfitCalculator(purchaseAmount, totalPrize);
         double profitRate = profitCalculator.getProfitRate();
         OutputView.printProfitRate(profitRate);
-    }
-
-    private List<Integer> convertInputToNumbers(String winningNumbersInput) {
-        return Arrays.stream(winningNumbersInput.split(", "))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
     }
 }
