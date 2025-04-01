19c9c183f2955601ffd36aad86b80de3353d818b
Jiwoo Kim <ziuoo0424@gmail.com>
2025-04-02 05:10:00 +0900

    feat: 보너스볼 입력 메서드 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,28 +1,25 @@
 package view;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView {
 
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     public static long readPurchaseAmount() {
-        while (true) {
-            System.out.println("구입금액을 입력해 주세요.");
-            String input = scanner.nextLine().trim();
-
-            if (!input.matches("\\d+")) {
-                System.out.println("유효한 정수를 입력하세요.");
-                continue;
-            }
-
-            return Long.parseLong(input);
-        }
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = SCANNER.nextLine().trim();
+        return Long.parseLong(input);
     }
 
     public static String readWinningNumbers() {
         System.out.printf("%n지난 주 당첨 번호를 입력해 주세요.%n");
-        return scanner.nextLine();
+        return SCANNER.nextLine();
+    }
+
+    public static int readBonusNumber() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+        String input = SCANNER.nextLine().trim();
+        return Integer.parseInt(input);
     }
 }
