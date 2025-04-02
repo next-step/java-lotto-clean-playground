13b10c8eebec348a3971a8d886d9f5f38f4c481a
Jiwoo Kim <ziuoo0424@gmail.com>
2025-04-02 23:42:56 +0900

    feat: 수동 로또 개수, 번호 입력 메서드 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,25 +1,38 @@
 package view;
 
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class InputView {
 
     private static final Scanner SCANNER = new Scanner(System.in);
 
-    public static long readPurchaseAmount() {
+    public long readPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        String input = SCANNER.nextLine().trim();
-        return Long.parseLong(input);
+        return Long.parseLong(SCANNER.nextLine());
     }
 
-    public static String readWinningNumbers() {
-        System.out.printf("%n지난 주 당첨 번호를 입력해 주세요.%n");
+    public int readManualLottoCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        return Integer.parseInt(SCANNER.nextLine());
+    }
+
+    public List<String> readManualLottoNumbers(int count) {
+        System.out.println("수동으로 구매할 번호를 입력해주세요.");
+        return IntStream.range(0, count)
+                .mapToObj(i -> SCANNER.nextLine().trim()) // 한 줄씩 입력받아 공백 제거
+                .collect(Collectors.toList()); // 리스트로 변환
+    }
+
+    public String readWinningNumbers() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         return SCANNER.nextLine();
     }
 
-    public static int readBonusNumber() {
+    public int readBonusNumber() {
         System.out.println("보너스 볼을 입력해 주세요.");
-        String input = SCANNER.nextLine().trim();
-        return Integer.parseInt(input);
+        return Integer.parseInt(SCANNER.nextLine());
     }
 }
