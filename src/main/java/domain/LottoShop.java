0579d901680107887ce46ab3055fe40120a51239
gjtjrl303 <gjtjrl303@naver.com>
2025-04-07 13:03:51 +0900

    refacotor: LottoPurchase에서 Lottos를 반환하도록 변경

--- src/main/java/domain/LottoShop.java
+++ src/main/java/domain/LottoShop.java
@@ -5,20 +5,21 @@ import java.util.List;
 
 public class LottoShop {
 
+    public static final int PRICE_PER_TICKET = 1000;
+
     private final LottoMachine lottoMachine;
 
     public LottoShop(LottoMachine lottoMachine) {
         this.lottoMachine = lottoMachine;
     }
 
-    public LottoPurchase purchaseLottos(Money money, LottoCount manualCount, List<Lotto> manualLottos) {
-        if (money.getPurchasedLottoCount() < manualCount.getLottoCount()) {
-            throw new IllegalArgumentException("금액이 수동 로또 개수보다 부족합니다.");
-        }
+    public Lottos purchaseLottos(Money money, LottoCount manualCount, List<Lotto> manualLottos) {
+        validate(money, manualCount, manualLottos);
+
         LottoCount autoLottoCount = LottoCount.from(money.getPurchasedLottoCount() - manualCount.getLottoCount());
         List<Lotto> autoLottos = lottoMachine.generateLottos(autoLottoCount);
-
-        List<Lotto> allLottos = new ArrayList<>(manualLottos);
+        List<Lotto> allLottos = new ArrayList<>();
+        allLottos.addAll(manualLottos);
         allLottos.addAll(autoLottos);
 
         return LottoPurchase.createLottoPurchase(money, manualCount, allLottos);
