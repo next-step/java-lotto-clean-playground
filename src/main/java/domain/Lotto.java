b8ab10f254c5b315e19ea37b62d04503dcf66d14
gjtjrl303 <gjtjrl303@naver.com>
2025-04-07 12:54:45 +0900

    feat: 로또 타입 enum 추가

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,5 +1,7 @@
 package domain;
 
+import enums.LottoType;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
@@ -11,13 +13,15 @@ public class Lotto {
     private static final int LOTTO_NUMBER_COUNT = 6;
 
     Set<LottoNumber> lotto;
+    LottoType lottoType;
 
-    private Lotto(Set<LottoNumber> lotto) {
+    private Lotto(Set<LottoNumber> lotto, LottoType lottoType) {
         validateLotto(lotto);
         this.lotto = lotto;
+        this.lottoType = lottoType;
     }
 
-    public static Lotto from(List<Integer> numbers) {
+    public static Lotto from(List<Integer> numbers, LottoType lottoType) {
         Set<LottoNumber> lotto = numbers.stream()
                 .map(LottoNumber::from)
                 .collect(Collectors.toCollection(TreeSet::new));
@@ -25,16 +29,21 @@ public class Lotto {
         if (lotto.size() != numbers.size()) {
             throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
         }
-        return new Lotto(lotto);
+
+        return new Lotto(lotto, lottoType);
     }
 
     public Set<LottoNumber> getLottoNumbers() {
         return Collections.unmodifiableSet(lotto);
     }
 
+    public LottoType getType() {
+        return lottoType;
+    }
+
     private void validateLotto(Set<LottoNumber> lotto) {
         if (lotto.size() != LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException("로또 숫자는 6개여야 합니다");
         }
-    } 
+    }
 }
