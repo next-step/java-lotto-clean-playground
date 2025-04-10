027bd8a06ea1d5dd16f4153e898e7ff74e27e9be
gjtjrl303 <gjtjrl303@naver.com>
2025-04-10 10:14:06 +0900

    refactor: money amount, int에서 BigDecimal로 변경

--- src/main/java/domain/Money.java
+++ src/main/java/domain/Money.java
@@ -1,33 +1,38 @@
 package domain;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import static domain.LottoShop.PRICE_PER_TICKET;
 
 public class Money {
 
-    private final int amount;
+    private final BigDecimal amount;
 
-    private Money(int amount) {
-        validate(amount);
+    private Money(BigDecimal amount) {
         this.amount = amount;
     }
 
-    public static Money from(int amount) {
-        return new Money(amount);
+    public static Money from(int inputAmount) {
+        BigDecimal decimalAmount = BigDecimal.valueOf(inputAmount);
+        validate(decimalAmount);
+        return new Money(decimalAmount);
     }
 
     public int getPurchasedLottoCount() {
-        return amount / PRICE_PER_TICKET;
+        return amount.divide(PRICE_PER_TICKET, RoundingMode.HALF_UP).intValue();
     }
 
-    public int getAmount() {
+    public BigDecimal getAmount() {
         return amount;
     }
 
-    private void validate(int value) {
-        if (value < PRICE_PER_TICKET) {
+    private static void validate(BigDecimal value) {
+        if (value.compareTo(PRICE_PER_TICKET) < 0) {
             throw new IllegalArgumentException("로또는 최소 1장 이상 구매해야 합니다.");
         }
-        if (value % PRICE_PER_TICKET != 0) {
+
+        if (value.remainder(PRICE_PER_TICKET).compareTo(BigDecimal.ZERO) != 0) {
             throw new IllegalArgumentException("1000원 단위로만 구매할 수 있습니다.");
         }
     }
