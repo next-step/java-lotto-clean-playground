3bb3e3bb244b62687abb4f2b6072b1fd83e8e69c
gjtjrl303 <gjtjrl303@naver.com>
2025-04-10 10:14:49 +0900

    refactor: PrizeMoney amount double 에서 BigDecimal로 변경

--- src/main/java/domain/ProfitCalculator.java
+++ src/main/java/domain/ProfitCalculator.java
@@ -2,28 +2,32 @@ package domain;
 
 import enums.LottoRank;
 
+import java.math.BigDecimal;
 import java.util.Map;
 
 import static domain.LottoShop.PRICE_PER_TICKET;
 
 public abstract class ProfitCalculator {
 
-    public static double calculateProfitRate(Map<LottoRank, Integer> resultByRank, LottoCount lottoCount) {
+    public static BigDecimal calculateProfitRate(Map<LottoRank, Integer> resultByRank, LottoCount lottoCount) {
         if (resultByRank == null) {
             throw new IllegalStateException("로또 결과가 생성되지 않았습니다.");
         }
         PrizeMoney totalPrize = calculateTotalPrize(resultByRank);
-        return totalPrize.getAmount() / (lottoCount.getLottoCount() * PRICE_PER_TICKET);
+        BigDecimal lottoCountDecimal  = BigDecimal.valueOf(lottoCount.getLottoCount());
+
+        return totalPrize.getAmount().divide(lottoCountDecimal.multiply(PRICE_PER_TICKET));
     }
 
     public static PrizeMoney calculateTotalPrize(Map<LottoRank, Integer> resultByRank) {
-        PrizeMoney totalPrize = new PrizeMoney(0.0);
+        PrizeMoney totalPrize = new PrizeMoney(BigDecimal.valueOf(0));
 
         for (Map.Entry<LottoRank, Integer> matchCount : resultByRank.entrySet()) {
             PrizeMoney prizePerRank = new PrizeMoney(matchCount.getKey().getPrize());
             PrizeMoney prizeTotal = prizePerRank.multiply(matchCount.getValue());
             totalPrize = totalPrize.plus(prizeTotal);
         }
+        System.out.println(totalPrize.getAmount());
         return totalPrize;
     }
 }
