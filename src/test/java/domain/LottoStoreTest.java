71cbaf83a0f6d666fd0f329146cd41f4e791c8b8
dd-jiyun <dlwldbs8015@gmail.com>
2025-05-16 18:40:04 +0900

    feat: 입력한 금액만큼 로또 번호 구매해주는 객체 생성

---
+++ src/test/java/domain/LottoStoreTest.java
@@ -0,0 +1,28 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import strategy.FixNumberGenerator;
+import strategy.LottoNumberGenerator;
+
+class LottoStoreTest {
+
+    @Test
+    @DisplayName("구입 금액에 맞춰 올바른 수의 로또가 생성된다")
+    void buy_shouldReturnCorrectNumberOfLottos() {
+        // given
+        LottoNumberGenerator generator = new FixNumberGenerator();
+        LottoStore store = new LottoStore(generator);
+        PurchaseAmount purchaseAmount = new PurchaseAmount(3000);
+
+        // when
+        Lottos lottos = store.buy(purchaseAmount);
+
+        // then
+        assertThat(lottos.getValues())
+                .hasSize(3)
+                .allSatisfy(lotto -> assertThat(lotto.getNumbers()).containsExactly(1, 2, 3, 4, 5, 6));
+    }
+}
