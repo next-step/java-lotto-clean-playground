71050a5d5c370e1a4fa50a6609e1c847b42c476d
jisoo78 <ekelldks@naver.com>
2025-05-16 17:18:01 +0900

    test: 테스트코드 작성 및 요구사항 개선

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,20 +4,29 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Application {
-    private static final int LOTTO_MONEY = 1000;
+    private static final int MINIMUM_AMOUNT = 1000;
 
     public static void main(String[] args) {
+        int money = inputMoney();
+        int count = money / MINIMUM_AMOUNT;
+        for (int i = 0; i < count; i++) {
+            List<Integer> lottoNumbers = LottoManage.pullOutNumbers();
+            LottoManage.shuffleNumbers(lottoNumbers);
+            List<Integer> ticket = LottoManage.pickupLottoNumbers(lottoNumbers);
+            System.out.println(ticket);
+            System.out.println("내가 찾고 싶은 숫자" + ticket.contains(14));
+        }
+    }
+
+    private static int inputMoney() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("구입 금액 입력(금액은 1000원 이상 입력해주세요): ");
         int money = scanner.nextInt();
-        if (money < 1000) {
-            System.out.println("구매에 필요한 최소 금액은 1000원 입니다.");
+
+        while (money < MINIMUM_AMOUNT) {
+            System.out.println("구매에 필요한 금액은 1000원 이상입니다 다시 입력해주세요");
+            money = scanner.nextInt();
         }
-        int count = money / LOTTO_MONEY;
-        for (int i = 0; i < count; i++) {
-            List<Integer> lottoNumbers = LottoManage.shuffleNumbers();
-            System.out.println(lottoNumbers);
-        }
-        scanner.close();
+        return money;
     }
 }
