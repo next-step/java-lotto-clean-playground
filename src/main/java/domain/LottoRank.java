608255c2c188aa3861c9cd654369a90e81c22858
siyoung <wfs0502@gmail.com>
2025-05-17 20:47:04 +0900

    feat: LottoRank 구현

---
+++ src/main/java/domain/LottoRank.java
@@ -0,0 +1,36 @@
+package domain;
+
+import java.util.Arrays;
+
+public enum LottoRank {
+    FIRST(6, 2000000000),
+    SECOND(5, 1500000),
+    THIRD(4, 50000),
+    FOURTH(3, 5000),
+    MISS(0, 0)
+    ;
+
+    private final int matchCount;
+
+    private final int prize;
+
+    LottoRank(int matchCount, int price) {
+        this.matchCount = matchCount;
+        this.prize = price;
+    }
+
+    public static LottoRank findRank(int matchCount) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.matchCount == matchCount)
+                .findFirst()
+                .orElse(MISS);
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+}
