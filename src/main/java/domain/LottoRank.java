e708800e4047900d4b68f9c33ea2eaa9387a3bd2
siyoung <wfs0502@gmail.com>
2025-05-17 21:04:58 +0900

    fix: 출력 메시지 수정

--- src/main/java/domain/LottoRank.java
+++ src/main/java/domain/LottoRank.java
@@ -3,10 +3,10 @@ package domain;
 import java.util.Arrays;
 
 public enum LottoRank {
-    FIRST(6, 2000000000),
-    SECOND(5, 1500000),
-    THIRD(4, 50000),
     FOURTH(3, 5000),
+    THIRD(4, 50000),
+    SECOND(5, 1500000),
+    FIRST(6, 2000000000),
     MISS(0, 0)
     ;
 
--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -5,12 +5,13 @@ import domain.LottoRank;
 import domain.LottoResult;
 import domain.Numbers;
 import java.util.Collections;
+import java.util.List;
 
 public class ResultView {
     private static final String PURCHASED_COUNT = "%d개를 구매했습니다.%n";
     private static final String RESULT_TITLE = "당첨 통계";
     private static final String DIVIDER_LINE = "---------";
-    private static final String RESULT_FORMAT = "%d개 일치 (%d원)- %d개";
+    private static final String RESULT_FORMAT = "%d개 일치 (%d원)- %d개%n";
 
     public void printLottoCount(int count) {
         System.out.printf(PURCHASED_COUNT, count);
@@ -18,14 +19,19 @@ public class ResultView {
 
     public void printLottoNumbers(Lotto lotto) {
         Numbers numbers = lotto.getNumbers();
-        Collections.sort(numbers.getNumbers());
-        System.out.println(numbers);
+        List<Integer> numbersList = numbers.getNumbers();
+        Collections.sort(numbersList);
+        System.out.println(numbersList);
     }
 
     public void printResult(LottoResult result) {
+        System.out.println();
         System.out.println(RESULT_TITLE);
         System.out.println(DIVIDER_LINE);
         for (LottoRank rank : LottoRank.values()) {
+            if (rank == LottoRank.MISS) {
+                continue;
+            }
             int count = result.getResult().getOrDefault(rank, 0);
             System.out.printf(RESULT_FORMAT, rank.getMatchCount(), rank.getPrize(), count);
         }
