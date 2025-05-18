3f71ff381aa8e467733330300ba49c32e2dca021
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-05-18 21:17:04 +0900

    feat: 당첨 번호 입력 및 결과 확인 기능 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,19 +1,1 @@
-import domain.AutoLottoGenerator;
-import domain.LottoGenerator;
-import domain.LottoHistory;
-import domain.LottoManager;
-import view.InputView;
-import view.OutputView;
-
-public class Application {
-    public static void main(String[] args) {
-        int money = InputView.readBuyMoney();
-
-        LottoGenerator generator = new AutoLottoGenerator();
-        LottoManager manager = new LottoManager(generator);
-        LottoHistory history = manager.purchaseLottos(money);
-
-        OutputView.printLottoHistory(history);
-
-    }
-}
+import domain.*;import domain.generator.AutoLottoGenerator;import domain.generator.LottoGenerator;import view.InputView;import view.OutputView;public class Application {    public static void main(String[] args) {        int money = InputView.readBuyMoney();        LottoGenerator generator = new AutoLottoGenerator();        LottoManager manager = new LottoManager(generator);        LottoHistory history = manager.purchaseLottos(money);        OutputView.printLottoHistory(history);        String input = InputView.readWinningNumber();        WinningNumbers winningNumbers = new WinningNumbers(input);        LottoWinningChecker checker = new LottoWinningChecker();        checker.checkLotto(history, winningNumbers);        OutputView.printWinningResult(checker, history.size());    }}
