ec0a255ee11950b1d8eff06a1cdfdb50d327bd7f
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-05-18 21:14:05 +0900

    feat: 당첨 번호 받는 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,4 +9,9 @@ public class InputView {
         System.out.println("구입 금액을 입력해 주세요.");
         return scanner.nextInt();
     }
+
+    public static String readWinningNumber() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        return scanner.next();
+    }
 }
