a6c23ab3fdcad4f5f76a86ec5bcd0f73f9865a4b
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-05-19 14:33:42 +0900

    refactor: LottoNumber record로 리팩토링

--- src/main/java/domain/LottoNumber.java
+++ src/main/java/domain/LottoNumber.java
@@ -2,38 +2,18 @@ package domain;
 
 import java.util.Objects;
 
-public class LottoNumber {
+public record LottoNumber(int number) {
     private static final int LOTTO_MIN = 1;
     private static final int LOTTO_MAX = 45;
 
-    private final int number;
-
-    public LottoNumber(int number) {
+    public LottoNumber {
         validateInRange(number);
-        this.number = number;
     }
 
     private void validateInRange(int number) {
         if (number < LOTTO_MIN || number > LOTTO_MAX) {
             throw new IllegalArgumentException("로또 번호는 1 ~ 45 사이여야 합니다.");
         }
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof LottoNumber)) return false;
-        LottoNumber that = (LottoNumber) o;
-        return number == that.number;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(number);
     }
 
     @Override
