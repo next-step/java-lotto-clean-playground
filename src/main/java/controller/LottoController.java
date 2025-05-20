2847bda66ce210b52ac6b4f9421d11f08792ce25
qkrcodus <qkrcodus0722@gmail.com>
2025-05-21 07:29:58 +0900

    fix : controller 메서드 분리

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -10,27 +10,44 @@ import model.LottoStatistics;
 import view.LottoView;
 
 public class LottoController {
-    public void run(){
+    public void run() {
         LottoView view = new LottoView();
         int purchaseAmount = view.readPurchaseAmount();
-        int manualLottoCount = view.readManualLottoCount();
-        List<Lotto> manualLottos = view.readManualLottos(manualLottoCount);
-
+        List<Lotto> manualLottos = getManualLottos(view);
         LottoPurchase purchase = new LottoPurchase(manualLottos, purchaseAmount);
-        int autoLottoCount = purchase.getAutoLottoCount();
-        view.printLottoCount(manualLottoCount, autoLottoCount);
 
-        LottoGenerator generator = new LottoGenerator();
-        List<Lotto> autoLottos = generator.generate(autoLottoCount);
-        List<Lotto> allLotttos = new ArrayList<>();
-        allLotttos.addAll(manualLottos);
-        allLotttos.addAll(autoLottos);
-        view.printLotto(allLotttos);
+        List<Lotto> allLottos = getAllLottos(view, manualLottos, purchase);
+        LottoStatistics statistics = createStatistics(view, allLottos, purchaseAmount);
 
-        List<LottoNumber> winningNumbers = view.readWinningNumbers();
-
-        LottoNumber bonusNumber = view.readBonusNumber();
-        LottoStatistics statistics = new LottoStatistics(allLotttos, winningNumbers, bonusNumber, purchaseAmount);
         view.printStatistics(statistics);
     }
+
+    private List<Lotto> getManualLottos(LottoView view) {
+        int manualCount = view.readManualLottoCount();
+        return view.readManualLottos(manualCount);
+    }
+
+    private List<Lotto> getAllLottos(LottoView view, List<Lotto> manualLottos, LottoPurchase purchase) {
+        int manualCount = manualLottos.size();
+        int autoCount = purchase.getAutoLottoCount();
+
+        view.printLottoCount(manualCount, autoCount);
+
+        LottoGenerator generator = new LottoGenerator();
+        List<Lotto> autoLottos = generator.generate(autoCount);
+
+        List<Lotto> allLottos = new ArrayList<>();
+        allLottos.addAll(manualLottos);
+        allLottos.addAll(autoLottos);
+
+        view.printLotto(allLottos);
+        return allLottos;
+    }
+
+    private LottoStatistics createStatistics(LottoView view, List<Lotto> allLottos, int purchaseAmount) {
+        List<LottoNumber> winningNumbers = view.readWinningNumbers();
+        LottoNumber bonusNumber = view.readBonusNumber();
+        return new LottoStatistics(allLottos, winningNumbers, bonusNumber, purchaseAmount);
+    }
 }
+
