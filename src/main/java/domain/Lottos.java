11a24a19f20ef7e0651b8a920dfd3fb881aeb0a1
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-05-25 19:35:48 +0900

    feat: Lottos에 merge 메서드 구현

--- src/main/java/domain/Lottos.java
+++ src/main/java/domain/Lottos.java
@@ -1,6 +1,7 @@
 package domain;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Lottos {
     //구매한 로또들의 내역
@@ -17,6 +18,14 @@ public class Lottos {
 
     public int size() {
         return lottos.size();
+    }
+
+    public static Lottos merge(Lottos manual, Lottos auto) {
+        List<Lotto> combined = Stream.concat(
+                manual.getLottos().stream(),
+                auto.getLottos().stream()
+        ).toList();
+        return new Lottos(combined);
     }
 }
 
