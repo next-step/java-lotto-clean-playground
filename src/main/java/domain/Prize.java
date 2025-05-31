9660d840ea098d6fb12626054b5c05316b849f12
wosyh18 <wosyh18@naver.com>
2025-05-31 15:53:17 +0900

    chore : 불필요 클래스 삭제

--- src/main/java/domain/Prize.java
+++ src/main/java/domain/Prize.java
@@ -3,11 +3,11 @@ package domain;
 import java.util.Arrays;
 
 public enum Prize {
-    FIRST(6, false, 2_000_000_000, "6개 일치"),
-    SECOND(5, true, 30_000_000, "5개 일치, 보너스 볼 일치"),
-    THIRD(5, false, 1_500_000, "5개 일치"),
-    FOURTH(4, false, 50_000, "4개 일치"),
-    FIFTH(3, false, 5_000, "3개 일치");
+    FIRST(6, false, 2000000000, "6개 일치"),
+    SECOND(5, true, 30000000, "5개 일치, 보너스 볼 일치"),
+    THIRD(5, false, 1500000, "5개 일치"),
+    FOURTH(4, false, 50000, "4개 일치"),
+    FIFTH(3, false, 5000, "3개 일치");
 
     private final int matchCount;
     private final boolean bonusStatus;
@@ -22,10 +22,6 @@ public enum Prize {
         this.description = description;
     }
 
-    public boolean isBonusStatus() {
-        return bonusStatus;
-    }
-
     public static Prize of(int matchCount, boolean bonusMatch) {
         return Arrays.stream(values())
                        .filter(p -> p.matchCount == matchCount)
@@ -36,11 +32,6 @@ public enum Prize {
 
     public String getDescription() {
         return description;
-    }
-
-    public int getMatchCount() {
-
-        return matchCount;
     }
 
     public int getReward() {
