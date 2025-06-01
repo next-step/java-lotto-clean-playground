97472576d912ffb31eff666b16949c5549d40b18
minji <mmm307955@gmail.com>
2025-06-02 08:23:45 +0900

    fix: 최신 LottoController 복구

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,49 +1,65 @@
 package controller;
 
+import domain.BonusNumber;
 import domain.BuyAmount;
 import domain.LottoMachine;
-import domain.LottoResult;
 import domain.LottoTicket;
 import domain.LottoTickets;
 import domain.MatchResult;
+import domain.Prize;
 import domain.WinningNumbers;
 import java.util.List;
+import java.util.stream.Collectors;
 import view.InputView;
 import view.OutputView;
 
 public class LottoController {
+    //메서드의 길이가 10이 넘지 않도록 한다.
     public void run() {
-        //로또들을 구매하고 티켓을 생성한다
-        BuyAmount buyAmount = new BuyAmount(InputView.inputMoney());
-        List<LottoTicket> lottoTickets = buyLottoTickets(buyAmount);
+        int totalAmount = InputView.inputMoney();
+        List<LottoTicket> handTickets = readHandTickets();
+        BuyAmount buyAmount = new BuyAmount(totalAmount, handTickets.size());
 
-        //구매한 로또들을 출력한다
+        List<LottoTicket> lottoTickets = buyLottoTickets(handTickets, buyAmount);
         OutputView.printTickets(lottoTickets);
 
-        //저번 주 당첨번호를 입력한다
-        WinningNumbers winningNumbers = new WinningNumbers(InputView.inputWinningNumbers());
+        BonusNumber bonusNumber = new BonusNumber(InputView.inputBonusNumber());
+        WinningNumbers winningNumbers = createWinningNumbers(bonusNumber);
 
-        //등수에 따라 몇 개가 맞았는지 출력한다
-        MatchResult matchResult = calculateMatchResult(lottoTickets, winningNumbers);
+        MatchResult matchResult = calculateMatchResult(lottoTickets, winningNumbers, bonusNumber);
         OutputView.printResult(matchResult);
-
-        //수익률을 계산하고 출력한다
         printProfit(matchResult, buyAmount);
     }
 
-    private List<LottoTicket> buyLottoTickets(BuyAmount buyAmount) {
-        LottoMachine lottoMachine = new LottoMachine();
-        int ticketCount = buyAmount.getPurchasableTicketCount();
-        OutputView.printTicketCount(ticketCount);
-        return lottoMachine.generateTickets(ticketCount);
+    private List<LottoTicket> readHandTickets() {
+        int handCount = InputView.howManyTimeBuyHandTicket();
+        List<String> handInputs = InputView.writeHandTickets(handCount);
+        return handInputs.stream()
+            .map(LottoTicket::from)
+            .collect(Collectors.toList());
     }
 
-    private MatchResult calculateMatchResult(List<LottoTicket> tickets, WinningNumbers winningNumbers) {
-        return new LottoTickets(tickets).countMatchResults(winningNumbers);
+    private List<LottoTicket> buyLottoTickets(List<LottoTicket> handTickets, BuyAmount buyAmount) {
+        LottoMachine lottoMachine = new LottoMachine();
+        int handCount = buyAmount.getHandCount();
+        int autoCount = buyAmount.getAutoCount();
+        OutputView.printTicketCount(handCount, autoCount);
+        return lottoMachine.generateTickets(handTickets, buyAmount.getAutoCount());
+    }
+
+    private WinningNumbers createWinningNumbers(BonusNumber bonusNumber) {
+        WinningNumbers winningNumbers = new WinningNumbers(InputView.inputWinningNumbers());
+        winningNumbers.validateBonusNumber(bonusNumber);
+        return winningNumbers;
+    }
+
+    private MatchResult calculateMatchResult(List<LottoTicket> tickets,
+        WinningNumbers winningNumbers, BonusNumber bonusNumber) {
+        return new LottoTickets(tickets).countMatchResults(winningNumbers, bonusNumber);
     }
 
     private void printProfit(MatchResult matchResult, BuyAmount buyAmount) {
-        double profitRate = LottoResult.calculateRateOfReturn(
+        double profitRate = Prize.calculateRateOfReturn(
             matchResult.calculateTotalPrize(),
             buyAmount.getAmount()
         );
