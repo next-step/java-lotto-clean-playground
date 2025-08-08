43ade5f3775990b22f8ea4a3ab9996f9a9784a8c
dldb-chamchi <dladkfl49@gmail.com>
2025-08-08 16:04:04 +0900

    style: 변수명 수정

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -7,27 +7,27 @@ import java.util.List;
 import java.util.Map;
 
 public class LottoController {
-    private final InputView in = new InputView();
-    private final ResultView out = new ResultView();
+    private final InputView input = new InputView();
+    private final ResultView output = new ResultView();
     private final StaticsResult stats = new StaticsResult();
 
     public void run() {
-        int amount = in.inputLottoAmount();
-        int manualCnt = in.manualLottoAmount(amount / 1000);
-        List<List<Integer>> manualNums = in.inputManualNumbers(manualCnt);
+        int amount = input.inputLottoAmount();
+        int manualCnt = input.manualLottoAmount(amount / 1000);
+        List<List<Integer>> manualNums = input.inputManualNumbers(manualCnt);
 
         LottoShop shop = new LottoShop(amount, manualNums);
         LottoTickets tickets = shop.getTickets();
-        out.printLottos(tickets.getAllLottos(), manualCnt, tickets.getRandomLottos().size());
+        output.printLottos(tickets.getAllLottos(), manualCnt, tickets.getRandomLottos().size());
 
         WinningLotto winning = new WinningLotto(
-                in.inputWinningNums(), in.inputBonusBall()
+                input.inputWinningNums(), input.inputBonusBall()
         );
 
         Map<Rank, Long> resultMap = stats.calculate(tickets.getAllLottos(), winning);
-        out.printWinningStatistics(resultMap);
+        output.printWinningStatistics(resultMap);
 
         double rate = stats.profitRate(resultMap, amount);
-        out.printProfitRate(rate);
+        output.printProfitRate(rate);
     }
 }
