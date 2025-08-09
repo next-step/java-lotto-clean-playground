1251cbc55c440d490263499c7df1fbe1b4dca4c0
dldb-chamchi <dladkfl49@gmail.com>
2025-08-09 22:52:06 +0900

    refactor: 로직 분리

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -12,21 +12,54 @@ public class LottoController {
     private final StaticsResult stats = new StaticsResult();
 
     public void run() {
-        int amount = input.inputLottoAmount();
-        int manualCnt = input.manualLottoAmount(amount / LottoShop.PRICE_PER_TICKET);
-        List<List<Integer>> manualNums = input.inputManualNumbers(manualCnt);
+        int amount = readPurchaseAmount();
+        int manualCnt = readManualCount(amount);
+        List<List<Integer>> manualNums = readManualNumbers(manualCnt);
 
+        LottoTickets tickets = createTickets(amount, manualNums);
+        printPurchasedLottos(tickets, manualCnt);
+
+        WinningLotto winning = readWinningLotto();
+
+        Map<Rank, Long> resultMap = calculateResults(tickets, winning);
+        printStatisticsAndProfit(resultMap, amount);
+    }
+
+    private int readPurchaseAmount() {
+        return input.inputLottoAmount();
+    }
+
+    private int readManualCount(int amount) {
+        return input.manualLottoAmount(amount / LottoShop.PRICE_PER_TICKET);
+    }
+
+    private List<List<Integer>> readManualNumbers(int manualCnt) {
+        return input.inputManualNumbers(manualCnt);
+    }
+
+    private LottoTickets createTickets(int amount, List<List<Integer>> manualNums) {
         LottoShop shop = new LottoShop(amount, manualNums);
-        LottoTickets tickets = shop.getTickets();
-        output.printLottos(tickets.getAllLottos(), manualCnt, tickets.getRandomLottos().size());
+        return shop.getTickets();
+    }
 
-        WinningLotto winning = new WinningLotto(
-                input.inputWinningNums(), input.inputBonusBall()
+    private void printPurchasedLottos(LottoTickets tickets, int manualCnt) {
+        int autoCnt = tickets.getRandomLottos().size();
+        output.printLottos(tickets.getAllLottos(), manualCnt, autoCnt);
+    }
+
+    private WinningLotto readWinningLotto() {
+        return new WinningLotto(
+                input.inputWinningNums(),
+                input.inputBonusBall()
         );
+    }
 
-        Map<Rank, Long> resultMap = stats.calculate(tickets.getAllLottos(), winning);
+    private Map<Rank, Long> calculateResults(LottoTickets tickets, WinningLotto winning) {
+        return stats.calculate(tickets.getAllLottos(), winning);
+    }
+
+    private void printStatisticsAndProfit(Map<Rank, Long> resultMap, int amount) {
         output.printWinningStatistics(resultMap);
-
         double rate = stats.profitRate(resultMap, amount);
         output.printProfitRate(rate);
     }
