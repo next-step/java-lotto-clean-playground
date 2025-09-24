849e2fedfb81cf85b6fa308a331829766bf38c11
taewoo <dnxorla03@naver.com>
2025-09-25 02:24:03 +0900

    RaceLotto 클래스에 금액 입력→번호 생성→당첨번호→통계 출력까지 실행 흐름 추가

---
+++ src/main/java/controller/RaceLotto.java
@@ -0,0 +1,31 @@
+package controller;
+
+import inputView.OutputView;
+import inputView.Price;
+import model.LottoNumbers;
+import model.LottoNumbersRepository;
+
+public class RaceLotto {
+    private final RaceLottoMethod race = new RaceLottoMethod();
+    private final OutputView outView = new OutputView();
+
+    public void race() {
+        // 1. 구입 금액 입력
+        Price price = race.readPrice();
+        outView.printPriceValue(price.getValue());
+        System.out.println();
+        outView.printBuyCount(price.howManyLottos());
+
+        // 2. 로또 번호 생성
+        LottoNumbersRepository repository = race.createLottos(price.howManyLottos());
+        outView.printLottos(repository.readLottoNumbersRepository());
+        System.out.println();
+
+        // 3. 당첨 번호 입력
+        LottoNumbers lastLotto = race.createLastLotto();
+        System.out.println();
+
+        // // 4. 통계 출력
+        race.createLotteryStatistics(repository.readLottoNumbersRepository(), lastLotto.getNumbers(), price.getValue());
+    }
+}
