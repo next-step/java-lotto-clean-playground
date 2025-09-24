52569901adddf469a27ac26fb0d25f9cb6b11fa7
taewoo <dnxorla03@naver.com>
2025-09-25 02:34:25 +0900

    Main 클래스 추가 – RaceLotto 실행 엔트리 포인트

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import controller.RaceLotto;
+
+public class Main {
+    public static void main(String[] args) {
+        RaceLotto controller = new RaceLotto();
+        controller.race();
+    }
+}
