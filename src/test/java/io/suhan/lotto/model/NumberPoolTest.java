4d53724fa3343ca823da8ab830f9a329b2d4465f
Suhan Ha <10433434+chemistryx@users.noreply.github.com>
2025-09-26 20:56:05 +0900

    test: NumberPool 범위 테스트 작성

--- src/test/java/io/suhan/lotto/model/NumberPoolTest.java
+++ src/test/java/io/suhan/lotto/model/NumberPoolTest.java
@@ -3,6 +3,7 @@ package io.suhan.lotto.model;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import io.suhan.lotto.model.lotto.Lotto;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
@@ -20,5 +21,10 @@ public class NumberPoolTest {
     @Test
     void from은_to보다_클_수_없다() {
         assertThrows(IllegalArgumentException.class, () -> new NumberPool(2, 1));
+    }
+
+    @Test
+    void 범위의_크기는_LOTTO_SIZE보다_커야_한다() {
+        assertThrows(IllegalArgumentException.class, () -> new NumberPool(Lotto.LOTTO_NUMBER_MIN, Lotto.LOTTO_NUMBER_MIN + Lotto.LOTTO_SIZE - 2));
     }
 }
