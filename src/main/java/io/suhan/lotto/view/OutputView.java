f64203ab900109b02f8d9d4e14cc2506fab63b6b
Suhan Ha <10433434+chemistryx@users.noreply.github.com>
2025-09-26 23:43:12 +0900

    refactor: OutputView 내 계산 로직 이동

--- src/main/java/io/suhan/lotto/view/OutputView.java
+++ src/main/java/io/suhan/lotto/view/OutputView.java
@@ -1,42 +1,29 @@
 package io.suhan.lotto.view;
 
-import io.suhan.lotto.model.DrawResult;
 import io.suhan.lotto.model.lotto.Lotto;
-import java.util.HashMap;
+import io.suhan.lotto.model.lotto.LottoStatistics;
 import java.util.List;
-import java.util.Map;
 
 public class OutputView {
-    public static final Map<Integer, Integer> winningsMap = Map.of(
-            3, 5000,
-            4, 50000,
-            5, 1500000,
-            6, 2000000000
-    );
-
     public static void printPurchaseResult(List<Lotto> lottos) {
         System.out.println("\n" + lottos.size() + "개를 구매했습니다.");
         printLottos(lottos);
     }
 
-    public static void printDrawResult(List<DrawResult> results, int totalSpent) {
-        Map<Integer, Long> countMap = calculateMatchedCounts(results);
+    public static void printStatistics(LottoStatistics statistics, int totalSpent) {
 
         System.out.println("\n당첨 통계");
         System.out.println("---------");
 
-        long totalWinnings = 0;
-
+        // 3개 ~ 6개 일치
         for (int i = 3; i <= 6; i++) {
-            long count = countMap.getOrDefault(i, 0L);
-            int winnings = winningsMap.get(i);
+            long count = statistics.getCountMap().getOrDefault(i, 0L);
+            int winnings = statistics.getWinningsMap().get(i);
 
             System.out.printf("%d개 일치 (%d원)- %d개\n", i, winnings, count);
-
-            totalWinnings += (winnings * count);
         }
 
-        double revenue = (double) totalWinnings / totalSpent;
+        double revenue = statistics.calculateRevenue(totalSpent);
 
         System.out.printf("총 수익률은 %.2f입니다.", revenue);
     }
@@ -47,16 +34,5 @@ public class OutputView {
 
     private static void printLotto(Lotto lotto) {
         System.out.println(lotto);
-    }
-
-    private static Map<Integer, Long> calculateMatchedCounts(List<DrawResult> results) {
-        Map<Integer, Long> countMap = new HashMap<>();
-
-        for (DrawResult result : results) {
-            int matchedCount = result.getMatchedCount();
-            countMap.put(matchedCount, countMap.getOrDefault(matchedCount, 0L) + 1);
-        }
-
-        return countMap;
     }
 }
