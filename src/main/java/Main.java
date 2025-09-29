32ff258bf0a0a0c4bf7e874fcdeb907443655059
taewoo <dnxorla03@naver.com>
2025-09-29 13:08:18 +0900

    Main에서 AutoLottoController 메서드들을 호출해 프로그램 실행하도록 구성

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,24 @@
 import controller.AutoLottoController;
+import inputView.Price;
+import model.LottoNumbers;
+import model.LottoNumbersRepository;
 
 public class Main {
     public static void main(String[] args) {
         AutoLottoController controller = new AutoLottoController();
-        controller.race();
+
+        // 1. 구입 금액 입력
+        Price money = controller.showPrice();
+
+        // 2. 로또 번호 생성
+        LottoNumbersRepository allLotteries = controller.showLottoNumbers(money.howManyLottos());
+
+        // 3. 당첨 번호 입력
+        LottoNumbers lastLotto = controller.readLastLotto();
+
+        // 4. 통계 출력
+        controller.showLotteryStatistics(allLotteries.readLottoNumbersRepository()
+                , lastLotto.getNumbers()
+                , money.getValue());
     }
 }
