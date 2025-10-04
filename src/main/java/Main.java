f157862e2eac8c41bae5843e61e5378f335f4f95
이고은 <6jtgoeun@naver.com>
2025-10-04 23:04:26 +0900

    [refector]getTicketCount수정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ import controller.LottoController;
 
 public class Main {
     public static void main(String[] args) {
-
         LottoController lottoController = new LottoController();
         lottoController.run();
     }
