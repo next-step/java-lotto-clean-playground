28647f0b880663abbe6ba5d8cd68681313d4ec45
taewoo <dnxorla03@naver.com>
2025-10-08 22:56:03 +0900

    MatchResult enum의 불필요한 description 파라미터 제거

--- src/main/java/model/MatchResult.java
+++ src/main/java/model/MatchResult.java
@@ -1,31 +1,25 @@
 package model;
 
 public enum MatchResult {
-    ZERO(0, "0개 일치", 0),
-    ONE(1, "1개 일치", 0),
-    TWO(2, "2개 일치", 0),
-    THREE(3, "3개 일치", 5000),
-    FOUR(4, "4개 일치", 50000),
-    FIVE(5, "5개 일치", 150000),
-    FIVE_BONUS(5, "5개 + 보너스 일치", 30000000),
-    SIX(6, "6개 일치", 2000000000);
+    ZERO(0, 0),
+    ONE(1, 0),
+    TWO(2, 0),
+    THREE(3, 5000),
+    FOUR(4, 50000),
+    FIVE(5, 150000),
+    FIVE_BONUS(5, 30000000),
+    SIX(6, 2000000000);
 
     private final int count;
-    private final String description;
     private final int reward;
 
-    MatchResult(int count, String description, int reward) {
+    MatchResult(int count, int reward) {
         this.count = count;
-        this.description = description;
         this.reward = reward;
     }
 
     public int getCount() {
         return count;
-    }
-
-    public String getDescription() {
-        return description;
     }
 
     public int getReward() {
