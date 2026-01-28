710f854d5ff246321d909dac440fc46ec4d36cfc
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-29 02:26:40 +0900

    feat: 로또 전반적 통계 정보와 수익 계산 기능을 담은 클래스 구현

---
+++ src/main/java/lotto/domain/model/WinningStatistics.java
@@ -0,0 +1,39 @@
+package lotto.domain.model;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public class WinningStatistics {
+
+    private final Map<LottoRank, Integer> statistics;
+
+    private final Money purchaseMoney;
+
+    public WinningStatistics(Money purchaseMoney) {
+        this.statistics = new EnumMap<>(LottoRank.class);
+        initDefaultValues();
+        this.purchaseMoney = purchaseMoney;
+    }
+
+    private void initDefaultValues() {
+        for (LottoRank rank : LottoRank.values()) {
+            statistics.put(rank, 0);
+        }
+    }
+
+    public void addResult(LottoRank rank) {
+        this.statistics.put(rank,  this.statistics.get(rank) + 1);
+    }
+
+    public int getCount(LottoRank rank) {
+        return statistics.get(rank);
+    }
+
+    public double calculateProfitRate() {
+        long totalPrize = 0;
+        for (LottoRank rank : statistics.keySet()) {
+            totalPrize += (long) rank.getPrizeMoney() * statistics.get(rank);
+        }
+        return (double) totalPrize / purchaseMoney.getAmount();
+    }
+}
