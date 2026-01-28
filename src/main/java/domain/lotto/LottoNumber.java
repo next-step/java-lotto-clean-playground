76ed65cf6ad9f048746a4f061ccfb5e04d87c913
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-28 23:30:14 +0900

    feat: 로또 번호 하나에 대한 원시값 포장으로, 1~45에 해당하는지 검증

---
+++ src/main/java/domain/lotto/LottoNumber.java
@@ -0,0 +1,23 @@
+package domain.lotto;
+
+public class LottoNumber {
+
+    private final int number;
+
+    public LottoNumber(int number) {
+        validate(number);
+        this.number = number;
+    }
+
+    private void validate(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(number);
+    }
+
+}
