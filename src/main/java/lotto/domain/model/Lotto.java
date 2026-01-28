cd8d3b0b5d9a14b8a690b94570b2b8cf11f6c5ac
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-29 01:39:57 +0900

    feat: 로또 번호 중복 방지를 위한 기존 리스트와 집합의 크기비교 적용

--- src/main/java/lotto/domain/model/Lotto.java
+++ src/main/java/lotto/domain/model/Lotto.java
@@ -1,31 +1,64 @@
 package lotto.domain.model;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 public class Lotto {
 
     private final List<LottoNumber> lottoNumbers;
 
     public Lotto(List<LottoNumber> lottoNumbers) {
-        validate(lottoNumbers);
+        validateSize(lottoNumbers);
         this.lottoNumbers = lottoNumbers;
     }
 
-    private void validate(List<LottoNumber> lottoNumbers) {
+    private void validateSize(List<LottoNumber> lottoNumbers) {
         if (lottoNumbers.size() != 6) {
             throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
         }
     }
 
     public static Lotto from(List<Integer> rawNumbers) {
+        validateDuplicate(rawNumbers);
         List<LottoNumber> lottoNumbers = rawNumbers.stream()
                 .map(LottoNumber::new)
                 .toList();
         return new Lotto(lottoNumbers);
     }
 
+    private static void validateDuplicate(List<Integer> rawNumbers) {
+        if (rawNumbers.size() != new HashSet<>(rawNumbers).size()) {
+            throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
+        }
+    }
+
     public List<LottoNumber> getNumbers() {
         return lottoNumbers;
     }
 
+    public int countMatch(Lotto other) { // 겹치는 수 반환
+        return (int) other.getNumbers().stream()
+            .filter(this.getNumbers()::contains)
+            .count();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Lotto lotto = (Lotto) o;
+        return Objects.equals(lottoNumbers, lotto.lottoNumbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumbers);
+    }
 }
