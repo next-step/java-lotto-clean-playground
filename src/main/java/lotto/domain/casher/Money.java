40a86fa9dd5e0e2c12bf6db08e5f36dd371efe6a
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-29 01:21:28 +0900

    chore: 에러 메세지 수정

--- src/main/java/lotto/domain/casher/Money.java
+++ src/main/java/lotto/domain/casher/Money.java
@@ -26,7 +26,7 @@ public class Money {
 
     private void validateUnit(int money) {
         if (money % 1000 != 0) {
-            throw new IllegalArgumentException("금액은 1000원 단위로 입력 가능합니다");
+            throw new IllegalArgumentException("금액은 1000원 단위로 입력 가능합니다.");
         }
     }
 
