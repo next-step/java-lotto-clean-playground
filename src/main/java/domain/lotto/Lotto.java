2f87c5a04fd3840c1d8ce3bdc7c69c054ba20f11
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-28 23:30:52 +0900

    feat: 로또 하나에 대한 클래스로 6개의 숫자를 가지고있는지 검증하고 정수 리스트를 받아 LottoNumber 리스트로 전환

---
+++ src/main/java/domain/lotto/Lotto.java
@@ -0,0 +1,31 @@
+package domain.lotto;
+
+import java.util.List;
+
+public class Lotto {
+
+    private final List<LottoNumber> lottoNumbers;
+
+    public Lotto(List<LottoNumber> lottoNumbers) {
+        validate(lottoNumbers);
+        this.lottoNumbers = lottoNumbers;
+    }
+
+    private void validate(List<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.size() != 6) {
+            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    public static Lotto from(List<Integer> rawNumbers) {
+        List<LottoNumber> lottoNumbers = rawNumbers.stream()
+                .map(LottoNumber::new)
+                .toList();
+        return new Lotto(lottoNumbers);
+    }
+
+    public List<LottoNumber> getNumbers() {
+        return lottoNumbers;
+    }
+
+}
