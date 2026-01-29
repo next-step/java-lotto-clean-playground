ba0d08f2ee5b24080ba1d423d020f823081e2465
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-29 19:52:02 +0900

    feat: 당첨로또에 보너스번호 필드 및 Getter 추가, 기존 번호들과 중복확인 로직 추가

--- src/main/java/lotto/domain/model/WinningLotto.java
+++ src/main/java/lotto/domain/model/WinningLotto.java
@@ -1,17 +1,29 @@
 package lotto.domain.model;
 
-import java.util.List;
-
 public class WinningLotto {
 
     private final Lotto winningLotto;
 
+    private final LottoNumber bonusNumber;
 
-    public WinningLotto(List<Integer> numbers) {
-        this.winningLotto = Lotto.from(numbers);
+
+    public WinningLotto(Lotto winningLotto, LottoNumber bonusNumber) {
+        validate(winningLotto, bonusNumber);
+        this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
     }
 
     public Lotto getWinningLotto() {
         return winningLotto;
+    }
+
+    public LottoNumber getBonusNumber() {
+        return bonusNumber;
+    }
+
+    private void validate(Lotto winningLotto, LottoNumber bonusNumber) {
+        if (winningLotto.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException("보너스 볼 번호는 기존 로또 번호와 중복될 수 없습니다.");
+        }
     }
 }
