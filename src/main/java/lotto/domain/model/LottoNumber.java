7e32036a5bffb4322288524eef80d3dd40d70fcf
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-29 19:57:16 +0900

    feat: 메모리 주소가 아닌 값으로 비교하도록 오버라이드

--- src/main/java/lotto/domain/model/LottoNumber.java
+++ src/main/java/lotto/domain/model/LottoNumber.java
@@ -1,5 +1,7 @@
 package lotto.domain.model;
 
+import java.util.Objects;
+
 public class LottoNumber {
 
     private final int number;
@@ -22,5 +24,18 @@ public class LottoNumber {
 
     public int getNumber() {
         return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
