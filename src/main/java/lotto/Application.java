ebef7a3f36e81a78cacceec7799ca25140777179
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-30 00:02:24 +0900

    feat: 수동 구매와 자동 구매를 따로 진행 후 결과를 낼 때 합하는 방식으로 어플리케이션 흐름 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,9 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
 import lotto.domain.model.Judge;
+import lotto.domain.model.Lotto;
 import lotto.domain.model.Money;
 import lotto.domain.generator.RandomLottoGenerator;
 import lotto.domain.model.LottoMachine;
@@ -16,21 +19,30 @@ public class Application {
         // 구입금액 입력받기
         Money purchaseAmount = InputView.inputMoney();
 
+        int maxPossibleCount = purchaseAmount.calculateLottoCount();
+        int manualCount = InputView.inputManualCount(maxPossibleCount);
+        int autoCount = maxPossibleCount - manualCount;
+
+        List<Lotto> manualLottos = InputView.inputManualLotto(manualCount);
+
         RandomLottoGenerator generator = new RandomLottoGenerator();
         LottoMachine lottoMachine = new LottoMachine(generator);
 
-        Lottos issuedLottos = lottoMachine.issue(purchaseAmount.calculateLottoCount());
+        Lottos autoLottos = lottoMachine.issue(autoCount);
+        List<Lotto> allLottoList = new ArrayList<>(manualLottos);
+        allLottoList.addAll(autoLottos.getValues());
+        Lottos totalLottos = new Lottos(allLottoList);
 
         // 매수 및 로또 출력
-        OutputView.print(issuedLottos.size());
-        OutputView.printLottos(issuedLottos);
+        OutputView.print(manualCount, autoCount);
+        OutputView.printLottos(totalLottos);
 
         // 당첨번호 입력받기
         WinningLotto winningLotto = InputView.inputWinningLotto();
 
         // 결과 판단
         WinningStatistics statistics = new WinningStatistics(purchaseAmount);
-        Judge.calculateResults(issuedLottos, winningLotto, statistics);
+        Judge.calculateResults(totalLottos, winningLotto, statistics);
 
         // 결과 출력
         OutputView.printResult(statistics);
