fbfca6db3e5a6399d88e16cff0eda16a180b7156
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-30 22:27:47 +0900

    refactor: 자동, 수동 로또 합산 로직을 LottoMachine에 위임하여 main 메서드 정리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -17,35 +17,28 @@ import lotto.view.OutputView;
 public class Application {
 
     public static void main(String[] args) {
-        // 구입금액 입력받기
+        // 구입 금액 입력
         Money purchaseAmount = InputView.inputMoney();
 
         int maxPossibleCount = purchaseAmount.calculateLottoCount();
         int manualCount = InputView.inputManualCount(maxPossibleCount);
-        int autoCount = maxPossibleCount - manualCount;
 
+        // 수동 구매
         List<Lotto> manualLottos = InputView.inputManualLotto(manualCount);
 
-        RandomLottoGenerator generator = new RandomLottoGenerator();
-        LottoMachine lottoMachine = new LottoMachine(generator);
+        LottoMachine lottoMachine = new LottoMachine(new RandomLottoGenerator());
 
-        Lottos autoLottos = lottoMachine.issue(autoCount);
-        List<Lotto> allLottoList = new ArrayList<>(manualLottos);
-        allLottoList.addAll(autoLottos.getValues());
-        Lottos totalLottos = new Lottos(allLottoList);
+        Lottos totalLottos = lottoMachine.issueWithManual(maxPossibleCount, manualLottos);
 
-        // 매수 및 로또 출력
+        int autoCount = maxPossibleCount - manualCount;
         OutputView.print(manualCount, autoCount);
         OutputView.printLottos(totalLottos);
 
-        // 당첨번호 입력받기
         WinningLotto winningLotto = InputView.inputWinningLotto();
 
-        // 결과 판단
         WinningStatistics statistics = new WinningStatistics(purchaseAmount);
         calculateResults(totalLottos, winningLotto, statistics);
 
-        // 결과 출력
         OutputView.printResult(statistics);
     }
 
--- src/main/java/lotto/domain/model/LottoMachine.java
+++ src/main/java/lotto/domain/model/LottoMachine.java
@@ -1,5 +1,6 @@
 package lotto.domain.model;
 
+import java.util.ArrayList;
 import lotto.domain.generator.LottoNumberGenerator;
 import lotto.domain.generator.RandomLottoGenerator;
 import java.util.List;
@@ -13,11 +14,28 @@ public class LottoMachine {
         this.generator = generator;
     }
 
-    public Lottos issue(int count) {
-        List<Lotto> issuedLottos = IntStream.range(0, count)
-            .mapToObj(i -> Lotto.from(generator.generateLottoNumbers()))
-                .toList();
-        return new Lottos(issuedLottos);
+    public Lottos issueWithManual(int totalCount, List<Lotto> manualLottos) {
+        int autoCount = calculateAutoCount(totalCount, manualLottos);
+
+        Lottos autoLottos = issue(autoCount);
+
+        List<Lotto> combinedLottos = new ArrayList<>(manualLottos);
+        combinedLottos.addAll(autoLottos.getValues());
+
+        return new Lottos(combinedLottos);
     }
 
+    private int calculateAutoCount(int totalCount, List<Lotto> manualLottos) {
+        int manualSize = manualLottos.size();
+        if (manualSize > totalCount) {
+            throw new IllegalArgumentException("수동 구매 수가 총 구매 가능 수를 초과할 수 없습니다.");
+        }
+        return totalCount - manualSize;
+    }
+
+    public Lottos issue(int count) {
+        return new Lottos(IntStream.range(0, count)
+            .mapToObj(i -> Lotto.from(generator.generateLottoNumbers()))
+            .toList());
+    }
 }
