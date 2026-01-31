2d3dc8cc8ddd3c9401cba8968785583d971dee8d
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-02-01 03:21:37 +0900

    test: LottoNumber에서 valueOf로 생성된 값들은 재사용이 되어 메모리가 같은지 테스트

--- src/test/java/lotto/model/LottoNumberTest.java
+++ src/test/java/lotto/model/LottoNumberTest.java
@@ -1,5 +1,7 @@
 package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import lotto.domain.model.LottoNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -32,6 +34,20 @@ public class LottoNumberTest {
         } catch (IllegalArgumentException e) {
             assert(e.getMessage().equals("로또 번호는 1부터 45 사이의 숫자여야 합니다."));
         }
+    }
+
+    @Test
+    @DisplayName("valueOf 메서드로 생성한 숫자들은 같은 숫자이면 캐싱된 값을 재사용한다.")
+    void valueOf_Returns_Cached_Instance() {
+        // given
+        int number = 7;
+
+        // when
+        LottoNumber firstCall = LottoNumber.valueOf(number);
+        LottoNumber secondCall = LottoNumber.valueOf(number);
+
+        // then
+        assertThat(firstCall).isSameAs(secondCall);
     }
 
 }
