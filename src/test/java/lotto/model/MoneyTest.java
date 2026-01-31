b7fc5c99b835e6864d47bc328ba57d87d0d46317
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-02-01 03:36:16 +0900

    test: Money에 대해 1000원 단위가 아닌 경우와 1000원 보다 적은 경우에 대한 테스트코드 추가

--- src/test/java/lotto/model/MoneyTest.java
+++ src/test/java/lotto/model/MoneyTest.java
@@ -34,4 +34,28 @@ public class MoneyTest {
                 .hasMessage("구입 금액은 숫자여야 합니다.");
     }
 
+    @Test
+    @DisplayName("입력된 금액이 1000원 단위가 아니면 IllegalArgumentException이 발생한다.")
+    void create_Money_With_Remainder_After_Division() {
+        // given
+        String input = "1100";
+
+        // when & then
+        assertThatThrownBy(() -> new Money(input))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("금액은 1000원 단위로 입력 가능합니다.");
+    }
+
+    @Test
+    @DisplayName("입력된 금액이 1000원 보다 적으면 IllegalArgumentException이 발생한다.")
+    void create_Money_With_Less_Than_1000() {
+        // given
+        String input = "900";
+
+        // when & then
+        assertThatThrownBy(() -> new Money(input))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("금액은 1000원 이상부터 가능합니다.");
+    }
+
 }
