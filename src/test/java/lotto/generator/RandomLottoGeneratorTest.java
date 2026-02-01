9fd3d927abb3d5457680344f99c1c4d8ee4d8c11
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-02-01 20:59:28 +0900

    test: RandomLottoGenerator의 테스트코드에 대해 구현체가 아닌 인터페이스와 람다식을 통해 결과를 고정하여 테스트

--- src/test/java/lotto/generator/RandomLottoGeneratorTest.java
+++ src/test/java/lotto/generator/RandomLottoGeneratorTest.java
@@ -3,7 +3,7 @@ package lotto.generator;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
-import lotto.domain.generator.RandomLottoGenerator;
+import lotto.domain.generator.LottoNumberGenerator;
 import lotto.domain.model.LottoNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,13 +14,21 @@ public class RandomLottoGeneratorTest {
     @DisplayName("랜덤 로또 번호 생성기는 6개의 로또 번호를 생성한다.")
     void generateLottoNumbers_ShouldReturnSixNumbers() {
         // given
-        RandomLottoGenerator generator = new RandomLottoGenerator();
+        LottoNumberGenerator stubGenerator = () -> List.of(
+            LottoNumber.valueOf(1), LottoNumber.valueOf(2),
+            LottoNumber.valueOf(3), LottoNumber.valueOf(4),
+            LottoNumber.valueOf(5), LottoNumber.valueOf(6)
+        );
 
         // when
-        List<LottoNumber> lottoNumbers = generator.generateLottoNumbers();
+        List<LottoNumber> result = stubGenerator.generateLottoNumbers();
 
         // then
-        assertThat(lottoNumbers.size()).isEqualTo(6);
+        assertThat(result).hasSize(6)
+            .containsExactly(
+                LottoNumber.valueOf(1), LottoNumber.valueOf(2), LottoNumber.valueOf(3),
+                LottoNumber.valueOf(4), LottoNumber.valueOf(5), LottoNumber.valueOf(6)
+            );
     }
 
 }
