caeccd2e338a155371eec1d05756e1e841380904
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-02-01 21:57:54 +0900

    test: 보너스볼과 당첨번호 중복 검증 로직에서 Lotto 내부에 미리 구현해둔 contains 메서드 사용

--- src/main/java/lotto/domain/model/WinningLotto.java
+++ src/main/java/lotto/domain/model/WinningLotto.java
@@ -17,7 +17,7 @@ public class WinningLotto {
         return LottoRank.valueOf(matchCount, matchBonus);
     }
     private void validate(Lotto winningLotto, LottoNumber bonusNumber) {
-        if (winningLotto.getNumbers().contains(bonusNumber)) {
+        if (winningLotto.contains(bonusNumber)) {
             throw new IllegalArgumentException("보너스 볼 번호는 기존 로또 번호와 중복될 수 없습니다.");
         }
     }
