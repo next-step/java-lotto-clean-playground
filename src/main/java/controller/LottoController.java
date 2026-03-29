c22fb34d7cfab2bde71f3e227b6d98bf28c2c722
dahyn <k0d5h16@gmail.com>
2026-03-29 18:25:45 +0900

    feat: LottoController 와 Application 구현

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,4 +1,19 @@
 package controller;
 
+import domain.LottoShop;
+import domain.Lottos;
+import domain.RandomNumberGenerator;
+import view.InputView;
+import view.OutputView;
+
 public class LottoController {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final LottoShop lottoShop = new LottoShop(new RandomNumberGenerator());
+
+    public void run() {
+        int amount = inputView.readAmount();
+        Lottos lottos = lottoShop.purchase(amount);
+        outputView.printLottos(lottos);
+    }
 }
