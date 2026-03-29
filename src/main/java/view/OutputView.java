41c70191bc00dd3778e83b6a83f07cf34b152709
hapdaypy <mgim9316@gmail.com>
2026-03-29 23:52:39 +0900

    feat: 결과 출력 기능 생성

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,7 +1,9 @@
 package view;
-
+import domain.CalculateLottoNumber;
 import domain.Lotto;
 import domain.LottoNumber;
+import domain.Rank;
+
 
 public class OutputView {
     public static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
@@ -19,5 +21,18 @@ public class OutputView {
             System.out.println(lotto.getNumbers());
         }
     }
+    public static void printWinningStatistics(CalculateLottoNumber result, int purchaseAmount) {
+        System.out.println("\n당첨 통계");
+        System.out.println("---------");
+        System.out.printf("3개 일치 (5000원)- %d개\n", result.getRankCount(Rank.THREE));
+        System.out.printf("4개 일치 (50000원)- %d개\n", result.getRankCount(Rank.FOUR));
+        System.out.printf("5개 일치 (1500000원)- %d개\n", result.getRankCount(Rank.FIVE));
+        System.out.printf("6개 일치 (2000000000원)- %d개\n", result.getRankCount(Rank.SIX));
+
+        double profitRate = result.calculateProfitRate(purchaseAmount);
+        System.out.printf("총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)\n", profitRate);
+    }
+
+
 
 }
