b5eb6ff3128dfcaa94df69cf616cca076b41b1c5
hapdaypy <mgim9316@gmail.com>
2026-03-29 20:53:37 +0900

    refactor: 메서드 이름 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,10 +4,10 @@ public class OutputView {
     public static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     public static final String LAST_WEEK_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
 
-    public static void InputPurchaseAmount(){
+    public static void printInputPurchaseAmount(){
         System.out.println(PURCHASE_AMOUNT_MESSAGE);
     }
-    public static void InputWinningNumber(){
+    public static void printInputWinningNumber(){
         System.out.println(LAST_WEEK_WINNING_NUMBER_MESSAGE);
     }
 }
