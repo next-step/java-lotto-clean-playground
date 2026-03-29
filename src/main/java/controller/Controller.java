bf79e605d27ebe1dfae4f67673f1cf2a4028a5c1
hapdaypy <mgim9316@gmail.com>
2026-03-30 00:01:12 +0900

    feat: 당첨번호 입력 호출, 결과 출력 호출 기능 추가

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,6 +1,7 @@
 package controller;
 
 import domain.LottoNumber;
+import domain.CalculateLottoNumber;
 import view.OutputView;
 import view.InputView;
 import domain.TrialNumber;
@@ -10,25 +11,23 @@ import domain.LottoNumberGenerator;
 import java.util.List;
 
 public class Controller {
-    public void run(){
+    public void run() {
         // [1] 구입 금액을 입력 받는다.
         OutputView.printInputPurchaseAmount();
         int purchaseNumber = InputView.inputPurchaseMoney();
-    //    System.out.println(purchaseNumber);
         // [2] 구입 금액으로 시도 횟수 만들기
         TrialNumber trialNumber = new TrialNumber(purchaseNumber);
-        // [3] 시도 횟수 만큼 로또 번호 생성하기
-        int lottoCount = trialNumber.getTrialNumber();
-     //   System.out.println(lottoCount);
-        // [4] 시도 횟수 만큼 랜덤 로또 생성;
+        // [3] 시도 횟수 만큼 랜덤 로또 생성;
         LottoNumberGenerator generator = new RandomLottoNumberGenerator();
         LottoNumber lottoNumber = new LottoNumber(trialNumber.getTrialNumber(), generator);
         OutputView.printLottos(lottoNumber);
-        // [5] 지난주 당첨 번호 입력 받기
-        List<Integer> winningNumber = InputView.inputWinningNumber();
-
-        // [6] 당첨번호와 로또 번호 비교해서 결과 탐색하기
-
-        // [7] 결과 출력하기
+        // [4] 지난주 당첨 번호 입력 받기
+        List<Integer> winningNumbers = InputView.inputWinningNumber();
+        // [5] 당첨번호와 로또 번호 비교해서 결과 탐색하기
+        CalculateLottoNumber statisticsResult = new CalculateLottoNumber(lottoNumber, winningNumbers);
+        // [6] 결과 출력하기
+        int PurchaseAmount = trialNumber.getPurchaseAmount();
+        OutputView.printWinningStatistics(statisticsResult, PurchaseAmount);
     }
 }
+
