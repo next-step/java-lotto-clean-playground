c5de9ef692a617c56fedd98513364548e3366819
hapdaypy <mgim9316@gmail.com>
2026-03-29 22:51:33 +0900

    feat: 랜덤 로또 번호를 출력하는 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,5 +1,8 @@
 package view;
 
+import domain.Lotto;
+import domain.LottoNumber;
+
 public class OutputView {
     public static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     public static final String LAST_WEEK_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
@@ -10,4 +13,11 @@ public class OutputView {
     public static void printInputWinningNumber(){
         System.out.println(LAST_WEEK_WINNING_NUMBER_MESSAGE);
     }
+
+    public static void printLottos(LottoNumber lottoNumber) {
+        for (Lotto lotto : lottoNumber.getLottoNumber()) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
 }
