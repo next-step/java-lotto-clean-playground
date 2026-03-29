57a69966c9a65a95b7f46e33bafa07b8ed60ad8c
dahyn <k0d5h16@gmail.com>
2026-03-29 14:46:24 +0900

    feat: Lotto 클래스 생성 및 검증 코드 추가

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,5 +1,36 @@
 package domain;
 
-public class Lotto {
+import java.util.Collections;
+import java.util.List;
 
+public class Lotto {
+    private final List<Integer> numbers;
+    private final int LOTTO_SIZE = 6;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = Collections.unmodifiableList(numbers);
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateLottoSize(numbers);
+        validateDuplicate(numbers);
+    }
+
+    private void validateLottoSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("로또 숫자의 갯수는 6개입니다.");
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        long distinctCount = numbers.stream().distinct().count();
+        if(distinctCount != LOTTO_SIZE){
+            throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
+        }
+    }
 }
