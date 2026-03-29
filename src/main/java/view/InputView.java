b0d1d4517624e790f15c35294f84df832d45adc5
hapdaypy <mgim9316@gmail.com>
2026-03-29 22:50:19 +0900

    chore: 컨벤션 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class InputView {
     public static final Scanner SCANNER = new Scanner(System.in);
     public static int inputPurchaseMoney(){
-        int PurchaseMoneny = SCANNER.nextInt();
-        return PurchaseMoneny;
+        int purchaseMoneny = SCANNER.nextInt();
+        return purchaseMoneny;
     }
 }
