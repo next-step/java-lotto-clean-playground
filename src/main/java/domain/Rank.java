96c0a6d1e273dd61e348dc2b8e5be39cb28aa1b6
hapdaypy <mgim9316@gmail.com>
2026-03-29 23:52:08 +0900

    feat: Enum을 통한 도메인 규칙 캡슐화

---
+++ src/main/java/domain/Rank.java
@@ -0,0 +1,30 @@
+package domain;
+
+import java.util.Arrays;
+
+public enum Rank {
+    NONE(0, 0),
+    THREE(3, 5000),
+    FOUR(4, 50000),
+    FIVE(5, 1500000),
+    SIX(6, 2000000000);
+
+    private final int matchCount;
+    private final int prizeMoney;
+
+    Rank(int matchCount, int prizeMoney) {
+        this.matchCount = matchCount;
+        this.prizeMoney = prizeMoney;
+    }
+
+    public static Rank valueOfMatchCount(int matchCount) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.matchCount == matchCount)
+                .findFirst()
+                .orElse(NONE);
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
+    }
+}
