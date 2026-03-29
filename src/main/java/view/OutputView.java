ab9294d8f866efb53cc2eb466801141000f48f15
hapdaypy <mgim9316@gmail.com>
2026-03-30 00:15:39 +0900

    refactor: 효율적으로 출력기능 변경

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -10,24 +10,32 @@ public class OutputView {
     public static final String LAST_WEEK_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
 
     public static void printInputPurchaseAmount(){
-        System.out.println(PURCHASE_AMOUNT_MESSAGE);
+        System.out.println("\n" + PURCHASE_AMOUNT_MESSAGE);
     }
     public static void printInputWinningNumber(){
-        System.out.println(LAST_WEEK_WINNING_NUMBER_MESSAGE);
+
+        System.out.println("\n" + LAST_WEEK_WINNING_NUMBER_MESSAGE);
     }
 
-    public static void printLottos(LottoNumber lottoNumber) {
+    public static void printLottos(LottoNumber lottoNumber,int count) {
+        System.out.println("\n" + count + "개를 구매했습니다.");
         for (Lotto lotto : lottoNumber.getLottoNumber()) {
             System.out.println(lotto.getNumbers());
         }
+
     }
     public static void printWinningStatistics(CalculateLottoNumber result, int purchaseAmount) {
-        System.out.println("\n당첨 통계");
-        System.out.println("---------");
-        System.out.printf("3개 일치 (5000원)- %d개\n", result.getRankCount(Rank.THREE));
-        System.out.printf("4개 일치 (50000원)- %d개\n", result.getRankCount(Rank.FOUR));
-        System.out.printf("5개 일치 (1500000원)- %d개\n", result.getRankCount(Rank.FIVE));
-        System.out.printf("6개 일치 (2000000000원)- %d개\n", result.getRankCount(Rank.SIX));
+        System.out.println("\n당첨 통계\n---------");
+
+        for (Rank rank : Rank.values()) {
+            if (rank == Rank.NONE) {
+                continue; // 미당첨 내역은 출력에서 제외
+            }
+            System.out.printf("%d개 일치 (%d원)- %d개\n",
+                    rank.getMatchCount(),
+                    rank.getPrizeMoney(),
+                    result.getRankCount(rank));
+        }
 
         double profitRate = result.calculateProfitRate(purchaseAmount);
         System.out.printf("총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)\n", profitRate);
