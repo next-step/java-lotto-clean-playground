adcb5d119cfc882e312cfa7ee3d2bbfa847a0f50
htdufhc-bob <htdufhc@gmail.com>
2026-03-30 16:37:29 +0900

    refactor: Lotto 클래스 내부로 유효성 검사 로직 통합 및 상수화

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,14 +1,18 @@
 package domain;
 
+import constant.ErrorMessage;
+
 import java.util.List;
 
 public record Lotto(List<Integer> lottoNumbers) {
     public Lotto {
-        validateLottoNumberCount(lottoNumbers);
-        validateLottoNumberRange(lottoNumbers);
-        validateNoDuplicateNumber(lottoNumbers);
+        lottoNumbers = lottoNumbers.stream()
+                .sorted()
+                .toList();
 
-        lottoNumbers = List.copyOf(lottoNumbers);
+        validateLottoLength(lottoNumbers);
+        validateLottoRange(lottoNumbers);
+        validateNoDuplicateNumber(lottoNumbers);
     }
 
     public int countMatchingNumbers(List<Integer> winningNumbers) {
@@ -17,24 +21,24 @@ public record Lotto(List<Integer> lottoNumbers) {
                 .count();
     }
 
-    private void validateLottoNumberCount(List<Integer> lottoNumbers) {
+    private void validateLottoLength(List<Integer> lottoNumbers) {
         if (lottoNumbers.size() != 6) {
-            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_COUNT.getMessage());
+        }
+    }
+
+    private void validateLottoRange(List<Integer> lottoNumbers) {
+        boolean isValidRange = lottoNumbers.stream()
+                .allMatch(num -> num >= 1 && num <= 45);
+        if (!isValidRange) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_RANGE.getMessage());
         }
     }
 
     private void validateNoDuplicateNumber(List<Integer> lottoNumbers) {
         long uniqueCount = lottoNumbers.stream().distinct().count();
         if (uniqueCount != 6) {
-            throw new IllegalArgumentException("로또 번호 사이에 중복된 숫자가 존재합니다.");
-        }
-    }
-
-    private void validateLottoNumberRange(List<Integer> lottoNumbers) {
-        long validRangeCount = lottoNumbers.stream()
-                .filter(num -> (num > 0 && num <= 45)).count();
-        if (validRangeCount != 6) {
-            throw new IllegalArgumentException("로또 번호는 1과 45 사이에 정수여야 합니다.");
+            throw new IllegalArgumentException(ErrorMessage.INVALID_DUPLICATE_NUMBER.getMessage());
         }
     }
 }
