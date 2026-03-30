3bb89481e19d674284baa2259f6a138971786dad
htdufhc-bob <htdufhc@gmail.com>
2026-03-30 09:40:39 +0900

    feat: 로또 실행을 위한 메인 애플리케이션 구현

---
+++ src/main/java/Application.java
@@ -0,0 +1,16 @@
+import controller.LottoController;
+import domain.LottoNumbersGenerator;
+import view.InputView;
+import view.OutputView;
+
+public class Application {
+
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        LottoNumbersGenerator lottoNumbersGenerator = new LottoNumbersGenerator();
+        LottoController lottoController = new LottoController(inputView, outputView, lottoNumbersGenerator);
+
+        lottoController.run();
+    }
+}
