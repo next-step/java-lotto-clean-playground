a5022ae530514acb21b7e0fdccb65b364f07d993
htdufhc-bob <htdufhc@gmail.com>
2026-03-30 16:36:37 +0900

    feat: 에러 메시지 관련 enum 추가

---
+++ src/main/java/constant/ErrorMessage.java
@@ -0,0 +1,20 @@
+package constant;
+
+public enum ErrorMessage {
+    INVALID_INPUT_FORMAT("정수만 입력할 수 있습니다."),
+    INVALID_LOTTO_COUNT("로또 번호는 6개여야 합니다."),
+    INVALID_LOTTO_RANGE("로또 번호는 1부터 45 사이만 입력 가능해야 합니다."),
+    INVALID_DUPLICATE_NUMBER("로또 번호에 중복된 숫자가 있습니다."),
+    INVALID_PURCHASE_UNIT("로또는 천 원 단위로 구매 가능합니다."),
+    INVALID_PURCHASE_AMOUNT("로또는 최소 구매 금액은 1000원입니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
