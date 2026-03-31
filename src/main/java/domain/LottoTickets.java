5e9291d5a84813cb5c692b75f7d2592761b47559
hapdaypy <mgim9316@gmail.com>
2026-03-31 14:25:26 +0900

    refactor: 로또 생성 기능 분리

--- src/main/java/domain/LottoTickets.java
+++ src/main/java/domain/LottoTickets.java
@@ -4,19 +4,13 @@ import java.util.Collections;
 import java.util.List;
 
 public class LottoTickets {
-
     private final List<Lotto> lottoTickets;
 
-    public LottoTickets(int trialCount, LottoNumberGenerator generator) {
-        List<Lotto> generatedLottoNumber = new ArrayList<>();
-        for (int i = 0; i < trialCount; i++) {
-            generatedLottoNumber.add(new Lotto(generator.generate()));
-        }
-        this.lottoTickets = generatedLottoNumber;
+    public LottoTickets(List<Lotto> lottoTickets) {
+        this.lottoTickets = lottoTickets;
     }
 
     public List<Lotto> getLottoNumber() {
         return Collections.unmodifiableList(lottoTickets);
     }
 }
-
