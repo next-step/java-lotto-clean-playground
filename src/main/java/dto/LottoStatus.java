4f94f9cee9a1a3049d1800bcf4c3087773ae19ce
htdufhc-bob <htdufhc@gmail.com>
2026-03-31 16:58:44 +0900

    refactor: LottoStatus DTO의 toString 메서드 오버라이딩
    - 출력 편의를 위해 로또 번호 리스트의 문자열 표현 형식으로 변경

--- src/main/java/dto/LottoStatus.java
+++ src/main/java/dto/LottoStatus.java
@@ -2,4 +2,13 @@ package dto;
 
 import java.util.List;
 
-public record LottoStatus(List<Integer> lottoNumbers) {}
+public record LottoStatus(List<Integer> lottoNumbers) {
+    public LottoStatus {
+        lottoNumbers = List.copyOf(lottoNumbers);
+    }
+
+    @Override
+    public String toString() {
+        return lottoNumbers.toString();
+    }
+}
