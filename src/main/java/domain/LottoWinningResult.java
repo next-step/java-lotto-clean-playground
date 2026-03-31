061e9badd071f73146b6c78fa82526f226737013
htdufhc-bob <htdufhc@gmail.com>
2026-03-31 16:57:19 +0900

    feat: 로또 당첨 통계 및 수익률 계산을 담당하는 LottoWinningResult 클래스 추가

---
+++ src/main/java/domain/LottoWinningResult.java
@@ -0,0 +1,29 @@
+package domain;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class LottoWinningResult {
+    private final Map<LottoRank, Integer> matchingCounts;
+
+    public LottoWinningResult(Map<LottoRank, Integer> matchingCounts) {
+        this.matchingCounts = matchingCounts;
+    }
+
+    public BigDecimal getLottoProfitRate(int purchaseAmount) {
+        BigDecimal totalProfit = matchingCounts.entrySet().stream()
+                .map(entry ->  BigDecimal.valueOf(entry.getKey().getPrice())
+                        .multiply(BigDecimal.valueOf(entry.getValue())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        return totalProfit.divide(BigDecimal.valueOf(1000L * purchaseAmount), 2, RoundingMode.HALF_UP);
+    }
+
+    public Map<LottoRank, Integer> getLottoStatistics() {
+        return matchingCounts.entrySet().stream()
+                .filter(entry -> entry.getKey() != LottoRank.MISS)
+                .collect(Collectors.toUnmodifiableMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+}
