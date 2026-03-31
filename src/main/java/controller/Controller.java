1bc083524e9946d0a15c0a4e476fae2de2212c9e
hapdaypy <mgim9316@gmail.com>
2026-03-31 14:18:25 +0900

    feat: 잘못 입력시 다시 숫자를 입력 받는 로직 추가

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -3,31 +3,49 @@ package controller;
 import domain.*;
 import view.OutputView;
 import view.InputView;
-import domain.Lotto;
 import java.util.List;
+import java.util.function.Supplier;
 
 public class Controller {
     public void run() {
-        // [1] 구입 금액을 입력 받는다.
-        OutputView.printInputPurchaseAmount();
-        int purchaseNumber = InputView.inputPurchaseMoney();
-        // [2] 구입 금액으로 시도 횟수 만들기
-        TrialNumber trialNumber = new TrialNumber(purchaseNumber);
-        // [3] 시도 횟수 만큼 랜덤 로또 생성;
-        LottoNumberGenerator generator = new RandomLottoNumberGenerator();
+        // [1], [2] 구입 금액 입력 및 시도 횟수 생성 (예외 발생 시 재입력)
+        TrialNumber trialNumber = retry(() -> {
+            OutputView.printInputPurchaseAmount();
+            int amount = InputView.inputPurchaseMoney();
+            return new TrialNumber(amount);
+        });
+
         int trialCount = trialNumber.getTrialNumber();
-        LottoTickets lottoNumber = new LottoTickets(trialCount, generator);
-        OutputView.printLottoNumber(lottoNumber,trialCount);
-        // [4] 지난주 당첨 번호 입력 받기
-        OutputView.printInputWinningNumber();
-        List<Integer> inputWinningNumbers = InputView.inputWinningNumber();
-        Lotto winningNumbers = new Lotto(inputWinningNumbers);
-        // [5] 당첨번호와 로또 번호 비교해서 결과 탐색하기
-        LottoResult statisticsResult = new LottoResult(lottoNumber, winningNumbers);
+
+        // [3] 로또 머신을 통한 로또 발행 및 티켓 관리 객체 초기화
+        LottoMachine lottoMachine = new LottoMachine(new RandomLottoNumberGenerator());
+        List<Lotto> generatedLottos = lottoMachine.issue(trialCount);
+        LottoTickets lottoTickets = new LottoTickets(generatedLottos);
+
+        OutputView.printLottoNumber(lottoTickets, trialCount);
+
+        // [4] 지난주 당첨 번호 입력 받기 및 Lotto 객체로 포장 (타입 오류 수정, 재입력 적용)
+        Lotto winningLotto = retry(() -> {
+            OutputView.printInputWinningNumber();
+            List<Integer> inputWinningNumbers = InputView.inputWinningNumber();
+            return new Lotto(inputWinningNumbers);
+        });
+
+        // [5] 당첨 번호와 로또 번호 비교해서 결과 탐색하기
+        LottoResult statisticsResult = new LottoResult(lottoTickets, winningLotto);
+
         // [6] 결과 출력하기
         int purchaseAmount = trialNumber.getPurchaseAmount();
         OutputView.printWinningStatistics(statisticsResult, purchaseAmount);
     }
+
+    private <T> T retry(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 }
-
-
