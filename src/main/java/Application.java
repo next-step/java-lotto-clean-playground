e2246706b01e249e1c9a698d630d5460a207ff74
htdufhc-bob <htdufhc@gmail.com>
2026-03-31 21:04:08 +0900

    refactor: AppConfig 사용하도록 수정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,17 +1,10 @@
 import controller.LottoController;
-import controller.Validator;
-import util.LottoNumbersGenerator;
-import view.InputView;
-import view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        LottoNumbersGenerator lottoNumbersGenerator = new LottoNumbersGenerator();
-        Validator validator = new Validator();
-        LottoController lottoController = new LottoController(inputView, outputView, lottoNumbersGenerator, validator);
+        AppConfig appConfig = new AppConfig();
+        LottoController lottoController = appConfig.setLottoController();
 
         lottoController.run();
     }
