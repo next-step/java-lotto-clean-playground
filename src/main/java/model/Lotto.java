ab4816e13fb284bc77c62f8304f6d7b77a011d93
이태규 <58386574+Cappucciyes@users.noreply.github.com>
2026-04-01 01:23:30 +0900

    refactor: removed Lotto.clone

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -13,8 +13,4 @@ public class Lotto {
     public List<Integer> getNumbers() {
         return List.copyOf(this.numbers);
     }
-
-    public Lotto clone() {
-        return new Lotto(this.getNumbers());
-    }
 }
