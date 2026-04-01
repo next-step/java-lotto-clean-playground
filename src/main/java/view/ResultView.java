d7805a288d91cee1219f07a3aac1d3406b0bd30e
mikeylili <a01073111145@gmail.com>
2026-04-01 16:52:23 +0900

    feat: 2단계 진행을 위해 로또 당첨을 통한 수익률 출력 로직을 추가함.

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -11,4 +11,18 @@ public class ResultView {
             System.out.println(numbers);
 
     }
+    public void printWinningStatics(int num, int price, int count){
+        System.out.println(num + "개 일치 (" + price + ")- " + count + "개");
+    }
+    public void printYield(double yield, boolean sign){
+        System.out.print("총 수익률은 " + yield + "입니다.");
+        if (sign){
+            System.out.println("(기준이 1이기 때문에 결과적으로 이득이라 더 뽑아도 된다는 의미임)");
+        }
+        if (!sign){
+            System.out.println("(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
+        }
+
+    }
+
 }
