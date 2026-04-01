021a3f1d61275630f9f0144b12ec4899bcfaeb00
mikeylili <a01073111145@gmail.com>
2026-04-01 16:54:11 +0900

    feat: 입력받은 로또번호들을 Lotto자료형으로 저장하는 로직을 추가하였고, 책임을 적절히 분배하기 위해 이 과정을 두개의 메서드로 분리함.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,10 @@
 package view;
 
+import domain.Lotto;
+import domain.LottoNumber;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -9,4 +14,22 @@ public class InputView {
         System.out.println("구입금액을 입력해 주세요.");
         return scanner.nextInt();
     }
+
+    public Lotto getWinnerNumbers(){
+        System.out.println("지난 주 당첨번호를 입력해 주세요.");
+        String input = scanner.nextLine();
+        return parseToLotto(input);
+    }
+
+    public Lotto parseToLotto(String input){
+       String[] tokens = input.split(", ");
+        List<LottoNumber> winningNumbers = new ArrayList<>();
+        for (String token : tokens){
+            winningNumbers.add(new LottoNumber(Integer.parseInt(token)));
+        }
+
+        return new Lotto(winningNumbers);
+    }
+
+
 }
