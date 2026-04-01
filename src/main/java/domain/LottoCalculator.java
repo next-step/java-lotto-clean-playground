2f79b401f8aac674048b1ba88cbddf83bb99d3cd
mikeylili <a01073111145@gmail.com>
2026-04-01 16:56:14 +0900

    feat: 수익률 계산을 Map을 이용하여 저장한 정보를 바탕으로 효과적으로 진행함. 이때 Map의 Key는 Rank를 넣어서 한번에 세개의 정보(matchNumbers,prizeMoney,등수별 당첨횟수)를 저장하여 계산함.

---
+++ src/main/java/domain/LottoCalculator.java
@@ -0,0 +1,30 @@
+package domain;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LottoCalculator {
+    private final Map<Rank, Integer>  result;
+
+    public LottoCalculator(){
+       result = new LinkedHashMap<>() ;
+       for(Rank rank : Rank.values()){
+           result.put(rank , 0);
+       }
+    }
+    public void valueAdd(Rank rank){
+        result.put(rank, result.get(rank)+1);
+    }
+
+    public double calculateYield(Money money){
+        double totalPrize = 0;
+        for (Rank rank : Rank.values()){
+            totalPrize += rank.getPrizemoney() * result.get(rank);
+        }
+        return totalPrize / money.getAmount() ;
+    }
+
+    public Map<Rank, Integer> getResult() {
+        return result;
+    }
+}
