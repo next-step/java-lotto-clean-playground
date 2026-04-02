a98be00e072adf3440511d56aab91ed28403752f
dahyn <k0d5h16@gmail.com>
2026-04-03 01:14:46 +0900

    test: LottoShopTest 수정

--- src/test/java/domain/LottoShopTest.java
+++ src/test/java/domain/LottoShopTest.java
@@ -11,14 +11,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 class LottoShopTest {
 
     @Test
-    void 구입금액만큼_로또를_구매한다() {
+    void 구입금액만큼_로또를_생성한다() {
         NumberGenerator numberGenerator = new TestNumberGenerator(List.of(
-                new LottoNumber(1),
-                new LottoNumber(2),
-                new LottoNumber(3),
-                new LottoNumber(4),
-                new LottoNumber(5),
-                new LottoNumber(6)
+                new LottoNumber(1), new LottoNumber(2),
+                new LottoNumber(3), new LottoNumber(4),
+                new LottoNumber(5), new LottoNumber(6)
+        ));
+        LottoShop lottoShop = new LottoShop(numberGenerator);
+
+        Lottos lottos = lottoShop.purchase(new PurchaseAmount(3000));
+
+        assertThat(lottos.toNumberLists()).hasSize(3);
+    }
+
+    @Test
+    void 생성된_번호로_로또가_구성된다() {
+        NumberGenerator numberGenerator = new TestNumberGenerator(List.of(
+                new LottoNumber(1), new LottoNumber(2),
+                new LottoNumber(3), new LottoNumber(4),
+                new LottoNumber(5), new LottoNumber(6)
         ));
         LottoShop lottoShop = new LottoShop(numberGenerator);
 
