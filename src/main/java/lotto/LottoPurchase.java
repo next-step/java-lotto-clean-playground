f73ff0c191e06559099298ef72054d508773b3df
lhwdev <lhwdev6@outlook.com>
2026-04-02 12:15:31 +0900

    feat: 로또 구매정보, 구매로직 구현

---
+++ src/main/java/lotto/LottoPurchase.java
@@ -0,0 +1,28 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoPurchase {
+    private static final int LOTTO_PRICE = 1000;
+
+    private final List<Lotto> lottoRows = new ArrayList<>();
+    private final int change;
+
+    public LottoPurchase(int totalPrice, LottoMaker lottoMaker) {
+        int numberOfLotto = totalPrice / LOTTO_PRICE;
+        change = totalPrice % LOTTO_PRICE;
+
+        for (int n = 0; n < numberOfLotto; n++) {
+            lottoRows.add(lottoMaker.makeLotto());
+        }
+    }
+
+    public LottoReceipt printReceipt() {
+        return new LottoReceipt(lottoRows);
+    }
+
+    public int getChange() {
+        return change;
+    }
+}
