18dffd6e137b75597ca53ce58c1d77398fbc85bb
PARK JUNSEONG <jun0829park@gmail.com>
2026-04-04 01:17:58 +0900

    feat: valueOf 메서드에 스태틱을 추가하여 객체 생성 없이 공용으로 사용할 수 있게 함.

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -1,27 +1,26 @@
 package domain;
 
 public enum Rank {
-    SIX(6,2000000000),
-    FIVE(5,1500000),
-    FOUR(4,50000),
-    THREE(3,5000),
-    MISS(0,0);
+    SIX(6, 2000000000),
+    FIVE(5, 1500000),
+    FOUR(4, 50000),
+    THREE(3, 5000),
+    MISS(0, 0);
 
     private int matchnumbers;
     private int prizemoney;
 
-    Rank (Integer matchnumbers, Integer prizemoney){
+    Rank(int matchnumbers, int prizemoney) {
         this.matchnumbers = matchnumbers;
         this.prizemoney = prizemoney;
     }
 
-    public Rank valueOf(Integer matchnumbers){
+    public static Rank valueOf(int matchnumbers) {
         for (Rank rank : values()) {
-            if (rank.matchnumbers == matchnumbers){
+            if (rank.matchnumbers == matchnumbers) {
                 return rank;
             }
         }
-
         return MISS;
     }
 
@@ -33,4 +32,3 @@ public enum Rank {
         return prizemoney;
     }
 }
-
