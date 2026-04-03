9b880d8468f5e81df034524f47884f04c0b48ea9
hapdaypy <mgim9316@gmail.com>
2026-04-03 10:25:19 +0900

    refactor:  given 주석 추가

--- src/test/java/domain/LottoMachineTest.java
+++ src/test/java/domain/LottoMachineTest.java
@@ -1,25 +1,24 @@
-package domain;
+        package domain;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+        import org.junit.jupiter.api.DisplayName;
+        import org.junit.jupiter.api.Test;
 
-import java.util.List;
+        import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+        import static org.assertj.core.api.Assertions.assertThat;
+        class LottoMachineTest {
 
-class LottoMachineTest {
-
-    @DisplayName("요청한 시도 횟수만큼 로또를 발행한다.")
-    @Test
-    void issueLottos() {
-
-        LottoNumberGenerator stubGenerator = () -> List.of(1, 2, 3, 4, 5, 6);
-        LottoMachine lottoMachine = new LottoMachine(stubGenerator);
-        int trialCount = 5;
-        // when
-        List<Lotto> lottos = lottoMachine.issue(trialCount);
-        // then
-        assertThat(lottos).hasSize(trialCount);
-        assertThat(lottos.get(0).getNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
-    }
-}
+            @DisplayName("요청한 시도 횟수만큼 로또를 발행한다.")
+            @Test
+            void issueLottos() {
+                // given
+                LottoNumberGenerator stubGenerator = () -> List.of(1, 2, 3, 4, 5, 6);
+                LottoMachine lottoMachine = new LottoMachine(stubGenerator);
+                int trialCount = 5;
+                // when
+                List<Lotto> lottos = lottoMachine.issue(trialCount);
+                // then
+                assertThat(lottos).hasSize(trialCount);
+                assertThat(lottos.get(0).getNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
+            }
+        }
