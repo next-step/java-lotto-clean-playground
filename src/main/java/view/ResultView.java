781b91d51ce12de042eb5e685ec6ae0e662316b0
PARK JUNSEONG <jun0829park@gmail.com>
2026-04-04 01:13:34 +0900

    feat: ResultView: Lotto 객체 대신 원시값 리스트(List<Integer>)만 받아 출력하도록 수정.

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -1,14 +1,14 @@
 package view;
 
-import domain.Lotto;
+import java.util.List;
 
 public class ResultView {
     public void printPurchaseCount(int count) {
         System.out.println("\n" + count + "개를 구매했습니다.");
     }
 
-    public void printLottoNumbers(Lotto numbers) {
-        System.out.println(numbers.getLotto());
+    public void printLottoNumbers(List<Integer> numbers) {
+        System.out.println(numbers);
     }
 
     public void printStatics() {
@@ -16,18 +16,15 @@ public class ResultView {
     }
 
     public void printWinningStatics(int num, int price, int count) {
-        System.out.println(num + "개 일치 (" + price + ")- " + count + "개");
+        System.out.println(num + "개 일치 (" + price + "원)- " + count + "개");
     }
 
     public void printYield(double yield, boolean sign) {
         System.out.printf("총 수익률은 %.2f입니다.", yield);
         if (sign) {
             System.out.println("(기준이 1이기 때문에 결과적으로 이득이라 더 뽑아도 된다는 의미임)");
-        }
-        if (!sign) {
+        } else {
             System.out.println("(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
         }
-
     }
-
 }
