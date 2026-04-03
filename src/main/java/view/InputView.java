bcbc789523af0a61d6d1328accca0c2f1f3de713
PARK JUNSEONG <jun0829park@gmail.com>
2026-04-04 01:13:15 +0900

    feat: Lotto 객체를 직접 생성하던 parseToLotto를 지우고, 순수하게 텍스트만 넘기도록 수정.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,9 +1,5 @@
 package view;
 
-import domain.Lotto;
-import domain.LottoNumber;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -15,37 +11,15 @@ public class InputView {
         if (input.isBlank()) {
             throw new IllegalArgumentException("구입금액이 비어있습니다.");
         }
-
         try {
-            return Integer.parseInt(input);
+            return Integer.parseInt(input.trim());
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("구입금액은 숫자여야 합니다.");
         }
     }
 
-    public Lotto getWinnerNumbers() {
+    public String getWinnerNumbers() {
         System.out.println("\n지난 주 당첨번호를 입력해 주세요.");
-        String input = scanner.nextLine();
-        return parseToLotto(input);
+        return scanner.nextLine();
     }
-
-    public Lotto parseToLotto(String input) {
-        if (input == null || input.isBlank()) {
-            throw new IllegalArgumentException("입력값이 비어 있습니다.");
-        }
-        String[] tokens = input.split(", ");
-        List<LottoNumber> winningNumbers = new ArrayList<>();
-        try {
-            for (String token : tokens) {
-                int number = Integer.parseInt(token.trim());
-                winningNumbers.add(new LottoNumber(number));
-            }
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("로또 번호는 숫자여야 하며, 구분자는 ', '여야 합니다.");
-        }
-
-        return new Lotto(winningNumbers);
-    }
-
-
 }
