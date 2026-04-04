222ff32f56fbaf7c8e93bee2819ebd1c8244543b
leechan7120 <chanhyoung7120@naver.com>
2026-04-04 17:09:52 +0900

    fix : 숫자 변경

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -2,10 +2,10 @@ package lotto;
 
 public enum LottoResult {
     NONE(0),
-    THREE(5000),
-    FOUR(5_0000),
-    FIVE(150_0000),
-    SIX(20_0000_0000);
+    THREE(5_000),
+    FOUR(50_000),
+    FIVE(1_500_000),
+    SIX(2_000_000_000);
 
     public final int reward;
 
