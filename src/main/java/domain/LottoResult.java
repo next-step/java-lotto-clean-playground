9ef612d01f95c9999dd8a2ebb96cdfe6200a0791
monsaumon <monsaumon@naver.com>
2026-04-05 01:17:43 +0900

    refactor: 불필요한 검증 제거

--- src/main/java/domain/LottoResult.java
+++ src/main/java/domain/LottoResult.java
@@ -15,17 +15,13 @@ public class LottoResult {
         }
     }
 
-    public Count getMatchCount(LottoRank rank) {
-        Count matchCount = ticketCountOfEachRank.get(rank);
-        if (matchCount == null) {
-            throw new IllegalArgumentException("존재하지 않는 등수입니다.");
-        }
-        return matchCount;
-    }
-
     private void validate(List<Count> matchingTicketCounts) {
         if (matchingTicketCounts.size() != LottoRank.values().length) {
             throw new IllegalArgumentException("등수 종류의 수가 올바르지 않습니다.");
         }
+    }
+
+    public Count getMatchCount(LottoRank rank) {
+        return ticketCountOfEachRank.get(rank);
     }
 }
