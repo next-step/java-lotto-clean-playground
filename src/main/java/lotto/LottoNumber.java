7502034ae3656915c83a821a629a71a7026a4689
leechan7120 <chanhyoung7120@naver.com>
2026-04-04 17:17:53 +0900

    fix : private 변경

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -1,8 +1,8 @@
 package lotto;
 
 public record LottoNumber(int number) implements Comparable<LottoNumber> {
-    public static int MIN_NUMBER = 1;
-    public static int MAX_NUMBER = 45;
+    private static int MIN_NUMBER = 1;
+    private static int MAX_NUMBER = 45;
 
     public LottoNumber {
         if (MIN_NUMBER > number) {
