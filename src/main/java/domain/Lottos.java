f90dfe225038fbc75867f2a35db3e0025daa99fc
jxinwoo <12jack030220@gmail.com>
2026-04-05 22:09:10 +0900

    refactor: 로또 번호 (1~45) 캐싱하여 재활용

--- src/main/java/domain/Lottos.java
+++ src/main/java/domain/Lottos.java
@@ -8,14 +8,18 @@ public class Lottos {
 
     private static final int PRICE = 1000;
     private static final int LOTTO_UPPER_BOUND = 45;
+    private static final List<Integer> LOTTO_NUMBERS_CACHE = new ArrayList<>();
+
     private final List<Lotto> lottos;
+
+    static {
+        for (int i = 0; i < LOTTO_UPPER_BOUND; i++) {
+            LOTTO_NUMBERS_CACHE.add(i, i + 1);
+        }
+    }
 
     public Lottos(final int purchaseAmount) {
         this.lottos = generateLottos(purchaseAmount / PRICE);
-    }
-
-    public List<Lotto> getLottos() {
-        return lottos;
     }
 
     private List<Lotto> generateLottos(int lottoCount) {
@@ -28,18 +32,14 @@ public class Lottos {
     }
 
     private Lotto getSingleLotto() {
-        ArrayList<Integer> lottoNumbers = generateLottoNumbersArray();
+        List<Integer> lottoNumbers = new ArrayList<>(LOTTO_NUMBERS_CACHE);
         Collections.shuffle(lottoNumbers);
-        List<Integer> subNumbers = lottoNumbers.subList(0, 6);
+        List<Integer> subNumbers = new ArrayList<>(lottoNumbers.subList(0, 6));
         Collections.sort(subNumbers);
         return new Lotto(subNumbers);
     }
 
-    private ArrayList<Integer> generateLottoNumbersArray() {
-        ArrayList<Integer> lottoNumbers = new ArrayList<>(LOTTO_UPPER_BOUND);
-        for (int i = 0; i < LOTTO_UPPER_BOUND; i++) {
-            lottoNumbers.add(i, i + 1);
-        }
-        return lottoNumbers;
+    public List<Lotto> getLottos() {
+        return lottos;
     }
 }
