b7b010ede14ea82ee8fe79b606153241b817db1b
mikeylili <a01073111145@gmail.com>
2026-04-05 22:11:43 +0900

    feat: 접근제어자 작성, 인덴트 1개 이하로 줄이기

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,6 @@
 import domain.Lotto;
 import domain.LottoCalculator;
+import domain.LottoMachine;
 import domain.Lottos;
 import domain.Money;
 import domain.RandomLottoGenerator;
@@ -10,20 +11,19 @@ import view.InputView;
 import view.ResultView;
 
 public class Application {
-    InputView inputView = new InputView();
-    ResultView resultView = new ResultView();
-    RandomLottoGenerator random = new RandomLottoGenerator();
+    private final InputView inputView = new InputView();
+    private final ResultView resultView = new ResultView();
+    private final RandomLottoGenerator random = new RandomLottoGenerator();
 
     public void run() {
         final int amount = inputView.getMoney(); //돈 받기
         Money money = new Money(amount); // 돈 저장
-        final int number = money.getNumber(); // 로또 뽑는 횟수
+        final int number = LottoMachine.calculateTicketCount(money); // 로또 뽑는 횟수
         resultView.printPurchaseCount(number);
         Lottos lottos = purchaseLotto(number);
         Lotto winnerNumbers = inputView.getWinnerNumbers(); //당첨번호 로또 입력
         LottoCalculator calculator = calculatorResult(lottos, winnerNumbers); //당첨 결과 계산
-        printStatistics(calculator, money);
-        //최종 통계 및 수익률 출력
+        printStatistics(calculator, money);//최종 통계 및 수익률 출력
     }
 
 
@@ -54,22 +54,26 @@ public class Application {
     public void printStatistics(LottoCalculator calculator, Money money) {
         resultView.printStatics();
         for (Rank rank : Rank.values()) {
-            if (rank != Rank.MISS) {
-                resultView.printWinningStatics(
-                        rank.getMatchnumbers(),
-                        rank.getPrizemoney(),
-                        calculator.getResult().get(rank)
-                );
-            }
+            AddWinningMoney(calculator, rank);
         }
         double yield = calculator.calculateYield(money);
         resultView.printYield(yield, yield >= 1.0);
     }
 
+    public void AddWinningMoney(LottoCalculator calculator, Rank rank) {
+        if (rank != Rank.MISS) {
+            resultView.printWinningStatics(
+                    rank.getMatchnumbers(),
+                    rank.getPrizemoney(),
+                    calculator.getResult().get(rank)
+            );
+        }
+    }
+
     public static void main(String[] args) {
         Application lotto = new Application();
         lotto.run();
-    }
 
+    }
 
 }
