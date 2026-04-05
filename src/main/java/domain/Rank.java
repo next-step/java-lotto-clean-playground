e4e62f4a065eb0446732ab3d20878dccd1f20ad8
mikeylili <a01073111145@gmail.com>
2026-04-05 22:23:30 +0900

    feat: 접근제어자 수정

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -1,23 +1,23 @@
 package domain;
 
 public enum Rank {
-    SIX(6,2000000000),
-    FIVE(5,1500000),
-    FOUR(4,50000),
-    THREE(3,5000),
-    MISS(0,0);
+    SIX(6, 2000000000),
+    FIVE(5, 1500000),
+    FOUR(4, 50000),
+    THREE(3, 5000),
+    MISS(0, 0);
 
-    private int matchnumbers;
-    private int prizemoney;
+    private final int matchnumbers;
+    private final int prizemoney;
 
-    Rank (Integer matchnumbers, Integer prizemoney){
+    Rank(Integer matchnumbers, Integer prizemoney) {
         this.matchnumbers = matchnumbers;
         this.prizemoney = prizemoney;
     }
 
-    public Rank valueOf(Integer matchnumbers){
+    public Rank valueOf(Integer matchnumbers) {
         for (Rank rank : values()) {
-            if (rank.matchnumbers == matchnumbers){
+            if (rank.matchnumbers == matchnumbers) {
                 return rank;
             }
         }
