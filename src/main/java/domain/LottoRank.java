8f23ce06b509f5484a957513b1aff74524a54394
htdufhc-bob <htdufhc@gmail.com>
2026-04-05 22:38:11 +0900

    feat: 로또 2등(보너스 번호 일치) 등수 및 판단 로직 추가

--- src/main/java/domain/LottoRank.java
+++ src/main/java/domain/LottoRank.java
@@ -3,21 +3,27 @@ package domain;
 import java.util.Arrays;
 
 public enum LottoRank {
-    THREE_MATCHED(3, 5_000),
-    FOUR_MATCHED(4, 50_000),
-    FIVE_MATCHED(5, 1_500_00),
-    SIX_MATCHED(6, 2_000_000_000),
-    MISS(0, 0);
+    THREE_MATCHED(3, 5_000, false),
+    FOUR_MATCHED(4, 50_000, false),
+    FIVE_MATCHED(5, 1_500_00, false),
+    BONUS_FIVE_MATCHED(5, 30_000_000, true),
+    SIX_MATCHED(6, 2_000_000_000, false),
+    MISS(0, 0, false);
 
     private final int matchCount;
-    private final int price;
+    private final long price;
+    private final boolean matchBonus;
 
-    LottoRank(int matchCount, int price) {
+    LottoRank(int matchCount, int price, boolean matchBonus) {
         this.matchCount = matchCount;
         this.price = price;
+        this.matchBonus = matchBonus;
     }
 
-    public static LottoRank getLottoRank(int count) {
+    public static LottoRank getLottoRank(int count, boolean matchBonus) {
+        if (count == BONUS_FIVE_MATCHED.matchCount && matchBonus) {
+            return BONUS_FIVE_MATCHED;
+        }
         return Arrays.stream(LottoRank.values())
                 .filter(rank -> rank.matchCount == count)
                 .findFirst()
@@ -28,7 +34,11 @@ public enum LottoRank {
         return matchCount;
     }
 
-    public int getPrice() {
+    public long getPrice() {
         return price;
+    }
+
+    public boolean isMatchBonus() {
+        return matchBonus;
     }
 }
