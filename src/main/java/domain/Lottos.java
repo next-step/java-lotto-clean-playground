81325e8e660ef94946f266e26022bb51d83e75c3
htdufhc-bob <htdufhc@gmail.com>
2026-04-05 22:40:23 +0900

    refactor: Lottos가 스스로 당첨 통계(LottoStatistics)를 생성하도록 개선

--- src/main/java/domain/Lottos.java
+++ src/main/java/domain/Lottos.java
@@ -1,7 +1,10 @@
 package domain;
 
+import dto.LottoStatistics;
 import dto.LottoStatus;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -14,22 +17,11 @@ public class Lottos {
         this.lottos = List.copyOf(lottoList);
     }
 
-    public Map<LottoRank, Integer> calculateMatchCounts(List<LottoNumber> winningNumbers) {
-        Map<LottoRank, Integer> matchingCounts = new LinkedHashMap<>();
+    public LottoStatistics getLottoStatistics(Lotto winningLotto, LottoNumber lottoNumber) {
+        Map<LottoRank, Integer> matchingCounts = calculateMatchedCounts(winningLotto, lottoNumber);
+        BigDecimal profitRate = calculateProfitRate(matchingCounts);
 
-        Arrays.stream(LottoRank.values())
-                .forEach(rank -> matchingCounts.put(rank, 0));
-
-        for (Lotto lotto : lottos) {
-            int count = lotto.countMatchingNumbers(winningNumbers);
-            updateCount(matchingCounts, count);
-        }
-
-        return Map.copyOf(matchingCounts);
-    }
-
-    public int getQuantity() {
-        return lottos.size();
+        return new LottoStatistics(matchingCounts, profitRate);
     }
 
     public List<LottoStatus> toStatus() {
@@ -38,8 +30,31 @@ public class Lottos {
                 .toList();
     }
 
-    private void updateCount(Map<LottoRank, Integer> matchingCounts, int count) {
-        LottoRank rank = LottoRank.getLottoRank(count);
-        matchingCounts.put(rank, matchingCounts.get(rank) + 1);
+    private Map<LottoRank, Integer> calculateMatchedCounts(Lotto winningLotto, LottoNumber bonusNumber) {
+        Map<LottoRank, Integer> matchedCounts = new LinkedHashMap<>();
+        Arrays.stream(LottoRank.values())
+                .forEach(rank -> matchedCounts.put(rank, 0));
+        lottos.forEach(lotto -> {
+            int count = lotto.countMatchingNumbers(winningLotto);
+            boolean hasBonus = lotto.contains(bonusNumber);
+            updateCount(matchedCounts, count, hasBonus);
+        });
+
+        return matchedCounts;
+    }
+
+    private BigDecimal calculateProfitRate(Map<LottoRank, Integer> matchedCounts) {
+        long totalPrize = matchedCounts.entrySet().stream()
+                .mapToLong(entry -> entry.getKey().getPrice() * entry.getValue())
+                .sum();
+        BigDecimal totalProfit = BigDecimal.valueOf(totalPrize);
+        BigDecimal purchasedPrice = BigDecimal.valueOf(lottos.size() * 1000L);
+
+        return totalProfit.divide(purchasedPrice, 2, RoundingMode.HALF_UP);
+    }
+
+    private void updateCount(Map<LottoRank, Integer> matchedCounts, int count, boolean hasBonus) {
+        LottoRank rank = LottoRank.getLottoRank(count, hasBonus);
+        matchedCounts.put(rank, matchedCounts.get(rank) + 1);
     }
 }
