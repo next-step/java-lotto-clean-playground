ed4b5059ec8f48aebcc20b38e452196b6b1ab11b
mikeylili <a01073111145@gmail.com>
2026-04-05 22:15:04 +0900

    feat: 메서드명 수정

--- src/main/java/domain/RandomLottoGenerator.java
+++ src/main/java/domain/RandomLottoGenerator.java
@@ -5,35 +5,32 @@ import java.util.Collections;
 import java.util.List;
 
 public class RandomLottoGenerator {
-    private static final int MAX_NUMBER=45;
-    private static final int MIN_NUMBER=1;
-    private static final int LOTTO_SIZE=6;
 
     private final List<Integer> seedNumbers;
 
-    public RandomLottoGenerator(){
+    public RandomLottoGenerator() {
         this.seedNumbers = initializeNumbers();
     }
 
-    private List<Integer> initializeNumbers(){
-        List<Integer>  numbers = new ArrayList<>();
-        for(int i = MIN_NUMBER;i<=MAX_NUMBER;i++){
+    private List<Integer> initializeNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = LottoNumber.MIN_NUMBER; i <= LottoNumber.MAX_NUMBER; i++) {
             numbers.add(i);
         }
         return numbers;
     }
 
-    public Lotto generate(){
+    public Lotto generate() {
         Collections.shuffle(seedNumbers);
-        List<Integer> LottoNumbers = new ArrayList<>(seedNumbers.subList(0,LOTTO_SIZE));
+        List<Integer> LottoNumbers = new ArrayList<>(seedNumbers.subList(0, Lotto.LOTTO_NUMBER_COUNT));
         Collections.sort(LottoNumbers);
 
-        return parseLN(LottoNumbers);
+        return parseLottoNumber(LottoNumbers);
     }
 
-    public Lotto parseLN(List<Integer> LottoNumbers){
+    public Lotto parseLottoNumber(List<Integer> LottoNumbers) {
         List<LottoNumber> lottoNumbers = new ArrayList<>();
-        for(Integer number : LottoNumbers){
+        for (Integer number : LottoNumbers) {
             lottoNumbers.add(new LottoNumber(number));
         }
 
