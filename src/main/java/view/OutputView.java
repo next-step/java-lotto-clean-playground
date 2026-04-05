e035a173d78921e21c6ba7abd390356ef96aded7
htdufhc-bob <htdufhc@gmail.com>
2026-04-05 22:49:51 +0900

    feat: 수동 구매 안내 및 보너스 번호 입력 UI 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,15 +1,26 @@
 package view;
 
+import domain.LottoRank;
+import dto.LottoStatistics;
+
 import java.math.BigDecimal;
 import java.util.List;
 
 public class OutputView {
-    public void printStartGuide() {
+    public void printInputStartGuide() {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    public void printQuantity(int num) {
-        System.out.println("\n" + num + "개를 구매했습니다.");
+    public void printInputManualCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해주세요.");
+    }
+
+    public void printInputManualNumbers() {
+        System.out.println("수동으로 구매할 번호를 입력해주세요.");
+    }
+
+    public void printQuantity(int num1, int num2) {
+        System.out.printf("\n수동으로 %d장, 자동으로 %d개를 구매했습니다.%n", num1, num2);
     }
 
     public void printElements(List<String> elements) {
@@ -20,13 +31,19 @@ public class OutputView {
         System.out.println("\n지난 주 당첨 번호를 입력해 주세요.");
     }
 
+    public void printBonusPrompt() {
+        System.out.println("\n보너스 볼을 입력해 주세요.");
+    }
+
     public void printStatisticHeader() {
         System.out.println("\n당첨 통계");
         System.out.println("---");
     }
 
-    public void printStatistics(int num1, int num2, int num3) {
-        System.out.printf("%d개 일치 (%d원)- %d개\n", num1, num2, num3);
+    public void printStatistics(LottoStatistics lottoStatistics) {
+        lottoStatistics.matchCount().entrySet().stream()
+                .filter(entry -> entry.getKey() != LottoRank.MISS)
+                .forEach(entry -> printMatchedResult(entry.getKey(), entry.getValue()));
     }
 
     public void printResult(BigDecimal value) {
@@ -40,5 +57,14 @@ public class OutputView {
 
     public void printErrorMessage(String errorMessage) {
         System.out.println("[ERROR] " + errorMessage);
+    }
+
+    private void printMatchedResult(LottoRank lottoRank, int matchedCount) {
+        String result = String.format("%d개 일치", lottoRank.getMatchCount());
+        if (lottoRank.isMatchBonus()) {
+            result += ", 보너스 볼 일치";
+        }
+        result += String.format("(%d원) - %d개", lottoRank.getPrice(), matchedCount);
+        System.out.println(result);
     }
 }
