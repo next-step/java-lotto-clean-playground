2a6242044b2d9ef9fa1c2eeeae154d3d08bba0cf
hapdaypy <mgim9316@gmail.com>
2026-04-06 00:15:44 +0900

    refactor: 값을 하드코딩하지 않는다.

--- src/main/java/domain/TrialNumber.java
+++ src/main/java/domain/TrialNumber.java
@@ -2,7 +2,7 @@ package domain;
 
 public class TrialNumber {
     private static final int LOTTO_PRICE = 1000;
-
+    private static final int ZERO = 0;
     private final int trialCount;
     private final int purchaseAmount;
 
@@ -14,10 +14,10 @@ public class TrialNumber {
     }
 
     private void validateAmount(int amount) {
-        if (amount <= 0) {
+        if (amount <= ZERO) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 0보다 커야 합니다.");
         }
-        if (amount % LOTTO_PRICE != 0) {
+        if (amount % LOTTO_PRICE != ZERO) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
         }
     }
