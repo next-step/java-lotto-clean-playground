e94095195c3f2661db5d2a1bed265a73990d937d
jxinwoo <12jack030220@gmail.com>
2026-04-05 21:32:34 +0900

    refactor: getMatchResult메소드 간결화

--- src/main/java/domain/WinningLotto.java
+++ src/main/java/domain/WinningLotto.java
@@ -7,14 +7,9 @@ import java.util.List;
 public class WinningLotto {
 
     public HashMap<MatchResult, Integer> getMatchResult(List<Lotto> allLottos, List<Integer> winningNumbers) {
-
         HashMap<MatchResult, Integer> resultMap = new HashMap<>();
 
-        for(MatchResult result: MatchResult.values()) {
-            resultMap.put(result, 0);
-        }
-
-        for(Lotto singleLotto: allLottos) {
+        for (Lotto singleLotto : allLottos) {
             int singleEqualCount = 0;
             for (Integer number : winningNumbers) {
                 if (singleLotto.getNumbers().contains(number)) {
@@ -22,9 +17,9 @@ public class WinningLotto {
                 }
             }
 
-            for(MatchResult result: MatchResult.values()) {
-                if(singleEqualCount == result.getMatchCount()) {
-                    resultMap.put(result, resultMap.get(result) + 1);
+            for (MatchResult result : MatchResult.values()) {
+                if (singleEqualCount == result.getMatchCount()) {
+                    resultMap.put(result, resultMap.getOrDefault(result, 0) + 1);
                 }
             }
         }
@@ -32,10 +27,10 @@ public class WinningLotto {
         return resultMap;
     }
 
-    public double getLottoProfitRate (HashMap<MatchResult, Integer> resultMap, int purchaseAmount) {
+    public double getLottoProfitRate(HashMap<MatchResult, Integer> resultMap, int purchaseAmount) {
         double totalReward = 0;
 
-        for(MatchResult result: MatchResult.values()) {
+        for (MatchResult result : MatchResult.values()) {
             totalReward += result.getMatchReward() * resultMap.get(result);
         }
 
