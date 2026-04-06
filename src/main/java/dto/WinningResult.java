6270d1282dea9dc12a3f2e0b67ed5496b25d3296
dahyn <k0d5h16@gmail.com>
2026-04-06 15:07:25 +0900

    refactor: WinningResult 구조 변경

--- src/main/java/dto/WinningResult.java
+++ src/main/java/dto/WinningResult.java
@@ -7,17 +7,17 @@ import java.util.Arrays;
 import java.util.List;
 
 public record WinningResult(String message, int count) {
-    public static WinningResult from(Rank rank, int count) {
+    public static List<WinningResult> from(WinningStatistics winningStatistics) {
+        return Arrays.stream(Rank.values())
+                .filter(Rank::isWinning)
+                .map(rank -> createMessage(rank, winningStatistics.countOf(rank)))
+                .toList();
+    }
+
+    private static WinningResult createMessage(Rank rank, int count) {
         if (rank == Rank.FIVE_BONUS_MATCH) {
             return new WinningResult("5개 일치, 보너스 볼 일치(" + rank.getPrizeMoney() + "원)", count);
         }
         return new WinningResult(rank.getMatchCount() + "개 일치 (" + rank.getPrizeMoney() + "원)", count);
-    }
-
-    public static List<WinningResult> from(WinningStatistics winningStatistics) {
-        return Arrays.stream(Rank.values())
-                .filter(Rank::isWinning)
-                .map(rank -> from(rank, winningStatistics.countOf(rank)))
-                .toList();
     }
 }
