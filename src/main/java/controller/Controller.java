63e613b7bbe9ada269ed4bace8b13df608b492e9
hapdaypy <mgim9316@gmail.com>
2026-04-06 11:42:09 +0900

    refactor: 로또 번호 validate 호출

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,12 +1,13 @@
 package controller;
 
-
 import domain.Lotto;
 import domain.LottoMachine;
 import domain.LottoTickets;
 import domain.TrialNumber;
 import domain.LottoResult;
 import domain.RandomLottoNumberGenerator;
+import domain.validator.BonusNumberValidator;
+import domain.validator.ManualCountValidator;
 
 import view.InputView;
 import view.OutputView;
@@ -22,23 +23,21 @@ public class Controller {
         // [1] 구입 금액을 입력 받는다.
         TrialNumber trialNumber = getTrialNumber();
         // [2] 수기로 입력할 로또 갯수를 입력 받는다.
-        int manualTrialCount = getManualTrialCount();
+        int manualTrialCount = getManualTrialCount(trialNumber.getTrialNumber());
         // [3] 수기로 입력 받을 로또 번호를 입력 받는다.
         LottoTickets manualTickets = getManualLottoTickets(manualTrialCount);
-
-        // [4] 자동으로 뽑을 로또 번호 = 총 구입 금액으로 만든 시도 회숫 - 수기로 입력 받을  로또 번호
+        // [4] 자동으로 생성할 로또 번호를 계산한다.
         int autoTrialCount = trialNumber.getTrialNumber() - manualTrialCount;
-        // [5] 자동으로 로또 번호를 입력 받는다.
+        // [5] 자동 로또 번호를 생성한다.
         LottoTickets autoTickets = issueLottoTickets(autoTrialCount, manualTrialCount);
-        // [6] 지난주 로또 번호를 입력 받는다.
+        // [6] 우승 로또 번호를 입력 받는다.
         Lotto winningLotto = getWinningLotto();
-        // [7] 보너스 번호를 입력 받는다.
+        // [7] 보너스 번호를 입력받는다.
         int bonusNumber = getBonusNumber(winningLotto);
-        // [8] 최종 결과를 계산한다.
+        // [8] 로또 결과를 계산한다.
         LottoResult statisticsResult = calculateAndPrintResults(autoTickets, winningLotto, bonusNumber, manualTickets);
         // [9] 최종 결과를 출력한다.
         OutputView.printWinningStatistics(statisticsResult, trialNumber.getPurchaseAmount());
-
     }
 
     private TrialNumber getTrialNumber() {
@@ -48,14 +47,19 @@ public class Controller {
         });
     }
 
-    private int getManualTrialCount() {
+    private int getManualTrialCount(int totalTrialCount) {
         return retry(() -> {
             OutputView.printManualTrialCount();
-            return InputView.inputManualLottoNumberTrialCount();
+            int manualCount = InputView.inputManualLottoNumberTrialCount();
+            ManualCountValidator.validate(totalTrialCount, manualCount);
+            return manualCount;
         });
     }
 
     private LottoTickets getManualLottoTickets(int trialCount) {
+        if (trialCount == 0) {
+            return new LottoTickets(List.of());
+        }
         return retry(() -> {
             OutputView.printManualLottoTickets();
             List<Lotto> manualLottos = IntStream.range(0, trialCount)
@@ -85,23 +89,13 @@ public class Controller {
         return retry(() -> {
             OutputView.printBonusNumber();
             int bonusNumber = InputView.inputBonusNumber();
-            validateBonusNumber(winningLotto, bonusNumber);
+            BonusNumberValidator.validate(winningLotto, bonusNumber);
             return bonusNumber;
         });
     }
 
     private LottoResult calculateAndPrintResults(LottoTickets autoTickets, Lotto winningLotto, int bonusNumber, LottoTickets manualTickets) {
-        LottoResult statisticsResult = new LottoResult(autoTickets, winningLotto, bonusNumber, manualTickets);
-        return statisticsResult ;
-    }
-
-    private void validateBonusNumber(Lotto winningLotto, int bonusNumber) {
-        if (bonusNumber < 1 || bonusNumber > 45) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
-        }
-        if (winningLotto.getNumbers().contains(bonusNumber)) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
-        }
+        return new LottoResult(autoTickets, winningLotto, bonusNumber, manualTickets);
     }
 
     private <T> T retry(Supplier<T> supplier) {
