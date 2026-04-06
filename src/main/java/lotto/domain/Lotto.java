fd3d254d977fbfdc9557407f1876a0890c8c4fc2
haeun92e0 <gracekim8729@gmail.com>
2026-04-06 15:53:11 +0900

    Feat: 로또 한 장을 관리하는 Lotto 클래스 리팩터링 및 자동 생성 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,12 +1,12 @@
 package lotto.domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Lotto {
     private static final int LOTTO_SIZE = 6;
+    private static final List<Integer> ALL_NUMBERS = IntStream.rangeClosed(1, 45).boxed().collect(Collectors.toList());
     private final List<LottoNumber> numbers;
 
     private Lotto(List<LottoNumber> numbers) {
@@ -14,32 +14,36 @@ public class Lotto {
         this.numbers = new ArrayList<>(numbers);
         Collections.sort(this.numbers);
     }
-    //Lotto는 반환 타입. 메서드가 끝나면 Lotto 객체 하나를 돌려준다는 뜻
+
     public static Lotto from(List<Integer> numbers) {
-        List<LottoNumber> lottoNumbers = numbers.stream() //리스트를 하나씩 꺼내서 가공할 수 있는 흐름으로 바꾸는 것
-                .map(LottoNumber::new) //stream 안의 각 요소를 변환하는 역할. integer을 new LottoNumber(1)과 같이 바꾼다
-                //.map(number -> new LottoNumber(number)과 같은 코드
-                .collect(Collectors.toList()); //변환된 결과를 다시 리스트로 모음
-        return new Lotto(lottoNumbers);
+        return new Lotto(numbers.stream().map(LottoNumber::new).collect(Collectors.toList()));
+    }
+
+    public static Lotto generateRandom() {
+        List<Integer> shuffleNumbers = new ArrayList<>(ALL_NUMBERS);
+        Collections.shuffle(shuffleNumbers);
+        return from(shuffleNumbers.subList(0, LOTTO_SIZE));
     }
 
     private void validate(List<LottoNumber> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException("로또 번호는 " + LOTTO_SIZE + "개여야 합니다.");
+        }
+        if (new HashSet<>(numbers).size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
         }
     }
 
-    public int countMatch(Lotto winningLotto) { //내 로또 번호와 당첨 번호가 몇개 같은지 세는 함수
-        return (int) numbers.stream() //내 로또 번호 하나씩 꺼내서 처리 시작
-                .filter(winningLotto.numbers::contains)//당첨 번호에 포함된 것만 남겨라
-                .count(); //개수 세기
+    public boolean contains(LottoNumber number) {
+        return numbers.contains(number);
+    }
+
+    public int countMatch(Lotto other) {
+        return (int) numbers.stream().filter(other.numbers::contains).count();
     }
 
     @Override
     public String toString() {
         return numbers.toString();
     }
-
-    //toString을 또 써주는 이유 : 각 클래스는 자기 역할에 맞게 따로 toString을 가져야 함
-    //LottoNumber에서는 하나의 숫자를 string으로 가지며, 여기서는 6개 묶음을.
 }
