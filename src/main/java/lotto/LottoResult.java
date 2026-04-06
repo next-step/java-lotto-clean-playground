a55c55ddfa41a473589a6b96ed30f9ef6cda2c2c
leechan7120 <chanhyoung7120@naver.com>
2026-04-06 22:27:24 +0900

    fix : enum matching number 추가

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -1,16 +1,22 @@
 package lotto;
 
 public enum LottoResult {
-    NONE(0),
-    THREE(5_000),
-    FOUR(50_000),
-    FIVE(1_500_000),
-    SIX(2_000_000_000);
+    NONE(0, 0),
+    THREE(3, 5_000),
+    FOUR(4, 50_000),
+    FIVE(5, 1_500_000),
+    SIX(6, 2_000_000_000);
 
+    private final int matchingNumber;
     private final int reward;
 
-    LottoResult(int reward) {
+    LottoResult(int matchingNumber, int reward) {
+        this.matchingNumber = matchingNumber;
         this.reward = reward;
+    }
+
+    public int getMatchingNumber() {
+        return matchingNumber;
     }
 
     public int getReward(){
