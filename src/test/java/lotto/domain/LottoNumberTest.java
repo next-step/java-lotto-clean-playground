fa4ac74e12d327930d4cabbe3840d29bad9024cd
haeun92e0 <gracekim8729@gmail.com>
2026-04-06 15:52:42 +0900

    Feat: 로또 번호 하나를 포장하는 LottoNumber 클래스 구현

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -1,19 +1,43 @@
 package lotto.domain;
 
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class LottoNumberTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {0, 46, -1})
-    @DisplayName("로또 번호가 1~45 범위를 벗어나면 예외가 발생한다.")
-    void invalidNumberRange(int number) {
-        assertThatThrownBy(() -> new LottoNumber(number))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("로또 번호는 1~45 사이여야 합니다.");
+    @ValueSource(ints = {1, 45})
+    void 로또_번호는_1에서_45_사이의_숫자여야_한다(int value) {
+        // given & when
+        LottoNumber lottoNumber = new LottoNumber(value);
+
+        // then
+        assertThat(lottoNumber).isNotNull();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46})
+    void 로또_번호_범위를_벗어나면_예외가_발생한다(int value) {
+        assertThatThrownBy(() -> new LottoNumber(value))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 숫자가_같으면_같은_객체로_인식한다() {
+        // given
+        LottoNumber number1 = new LottoNumber(10);
+        LottoNumber number2 = new LottoNumber(10);
+
+        // then
+        assertThat(number1).isEqualTo(number2);
+        assertThat(number1.hashCode()).isEqualTo(number2.hashCode());
     }
 }
