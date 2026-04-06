8af0a58d7ca05ab716eee4a37335e9111b6350f0
haeun92e0 <gracekim8729@gmail.com>
2026-04-06 15:52:58 +0900

    Feat: 2등 당첨 판정 로직을 포함한 Rank Enum 구현

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -2,33 +2,35 @@ package lotto.domain;
 
 import java.util.Arrays;
 
-public enum Rank { //열거형. 정해진 값들만 쓰게 만드는 타입
-    MISS(0, 0),
-    FIFTH(3, 5_000),
-    FOURTH(4, 50_000),
+public enum Rank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
     THIRD(5, 1_500_000),
-    FIRST(6, 2_000_000_000);
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0);
 
-    private final int matchCount; //몇개 일치?
-    private final int winningMoney; //상금
+    private final int matchCount;
+    private final int winningMoney;
 
-    Rank(int matchCount, int winningMoney) { //생성자
+    Rank(int matchCount, int winningMoney) {
         this.matchCount = matchCount;
         this.winningMoney = winningMoney;
     }
 
-    public static Rank valueOf(int matchCount) { //일치하는 수를 등수로 바꿔주는 함수
-        return Arrays.stream(values()) //values는 enum의 모든 값을 가져온다
-                .filter(rank -> rank.matchCount == matchCount)//matchCount가 같은 등수만 남겨라
-                .findFirst() //첫번째 결과 가져오기
-                .orElse(MISS); //못찾으면 MISS 반환
-    }
-
-    public int getMatchCount() {
-        return matchCount;
+    public static Rank valueOf(int matchCount, boolean matchBonus) {
+        if (matchCount == 5) return matchBonus ? SECOND : THIRD;
+        return Arrays.stream(values())
+                .filter(rank -> rank != SECOND && rank.matchCount == matchCount)
+                .findFirst()
+                .orElse(MISS);
     }
 
     public int getWinningMoney() {
         return winningMoney;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
     }
 }
