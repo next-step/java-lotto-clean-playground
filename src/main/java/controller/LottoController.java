97c03ab8b8f1fe67cb89d98618b21fadf52eba12
dahyn <k0d5h16@gmail.com>
2026-04-06 15:47:25 +0900

    feat: manualCount를 ManualLottoCount로 원시값 포장 refactor: run 메서드 분리

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -20,20 +20,35 @@ public class LottoController {
 
     public void run() {
         PurchaseAmount purchaseAmount = new PurchaseAmount(inputView.readAmount());
-        int manualCount = inputView.readManualCount();
-        Lottos manualLottos = toLottos(inputView.readManualNumbers(manualCount));
-        Lottos lottos = lottoShop.purchase(purchaseAmount, manualLottos);
+        ManualLottoCount manualLottoCount = new ManualLottoCount(inputView.readManualCount(), purchaseAmount);
+        Lottos lottos = purchaseLottos(purchaseAmount, manualLottoCount);
 
-        int autoCount = lottos.size() - manualCount;
-        outputView.printResultHeader(manualCount, autoCount);
-        outputView.printLottos(lottos.toNumberLists());
+        printPurchaseResult(lottos, manualLottoCount);
 
-        List<Integer> winningNumbers = inputView.readWinningNumbers();
-        BonusBall bonusBall = new BonusBall(new LottoNumber(inputView.readBonusBall()));
-        WinningLotto winningLotto = new WinningLotto(toLotto(winningNumbers), bonusBall);
-
+        WinningLotto winningLotto = readWinningLotto();
         WinningStatistics winningStatistics = WinningStatistics.from(lottos, winningLotto);
 
+        printWinningResult(winningStatistics, purchaseAmount);
+    }
+
+    private Lottos purchaseLottos(PurchaseAmount purchaseAmount, ManualLottoCount manualLottoCount) {
+        Lottos manualLottos = toLottos(inputView.readManualNumbers(manualLottoCount.count()));
+        return lottoShop.purchase(purchaseAmount, manualLottos);
+    }
+
+    private void printPurchaseResult(Lottos lottos, ManualLottoCount manualLottoCount) {
+        int autoCount = lottos.size() - manualLottoCount.count();
+        outputView.printResultHeader(manualLottoCount.count(), autoCount);
+        outputView.printLottos(lottos.toNumberLists());
+    }
+
+    private WinningLotto readWinningLotto() {
+        List<Integer> winningNumbers = inputView.readWinningNumbers();
+        BonusBall bonusBall = new BonusBall(new LottoNumber(inputView.readBonusBall()));
+        return new WinningLotto(toLotto(winningNumbers), bonusBall);
+    }
+
+    private void printWinningResult(WinningStatistics winningStatistics, PurchaseAmount purchaseAmount) {
         outputView.printWinningStatistics(WinningResult.from(winningStatistics));
         outputView.printProfitRate(winningStatistics.calculateProfitRate(purchaseAmount));
     }
--- src/main/java/domain/PurchaseAmount.java
+++ src/main/java/domain/PurchaseAmount.java
@@ -16,21 +16,11 @@ public record PurchaseAmount(int amount) {
         }
     }
 
-    public int calculateAutoLottoCount(int manualCount) {
-        validateManualCount(manualCount);
-        return calculateLottoCount() - manualCount;
-    }
-
-    private void validateManualCount(int manualCount) {
-        if (manualCount < 0) {
-            throw new IllegalArgumentException("수동 구매 수는 0 이상이어야 합니다.");
-        }
-        if (manualCount > calculateLottoCount()) {
-            throw new IllegalArgumentException("수동 로또 수는 총 로또 수를 넘을 수 없습니다.");
-        }
-    }
-
-    private int calculateLottoCount() {
+    public int calculateLottoCount() {
         return amount / LOTTO_PRICE;
+    }
+
+    public int calculateAutoLottoCount(int manualCount) {
+        return calculateLottoCount() - manualCount;
     }
 }
