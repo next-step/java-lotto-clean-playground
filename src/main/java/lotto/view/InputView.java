3faf59fe35a144c54f3b11bfcc0d0f29c6343bf6
haeun92e0 <gracekim8729@gmail.com>
2026-04-06 15:54:27 +0900

    Feat: 수동 구매 수량 및 번호 입력을 위한 InputView 기능 확장

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,17 +1,33 @@
 package lotto.view;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static String inputMoney() {
+    public static int inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-        return scanner.nextLine();
+        return Integer.parseInt(scanner.nextLine());
     }
 
-    public static String inputWinningNumbers() {
-        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        return scanner.nextLine();
+    public static int inputManualCount() {
+        System.out.println("\n수동으로 구매할 로또 수를 입력해 주세요.");
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static List<Integer> inputNumbers() {
+        return Arrays.stream(scanner.nextLine().split(","))
+                .map(String::trim).map(Integer::parseInt).collect(Collectors.toList());
+    }
+
+    public static List<Integer> inputWinningNumbers() {
+        System.out.println("\n지난 주 당첨 번호를 입력해 주세요.");
+        return inputNumbers();
+    }
+
+    public static int inputBonusNumber() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+        return Integer.parseInt(scanner.nextLine());
     }
 }
