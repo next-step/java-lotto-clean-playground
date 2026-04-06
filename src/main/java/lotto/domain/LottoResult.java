25d5fe62b7621a9e703b8226e341b73a0b19f64d
haeun92e0 <gracekim8729@gmail.com>
2026-04-06 15:54:12 +0900

    Feat: 당첨 결과 집계 및 수익률 계산을 담당하는 LottoResult 구현

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,23 +1,23 @@
 package lotto.domain;
-import java.util.EnumMap;
+
+import java.util.Collections;
 import java.util.Map;
 
-public class LottoResult { //결과 자체를 담는 객체 (ex. 1개 일치 >2장, 등등)
+public class LottoResult {
     private final Map<Rank, Long> result;
 
     public LottoResult(Map<Rank, Long> result) {
-        this.result = new EnumMap<>(result);
+        this.result = result;
     }
-    //받은 Map을 그대로 참조하지 않고 새 EnumMap으로 복사해서 저장하는 것
 
-    public double calculateYield(int investment) { //수익률 계산, 수익률은 소수로 나올 수 있으므로 반환타입은 double
-        long totalPrize = result.entrySet().stream() //result는 map임 entrySet은 이걸 (key, value) 쌍 묶음들로 꺼내줌
-                .mapToLong(entry -> entry.getKey().getWinningMoney() * entry.getValue())// 등수 상금 x 등수 개수
+    public double calculateYield(int investment) {
+        long totalPrize = result.entrySet().stream()
+                .mapToLong(e -> e.getKey().getWinningMoney() * e.getValue())
                 .sum();
-        return (double) totalPrize / investment; //investment는 로또를 산 금액.
+        return (double) totalPrize / investment;
     }
 
     public Map<Rank, Long> getResult() {
-        return result;
+        return Collections.unmodifiableMap(result);
     }
 }
