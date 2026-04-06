07e7740ae16b32d4c1964f1abe696bcac67be869
haeun92e0 <gracekim8729@gmail.com>
2026-04-06 15:54:01 +0900

    Feat: 수동/자동 로또를 통합 관리하는 일급 컬렉션 LottoTickets 구현

--- src/main/java/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -1,41 +1,30 @@
 package lotto.domain;
 
-import java.util.Collections;
-import java.util.EnumMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
-public class LottoTickets {// 로또 여러장을 관리
-    private final List<Lotto> tickets; //이젠 Lotto로 구성된 리스트 사용
+public class LottoTickets {
+    private final List<Lotto> tickets;
 
     public LottoTickets(List<Lotto> tickets) {
-        this.tickets = tickets;
+        this.tickets = new ArrayList<>(tickets);
     }
 
-    public static LottoTickets generate(int count) {//로또를 count 수 만큼 자동 생성해서 LottoTickets으로 만들어줌
-        List<Integer> allNumbers = IntStream.rangeClosed(1, 45).boxed().collect(Collectors.toList()); //1부터 45까지 숫자를 리스트로 만드는 코드
-        List<Lotto> tickets = IntStream.range(0, count)//0부터 count 전까지 반복
-                .mapToObj(i -> { //각 반복마다 객체를 하나씩 만들겠다는 뜻
-                    Collections.shuffle(allNumbers); //allNumbers 리스트 순서를 섞는다
-                    return Lotto.from(allNumbers.subList(0, 6)); //섞은 리스트의 앞에 6개만 자름
-                })
-                .collect(Collectors.toList());
-        return new LottoTickets(tickets);
+    public static LottoTickets createCombined(List<Lotto> manualTickets, int autoCount) {
+        List<Lotto> total = new ArrayList<>(manualTickets);
+        for (int i = 0; i < autoCount; i++) {
+            total.add(Lotto.generateRandom());
+        }
+        return new LottoTickets(total);
     }
 
-    public Map<Rank, Long> matchAll(Lotto winningLotto) { //반환 타입은 Rank: 등수, Long : 등수 개수(Collectors.counting 메서드의 반환타입이 long으로 정해져있음)
+    public Map<Rank, Long> matchAll(WinningLotto winningLotto) {
         return tickets.stream()
-                .map(ticket -> Rank.valueOf(ticket.countMatch(winningLotto))) //각 티켓이 당첨 번호와 몇개 맞는지 세서 그 개수를 가지고 Rank.valueOf(등수 결정)
+                .map(winningLotto::judge)
                 .collect(Collectors.groupingBy(rank -> rank, () -> new EnumMap<>(Rank.class), Collectors.counting()));
-    } // 랭크 별로 묶고, Rank enum을 key로 쓰는 Map 하나를 만듦, 각 Rank안에 몇개의 요소가 있는지 셈
+    }
 
     public List<Lotto> getTickets() {
-        return tickets;
+        return Collections.unmodifiableList(tickets);
     }
-
-    public int size() {
-        return tickets.size();
-    } //티켓이 몇장 있는지 반환하는 메서드
 }
