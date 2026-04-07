d7fafe35ad4eae4d7f5d982eafd64c6d10ebe3a5
hapdaypy <mgim9316@gmail.com>
2026-04-07 19:58:19 +0900

    refactor: 중복되는 검증값 단일화

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,12 +1,12 @@
 package domain;
 
+import domain.validator.LottoNumberValidator;
+
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 45;
     private static final int LOTTO_SIZE = 6;
 
     private final List<Integer> lottoNumber;
@@ -26,15 +26,8 @@ public class Lotto {
         }
     }
 
-
     private void validateRange(List<Integer> lottoNumber) {
-        if (lottoNumber.stream().anyMatch(this::isOutOfRange)) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-        }
-    }
-
-    private boolean isOutOfRange(int number) {
-        return number < MIN_NUMBER || number > MAX_NUMBER;
+        lottoNumber.forEach(LottoNumberValidator::validateRange);
     }
 
     public List<Integer> getNumbers() {
--- src/main/java/domain/validator/BonusNumberValidator.java
+++ src/main/java/domain/validator/BonusNumberValidator.java
@@ -5,9 +5,7 @@ import domain.Lotto;
 public class BonusNumberValidator {
 
     public static void validate(Lotto winningLotto, int bonusNumber) {
-        if (bonusNumber < 1 || bonusNumber > 45) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
-        }
+        LottoNumberValidator.validateRange(bonusNumber);
         if (winningLotto.getNumbers().contains(bonusNumber)) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
         }
