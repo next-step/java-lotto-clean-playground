1649b26cb2d2cf87b5cec2f13555c80400458f13
hapdaypy <mgim9316@gmail.com>
2026-04-07 19:59:22 +0900

    refactor: 매직넘버 수정 리터럴 추가

--- src/main/java/domain/LottoResult.java
+++ src/main/java/domain/LottoResult.java
@@ -7,11 +7,10 @@ import java.util.Map;
 public class LottoResult {
     private final Map<Rank, Integer> matchResults;
     private final int bonusNumber;
-    private final int ZERO = 0;
-    private final int ONE = 1;
-    private final int HUNDRED = 100;
+    private static final int INITIAL_COUNT = 0;
+    private static final int INCREMENT_COUNT = 1;
 
-    public LottoResult(LottoTickets lottoTickets, Lotto winningLotto, int bonusNumber,LottoTickets manualLottoTickets) {
+    public LottoResult(LottoTickets lottoTickets, Lotto winningLotto, int bonusNumber, LottoTickets manualLottoTickets) {
         this.matchResults = new EnumMap<>(Rank.class);
         this.bonusNumber = bonusNumber;
         initResults();
@@ -20,21 +19,20 @@ public class LottoResult {
 
     private void initResults() {
         for (Rank rank : Rank.values()) {
-            matchResults.put(rank, ZERO);
+            matchResults.put(rank, INITIAL_COUNT);
         }
     }
 
-    private void calculate(List<Lotto> lottoNumber, List<Integer> winningNumbers, int bonusNumber,List<Lotto> manullottoNumber) {
+    private void calculate(List<Lotto> lottoNumber, List<Integer> winningNumbers, int bonusNumber, List<Lotto> manullottoNumber) {
         lottoNumber.forEach(lotto -> updateMatchResult(lotto, winningNumbers, bonusNumber));
         manullottoNumber.forEach(lotto -> updateMatchResult(lotto, winningNumbers, bonusNumber));
-
     }
 
     private void updateMatchResult(Lotto lotto, List<Integer> winningNumbers, int bonusNumber) {
         int matchCount = countMatch(lotto.getNumbers(), winningNumbers);
         boolean matchBonus = lotto.getNumbers().contains(bonusNumber);
         Rank rank = Rank.valueOf(matchCount, matchBonus);
-        matchResults.put(rank, matchResults.get(rank) + ONE);
+        matchResults.put(rank, matchResults.get(rank) + INCREMENT_COUNT);
     }
 
     private int countMatch(List<Integer> lottoNumber, List<Integer> winningNumbers) {
@@ -44,12 +42,11 @@ public class LottoResult {
     }
 
     public double calculateProfitRate(int purchaseAmount) {
-        long totalPrize = ZERO;
+        long totalPrize = 0L;
         for (Map.Entry<Rank, Integer> entry : matchResults.entrySet()) {
             totalPrize += (long) entry.getKey().getPrizeMoney() * entry.getValue();
         }
-        double rawProfitRate = (double) totalPrize / purchaseAmount;
-        return Math.floor(rawProfitRate * HUNDRED) / 100.0;
+        return (double) totalPrize / purchaseAmount;
     }
 
     public int getRankCount(Rank rank) {
