ec91681177405cded272db9275032fb715f0646c
hapdaypy <mgim9316@gmail.com>
2026-04-07 20:03:57 +0900

    refactor: 매직넘버 추가

--- src/main/java/domain/validator/ManualCountValidator.java
+++ src/main/java/domain/validator/ManualCountValidator.java
@@ -1,12 +1,13 @@
 package domain.validator;
 
 public class ManualCountValidator {
+    private static final int MIN_MANUAL_COUNT = 0;
 
     public static void validate(int totalTrialCount, int manualCount) {
         if (manualCount > totalTrialCount) {
             throw new IllegalArgumentException("[ERROR] 수기 작성 로또 갯수가 전체 시도 횟수를 넘어서는 안 됩니다.");
         }
-        if (manualCount < 0) {
+        if (manualCount < MIN_MANUAL_COUNT) {
             throw new IllegalArgumentException("[ERROR] 수기 작성 로또 갯수는 0 이상이어야 합니다.");
         }
     }
