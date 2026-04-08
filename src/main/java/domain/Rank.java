16f706a25f877ef26f2ff8ba2f5d9eb5aca061a8
dahyn <k0d5h16@gmail.com>
2026-04-08 18:17:41 +0900

    refactor: matchCount 필드 제거

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -1,19 +1,17 @@
 package domain;
 
 public enum Rank {
-    THREE_MATCH(3, 5000, "3개 일치"),
-    FOUR_MATCH(4, 50000, "4개 일치"),
-    FIVE_MATCH(5, 1500000, "5개 일치"),
-    FIVE_BONUS_MATCH(5, 30000000, "5개 일치, 보너스 볼 일치"),
-    SIX_MATCH(6, 2000000000, "6개 일치"),
-    MISS(0, 0, "꽝");
+    THREE_MATCH(5000, "3개 일치"),
+    FOUR_MATCH(50000, "4개 일치"),
+    FIVE_MATCH(1500000, "5개 일치"),
+    FIVE_BONUS_MATCH(30000000, "5개 일치, 보너스 볼 일치"),
+    SIX_MATCH(2000000000, "6개 일치"),
+    MISS(0, "꽝");
 
-    private final int matchCount;
     private final int prizeMoney;
     private final String displayName;
 
-    Rank(int matchCount, int prizeMoney, String displayName) {
-        this.matchCount = matchCount;
+    Rank(int prizeMoney, String displayName) {
         this.prizeMoney = prizeMoney;
         this.displayName = displayName;
     }
