2f4954f2e27458e6e5d0022546a3ffca486e5065
PARK JUNSEONG <jun0829park@gmail.com>
2026-04-09 00:58:19 +0900

    feat: 2등이랑 3등을 구분하기 위해서 matchBonus(불린값)을 추가로 받게 된 로직으 테스트함.

--- src/test/java/domain/RankTest.java
+++ src/test/java/domain/RankTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -8,17 +9,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class RankTest {
 
-    @DisplayName("일치하는 개수에 따라 정확한 등수(Rank)를 반환한다.")
-    @ParameterizedTest
-    @CsvSource(value = {"6:SIX", "5:FIVE", "4:FOUR", "3:THREE"}, delimiter = ':')
-    void valueOfMatchTest(int matchCount, Rank expectedRank) {
-        assertThat(Rank.valueOf(matchCount)).isEqualTo(expectedRank);
+    @DisplayName("일치 개수와 보너스 일치 여부에 따라 정확한 등수를 반환한다.")
+    @Test
+    void valueOfMatchTest() {
+        assertThat(Rank.valueOf(6, false)).isEqualTo(Rank.FIRST);
+        assertThat(Rank.valueOf(5, true)).isEqualTo(Rank.SECOND); // 2등
+        assertThat(Rank.valueOf(5, false)).isEqualTo(Rank.THIRD); // 3등
+        assertThat(Rank.valueOf(4, false)).isEqualTo(Rank.FOURTH);
     }
 
     @DisplayName("일치하는 개수가 0, 1, 2개일 경우 모두 MISS(꽝)를 반환한다.")
     @ParameterizedTest
     @CsvSource(value = {"0", "1", "2"})
     void valueOfMissTest(int matchCount) {
-        assertThat(Rank.valueOf(matchCount)).isEqualTo(Rank.MISS);
+        assertThat(Rank.valueOf(matchCount, false)).isEqualTo(Rank.MISS);
+        assertThat(Rank.valueOf(matchCount, true)).isEqualTo(Rank.MISS);
     }
 }
