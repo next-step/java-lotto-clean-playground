db4420d9f72447b151191232adacf663a4cce2cf
PARK JUNSEONG <jun0829park@gmail.com>
2026-04-09 00:43:30 +0900

    feat: 수동구매 장수를 묻는 기능 추가. - 그 장수만큼 반복문 돌려서 수동번호 문자열을 리스트로 받아오는 기능 추가함.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -26,5 +27,29 @@ public class InputView {
     public String getBonusNumber() {
         System.out.println("\n보너스 볼을 입력해 주세요.");
         return scanner.nextLine();
+    }
+
+    public int getManualCount() {
+        System.out.println("\n수동으로 구매할 로또 수를 입력해 주세요.");
+        String input = scanner.nextLine();
+        if (input.isBlank()) {
+            throw new IllegalArgumentException("입력값이 비어있습니다.");
+        }
+        try {
+            return Integer.parseInt(input.trim());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자여야 합니다.");
+        }
+    }
+
+    public List<String> getManualNumbers(int count) {
+        if (count > 0) {
+            System.out.println("\n수동으로 구매할 번호를 입력해 주세요.");
+        }
+        List<String> manualNumbers = new java.util.ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            manualNumbers.add(scanner.nextLine());
+        }
+        return manualNumbers;
     }
 }
