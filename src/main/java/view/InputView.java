059312d627263180c1411d3de074748679bb4d05
PARK JUNSEONG <jun0829park@gmail.com>
2026-04-08 23:22:24 +0900

    feat: - 보너스넘버에 대한 입력받을수 있는 메서드 추가함.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -22,4 +22,9 @@ public class InputView {
         System.out.println("\n지난 주 당첨번호를 입력해 주세요.");
         return scanner.nextLine();
     }
+
+    public String getBonusNumber() {
+        System.out.println("\n보너스 볼을 입력해 주세요.");
+        return scanner.nextLine();
+    }
 }
