5cf7cd232a5d93d9595cafbf309ac6955408adfc
PARK JUNSEONG <jun0829park@gmail.com>
2026-04-08 23:28:40 +0900

    feat: 기존에는 당첨 번호를 Lotto 클래스 하나로 퉁쳤지만, 이제 당첨 번호(Lotto)와 보너스 볼(LottoNumber)을 세트로 묶어서 관리하고 대조하는 전담 객체인 WinningLotto를 만들도록 수정.

---
+++ src/main/java/domain/WinningLotto.java
@@ -0,0 +1,25 @@
+package domain;
+
+public class WinningLotto {
+    private final Lotto winningLotto;
+    private final LottoNumber bonusNumber;
+
+    public WinningLotto(Lotto winningLotto, LottoNumber bonusNumber) {
+        validateBonusNumber(winningLotto, bonusNumber);
+        this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(Lotto winningLotto, LottoNumber bonusNumber) {
+        if (winningLotto.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException("보너스 볼은 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
+
+    public Rank match(Lotto userLotto) {
+        int matchNumbers = userLotto.getMatchNumbers(winningLotto);
+        boolean matchBonus = userLotto.getNumbers().contains(bonusNumber);
+
+        return Rank.valueOf(matchNumbers, matchBonus);
+    }
+}
