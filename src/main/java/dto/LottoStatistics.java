2399c9fd4d564849330dc50e6a602b6b694740cb
htdufhc-bob <htdufhc@gmail.com>
2026-04-09 15:55:05 +0900

    feat: dto의 정적 팩토리 메서드 추가

--- src/main/java/dto/LottoStatistics.java
+++ src/main/java/dto/LottoStatistics.java
@@ -5,4 +5,8 @@ import domain.LottoRank;
 import java.math.BigDecimal;
 import java.util.Map;
 
-public record LottoStatistics(Map<LottoRank, Integer> matchCount, BigDecimal profitRate) { }
+public record LottoStatistics(Map<LottoRank, Integer> matchedCount, BigDecimal profitRate) {
+    public static LottoStatistics of(Map<LottoRank, Integer> matchedCount, BigDecimal profitRate) {
+        return new LottoStatistics(matchedCount, profitRate);
+    }
+}
--- src/main/java/dto/LottoStatus.java
+++ src/main/java/dto/LottoStatus.java
@@ -1,12 +1,13 @@
 package dto;
 
+import domain.Lotto;
 import domain.LottoNumber;
 
 import java.util.List;
 
 public record LottoStatus(List<LottoNumber> lottoNumbers) {
-    public LottoStatus {
-        lottoNumbers = List.copyOf(lottoNumbers);
+    public static LottoStatus from(Lotto lotto) {
+        return new LottoStatus(lotto.getLottoNumber());
     }
 
     @Override
