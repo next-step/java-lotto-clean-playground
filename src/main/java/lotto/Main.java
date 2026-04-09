f5606e21a50174e23e7f400a706d6ff6638fa22f
leechan7120 <chanhyoung7120@naver.com>
2026-04-09 10:00:13 +0900

    fix : 구조에 맞게 main 수정

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,7 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int totalPrice;
+        int totalPrice, numberOfManual;
 
         System.out.println("구입금액을 입력해 주세요. (ex. 1000) (숫자가 아닌 경우 0으로 간주)");
 
@@ -21,7 +22,22 @@ public class Main {
 
         System.out.println();
 
-        LottoPurchase purchase = purchaseAndPrintLotto(totalPrice);
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        numberOfManual = Integer.parseInt(scanner.nextLine());
+
+        System.out.println();
+
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        List<Lotto> manualLottos = new ArrayList<>();
+        for (int i = 0; i < numberOfManual; i++) {
+            manualLottos.add(LOTTO_PARSER.parse(scanner.nextLine()));
+        }
+
+        LottoPurchase purchase = new LottoPurchase(totalPrice, manualLottos, LOTTO_MAKER);
+
+        System.out.println("\n수동으로 " + numberOfManual + "장, 자동으로 " +
+                (purchase.getNumberOfLotto() - numberOfManual) + "개를 구매했습니다.");
+
         LottoReceipt receipt = purchase.printReceipt();
         displayReceipt(receipt);
         displayChange(purchase.getChange());
@@ -30,16 +46,14 @@ public class Main {
         System.out.println("지난주 당첨 번호를 입력해 주세요.");
         Lotto drawnLotto = LOTTO_PARSER.parse(scanner.nextLine());
 
-        LottoDraw draw = new LottoDraw(drawnLotto, receipt);
+        System.out.println("보너스 볼을 입력해 주세요.");
+        int bonusNumber = Integer.parseInt(scanner.nextLine());
+
+        LottoDraw draw = new LottoDraw(drawnLotto, bonusNumber, receipt);
         System.out.println();
         displayResult(draw);
     }
 
-    private static LottoPurchase purchaseAndPrintLotto(int totalPrice) {
-        LottoPurchase purchase = new LottoPurchase(totalPrice, LOTTO_MAKER);
-        System.out.println(purchase.getNumberOfLotto() + "개를 구매했습니다.");
-        return purchase;
-    }
 
     private static void displayReceipt(LottoReceipt receipt) {
         for (Lotto lotto : receipt.lottos().getLottos()) {
@@ -63,8 +77,11 @@ public class Main {
         System.out.println("3개 일치 (" + LottoResult.THREE.getReward() + "원)- " + draw.getCount(LottoResult.THREE));
         System.out.println("4개 일치 (" + LottoResult.FOUR.getReward() + "원)- " + draw.getCount(LottoResult.FOUR));
         System.out.println("5개 일치 (" + LottoResult.FIVE.getReward() + "원)- " + draw.getCount(LottoResult.FIVE));
+        System.out.println(
+                "5개 일치, 보너스 볼 일치 (" + LottoResult.BONUS.getReward() + "원)- " + draw.getCount(LottoResult.BONUS));
         System.out.println("6개 일치 (" + LottoResult.SIX.getReward() + "원)- " + draw.getCount(LottoResult.SIX));
 
         System.out.println("총 수익률은 " + draw.getRateOfReturn() + "입니다.");
     }
+
 }
