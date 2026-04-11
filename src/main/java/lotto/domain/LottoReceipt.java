50c2ff31845fd8aefd7fcb1deff3f1e456a33508
lhwdev <lhwdev6@outlook.com>
2026-04-11 14:22:51 +0900

    refactor: LottoReceipt에서 출력 관련 코드 제거

--- src/main/java/lotto/domain/LottoReceipt.java
+++ src/main/java/lotto/domain/LottoReceipt.java
@@ -3,9 +3,4 @@ package lotto.domain;
 import java.util.List;
 
 public record LottoReceipt(List<Lotto> lottoRows, int totalPrice) {
-    public void printToConsole() {
-        for (Lotto lottoRow : lottoRows) {
-//            lottoRow.printToConsole();
-        }
-    }
 }
