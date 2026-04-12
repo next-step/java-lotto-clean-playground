f9feedea07186429f8e46e5f7ca83c804a176933
monsaumon <monsaumon@naver.com>
2026-04-13 02:51:32 +0900

    refactor: lottoNumberList 캐싱

--- src/main/java/domain/RandomLottoTicketGenerator.java
+++ src/main/java/domain/RandomLottoTicketGenerator.java
@@ -9,12 +9,16 @@ import static domain.LottoNumber.MAX_NUMBER;
 import static domain.LottoTicket.TICKET_LENGTH;
 
 public class RandomLottoTicketGenerator implements LottoTicketGenerator {
-    @Override
-    public LottoTicket generate() {
-        List<LottoNumber> lottoNumberList = new ArrayList<>();
+    private static final List<LottoNumber> lottoNumberList = new ArrayList<>();
+
+    static {
         for (int i = MIN_NUMBER; i <= MAX_NUMBER; i++) {
             lottoNumberList.add(LottoNumber.valueOf(i));
         }
+    }
+
+    @Override
+    public LottoTicket generate() {
         Collections.shuffle(lottoNumberList);
         return new LottoTicket(lottoNumberList.subList(0, TICKET_LENGTH));
     }
