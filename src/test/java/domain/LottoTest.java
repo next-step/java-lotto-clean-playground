ccaab98f649322f1ac1fb076f8177590a4f91bcd
monsaumon <monsaumon@naver.com>
2026-04-13 15:02:21 +0900

    refactor: 테스트 간소화

--- src/test/java/domain/LottoTest.java
+++ src/test/java/domain/LottoTest.java
@@ -1,29 +1,29 @@
 package domain;
 
+import static domain.Price.PRICE_OF_ONE_LOTTO;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class LottoTest {
     @DisplayName("입력 개수만큼 로또 티켓을 발급한다.")
     @ParameterizedTest
-    @ValueSource(ints = {1000, 3000, 10000, 600000})
-    public void testLotto(int price) {
+    @ValueSource(ints = {1, 3, 10})
+    public void testLotto(int expected) {
         // given
         LottoTicketGenerator lottoTicketGenerator = new RandomLottoTicketGenerator();
-        Lotto lotto = new Lotto(new Price(price), lottoTicketGenerator, new ArrayList<>());
+        Price price = new Price(expected * PRICE_OF_ONE_LOTTO);
 
         // when
+        Lotto lotto = new Lotto(price, lottoTicketGenerator, new ArrayList<>());
         int actual = lotto.getNumberOfTickets();
-        int expected = price / 1000;
 
         // then
         assertThat(actual).isEqualTo(expected);
@@ -31,49 +31,35 @@ public class LottoTest {
 
     @DisplayName("티켓 리스트를 당첨 티켓과 비교하여 LottoResult를 반환한다.")
     @ParameterizedTest
-    @MethodSource
-    public void testGetResults(List<List<Integer>> tickets, LottoTicket winnerTicket, LottoNumber bonusNumber,
-                               LottoResult expected) {
+    @CsvSource(value = {"10,0", "10,5", "10,10"})
+    public void testGetResults(int totalTicketCount, int manualTicketCount) {
         // given
-        LottoTicketGenerator lottoTicketGenerator = new RandomLottoTicketGenerator();
-        Lotto lotto = new Lotto(new Price(tickets.size() * 1000), lottoTicketGenerator, tickets);
+        LottoTicket winnerTicket = new LottoTicket(new ArrayList<>(
+                Stream.of(1, 2, 3, 4, 5, 6)
+                        .map(LottoNumber::valueOf)
+                        .toList()
+        ));
+
+        LottoNumber bonusNumber = LottoNumber.valueOf(7);
+
+        List<List<Integer>> manualTickets = Stream.generate(() -> List.of(8, 9, 10, 11, 12, 13))
+                .limit(manualTicketCount)
+                .toList();
+
+        LottoTicketGenerator lottoTicketGenerator = new TestLottoTicketGenerator(winnerTicket);
+        Price price = new Price(totalTicketCount * PRICE_OF_ONE_LOTTO);
+        Lotto lotto = new Lotto(price, lottoTicketGenerator, manualTickets);
+
+        List<LottoRank> lottoRankOfEachTicket = Stream.concat(
+                Stream.generate(() -> LottoRank.MISS).limit(manualTicketCount),
+                Stream.generate(() -> LottoRank.FIRST).limit(totalTicketCount - manualTicketCount)
+        ).collect(Collectors.toList());
 
         // when
         LottoResult actual = lotto.getResults(winnerTicket, bonusNumber);
+        LottoResult expected = new LottoResult(lottoRankOfEachTicket);
 
         // then
         assertThat(actual).isEqualTo(expected);
-    }
-
-    private static Stream<Arguments> testGetResults() {
-        return Stream.of(
-                Arguments.arguments(
-                        Arrays.asList(
-                                Arrays.asList(1, 2, 3, 4, 5, 7),
-                                Arrays.asList(1, 2, 3, 4, 5, 6),
-                                Arrays.asList(7, 8, 3, 4, 5, 6),
-                                Arrays.asList(8, 2, 3, 4, 5, 6),
-                                Arrays.asList(1, 2, 3, 14, 15, 16),
-                                Arrays.asList(11, 12, 13, 14, 15, 6),
-                                Arrays.asList(11, 21, 31, 41, 15, 16)
-                        ),
-                        new LottoTicket(
-                                Arrays.asList(LottoNumber.valueOf(1), LottoNumber.valueOf(2), LottoNumber.valueOf(3),
-                                        LottoNumber.valueOf(4), LottoNumber.valueOf(5), LottoNumber.valueOf(6))),
-                        LottoNumber.valueOf(7),
-                        new LottoResult(List.of(LottoRank.FIFTH, LottoRank.FOURTH, LottoRank.THIRD, LottoRank.SECOND,
-                                LottoRank.FIRST, LottoRank.MISS, LottoRank.MISS))
-                ),
-                Arguments.arguments(
-                        List.of(
-                                Arrays.asList(1, 2, 3, 4, 5, 6)
-                        ),
-                        new LottoTicket(
-                                Arrays.asList(LottoNumber.valueOf(1), LottoNumber.valueOf(2), LottoNumber.valueOf(3),
-                                        LottoNumber.valueOf(4), LottoNumber.valueOf(5), LottoNumber.valueOf(6))),
-                        LottoNumber.valueOf(7),
-                        new LottoResult(List.of(LottoRank.FIRST))
-                )
-        );
     }
 }
