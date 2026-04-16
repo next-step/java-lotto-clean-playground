ce1d2b7247fc3f97861b57e8fa670669698fce93
RomanticGuy <109668122+codeTravelerLee@users.noreply.github.com>
2026-04-16 16:14:24 +0900

    고생하셨습니다.
    :tada: PR 머지 완료! :tada:

--- src/main/java/domain/MatchResult.java
+++ src/main/java/domain/MatchResult.java
@@ -1,12 +1,15 @@
 package domain;
 
 public enum MatchResult {
-    THREE(3, 5_000),
-    FOUR(4, 50_000),
-    FIVE(5, 1_500_000),
+
     SIX(6, 2_000_000_000),
+    FIVE_WITH_BONUS(5, 30_000_000),
+    FIVE(5, 1_500_000),
+    FOUR(4, 50_000),
+    THREE(3, 5_000),
     MISS(0, 0);
 
+    private static final int BONUS_AVAILABLE_EQUAL_COUNT = 5;
     private final int matchCount;
     private final int matchReward;
 
@@ -15,7 +18,13 @@ public enum MatchResult {
         this.matchReward = matchReward;
     }
 
-    public static MatchResult getResultByMatchCount(int singleEqualCount) {
+    public static MatchResult getResultByMatchCount(int singleEqualCount, boolean isBonusBallMatch) {
+        if (singleEqualCount == BONUS_AVAILABLE_EQUAL_COUNT) {
+            if (isBonusBallMatch) {
+                return FIVE_WITH_BONUS;
+            }
+            return FIVE;
+        }
         for (MatchResult result : values()) {
             if (result.matchCount == singleEqualCount) {
                 return result;
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,6 @@
 package view;
 
+import domain.Lotto;
 import domain.LottoNumber;
 
 import java.util.ArrayList;
@@ -22,6 +23,39 @@ public class InputView {
         return amount;
     }
 
+    public int getManualPurchaseCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        int manualCount = scanner.nextInt();
+
+        if (manualCount < 0) {
+            throw new IllegalArgumentException("0 이상의 수를 입력해주세요!");
+        }
+
+        scanner.nextLine();
+
+        return manualCount;
+    }
+
+    public List<Lotto> getManuallyPurchasedLottos(int manualPurchaseAmount) {
+        System.out.println("수동으로 구매할 로또 번호를 입력해주세요.");
+
+        List<Lotto> manuallyPurchasedLottos = new ArrayList<>();
+
+        for (int i = 0; i < manualPurchaseAmount; i++) {
+            String numbers = scanner.nextLine();
+            String[] numbersArray = numbers.split(",");
+
+            List<LottoNumber> lottoNumbers = new ArrayList<>();
+            for (String number : numbersArray) {
+                int parsedNumber = Integer.parseInt(number.trim());
+                lottoNumbers.add(new LottoNumber(parsedNumber));
+            }
+            manuallyPurchasedLottos.add(new Lotto(lottoNumbers));
+        }
+
+        return manuallyPurchasedLottos;
+    }
+
     public List<LottoNumber> getWinningNumbers() {
         System.out.println("지난 주 당첨 번호를 입력해주세요.");
 
@@ -36,5 +70,14 @@ public class InputView {
         }
 
         return winningNumbers;
+    }
+
+    public LottoNumber getBonusBall() {
+        System.out.println("보너스 볼을 입력해주세요.");
+
+        int bonusNumber = scanner.nextInt();
+        scanner.nextLine();
+
+        return new LottoNumber(bonusNumber);
     }
 }
