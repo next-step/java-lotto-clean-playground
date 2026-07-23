db7b90faffdaade5f1fb2b5e1ab9ecea8c0f1986
anrwkd9902 <anrwkd9902@naver.com>
2026-07-23 22:03:41 +0900

    ëfeat : 1단계 로또 번호 생성 기능 구현

---
+++ src/main/java/domain/LottoGenerator.java
@@ -0,0 +1,30 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoGenerator {
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int LOTTO_SIZE = 6;
+
+    public List<Integer> generateNumbers() {
+        List<Integer> allNumbers = new ArrayList<>();
+        for (int i = MIN_LOTTO_NUMBER; i <= MAX_LOTTO_NUMBER; i++) {
+            allNumbers.add(i);
+        }
+
+        return allNumbers;
+    }
+
+    public List<Integer> generateLotto() {
+        List<Integer> numbers = generateNumbers();
+        Collections.shuffle(numbers);
+
+        List<Integer> lotto = new ArrayList<>(numbers.subList(0, LOTTO_SIZE));
+        Collections.sort(lotto);
+
+        return lotto;
+    }
+}
