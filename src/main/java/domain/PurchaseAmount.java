94effd9fe6fb8c9d98c8c2822b9f9a7dc91d2ab4
anrwkd9902 <anrwkd9902@naver.com>
2026-07-24 00:47:38 +0900

    ãfeat : 1단계 로또 구입 개수 조회 기능 구현

---
+++ src/main/java/domain/PurchaseAmount.java
@@ -0,0 +1,27 @@
+package domain;
+
+public class PurchaseAmount {
+    private static final int LOTTO_PRICE = 1000;
+    private final int amount;
+
+    public PurchaseAmount(int amount) {
+        validateAmount(amount);
+        this.amount = amount;
+    }
+
+    private static void validateAmount(int amount) {
+        if (amount < LOTTO_PRICE) {
+            throw new IllegalArgumentException("구입 금액은 1000원 이상이어야 합니다");
+        }
+
+        if (amount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("구입 금액은 1000원 단위어야 합니다");
+        }
+    }
+
+    public int calculateLottoCount() {
+        return amount / LOTTO_PRICE;
+    }
+
+
+}
