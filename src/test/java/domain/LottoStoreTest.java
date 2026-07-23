87d4dd91dc7086b2947a4b31db371eb5a6ba502c
jh-lee0 <justin030627@naver.com>
2026-07-23 22:26:38 +0900

    test(domain): LottoStore 테스트 추가

---
+++ src/test/java/domain/LottoStoreTest.java
@@ -0,0 +1,49 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoStoreTest {
+    @Test
+    void 구매금액이_정확히_1000원이면_로또가_1장_생성된다() {
+        // Given
+        LottoStore lottoStore = new LottoStore();
+        // When
+        List<Lotto> lottos = lottoStore.buy(1000);
+        // Then
+        assertEquals(1, lottos.size());
+    }
+    @Test
+    void 구매금액만큼_로또를_생성한다() {
+        // Given
+        LottoStore lottoStore = new LottoStore();
+        // When
+        List<Lotto> lottos = lottoStore.buy(5000);
+        // Then
+        assertEquals(5, lottos.size());
+    }
+    @Test
+    void 구매금액이_1000원_단위가_아니면_예외가_발생한다() {
+        // Given
+        LottoStore lottoStore = new LottoStore();
+        // When & Then
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> lottoStore.buy(1500));
+        assertEquals("구매 금액은 1000원 단위여야 합니다.", exception.getMessage());
+    }
+    @Test
+    void 구매금액이_1000원보다_적을_때_예외가_발생한다() {
+        // Given
+        LottoStore lottoStore = new LottoStore();
+        // When & Then
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> lottoStore.buy(999)
+        );
+        assertEquals("구매 금액은 1000원 이상이어야 합니다.", exception.getMessage());
+    }
+}
