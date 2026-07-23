bf7b2b9be16c352d031788e22da1013cc34e39b9
Seong Jin <oroi2009@gmail.com>
2026-07-23 15:15:07 +0900

    feat : InputView 구현

--- src/main/java/domain/LottoNumber.java
+++ src/main/java/domain/LottoNumber.java
@@ -1,3 +1,5 @@
+package domain;
+
 public class LottoNumber {
     private static final int MIN_LOTTO_NUMBER = 1;
     private static final int MAX_LOTTO_NUMBER = 45;
---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,21 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public int readPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = scanner.nextLine();
+        return parseInt(input);
+    }
+
+    private int parseInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("숫자를 입력해야 합니다.");
+        }
+    }
+}
