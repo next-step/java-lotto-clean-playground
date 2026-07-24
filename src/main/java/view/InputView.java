38dd77bf7e9a805a5b25d08264c8b1f3c36ce759
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-24 11:18:50 +0900

    feat: 금액을 입력하면 양의 정수가 입력되었는지 검증하고, 검증 완료 후에 지불 금액을 반환하는 InputView 객체 작성

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,43 @@
+package view;
+
+import java.io.InputStream;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class InputView {
+
+    private final Scanner scanner;
+
+    public InputView(InputStream inputStream) {
+        this.scanner = new Scanner(inputStream);
+    }
+
+    public int payment() {
+        int paid;
+
+        do {
+            paid = validPayment();
+        } while (paid <= 0);
+
+        return paid;
+    }
+
+    private int validPayment() {
+        try {
+            return initPayment();
+        } catch(InputMismatchException e) {
+            System.out.println("지불 금액은 음이 아닌 정수여야 합니다.");
+        }
+        return -1;
+    }
+
+    private int initPayment() {
+        int amount = scanner.nextInt();
+
+        if (amount < 0) {
+            throw new InputMismatchException();
+        }
+
+        return amount;
+    }
+}
