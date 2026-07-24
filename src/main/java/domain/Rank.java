71557c0b4b8a6e175783275cc978323d6e764509
jh-lee0 <justin030627@naver.com>
2026-07-24 15:20:25 +0900

    feat(rank): 로또 당첨 등수 추가

---
+++ src/main/java/domain/Rank.java
@@ -0,0 +1,33 @@
+package domain;
+
+import java.util.Arrays;
+
+public enum Rank {
+    NONE(0, 0),
+    THREE(3, 5000),
+    FOUR(4, 50000),
+    FIVE(5, 1500000),
+    SIX(6, 2000000000);
+
+    private final int matchCount;
+    private final int price;
+
+    Rank(int matchCount, int price) {
+        this.matchCount = matchCount;
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public static Rank findByMatchCount(int matchCount) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.matches(matchCount))
+                .findFirst()
+                .orElse(NONE);
+    }
+    private boolean matches(int matchCount) {
+        return this.matchCount == matchCount;
+    }
+}
