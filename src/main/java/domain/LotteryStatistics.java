3e6332d79d08300d6eec3965c9a940b6163cdd23
jh-lee0 <justin030627@naver.com>
2026-07-24 15:20:34 +0900

    feat(statistics): 로또 당첨 통계 기능 구현

--- src/main/java/domain/LotteryStatistics.java
+++ src/main/java/domain/LotteryStatistics.java
@@ -1,18 +1,39 @@
 package domain;
 
-import java.util.List;
+import java.util.EnumMap;
+import java.util.Map;
 
 public class LotteryStatistics {
-    private int threeMatchCount = 0;
-    private int fourMatchCount = 0;
-    private int fiveMatchCount = 0;
-    private int sixMatchCount = 0;
+    private final Map<Rank, Integer> statistics = new EnumMap<Rank, Integer>(Rank.class);
 
-    public void checkLottos(List<Lotto> lottos, Lotto winningLotto) {
-        int match = 0;
-        for(Lotto lotto : lottos) {
-            lotto.matchCount(winningLotto);
+    public LotteryStatistics() {
+        for (Rank rank : Rank.values()) {
+            statistics.put(rank, 0);
         }
     }
+
+    public Map<Rank, Integer> getStatistics() {
+        return new EnumMap<Rank, Integer>(statistics);
+    }
+
+    public void calculateStatistics(Lottos lottos, Lotto winningLotto) {
+        for (Lotto lotto : lottos) {
+            Rank rank = Rank.findByMatchCount(lotto.matchCount(winningLotto));
+            increaseCount(rank);
+        }
+    }
+
+    private void increaseCount(Rank rank) {
+        int count = statistics.get(rank);
+        statistics.put(rank, ++count);
+    }
+
+   public int calculatePrice() {
+        int total = 0;
+        for (Map.Entry<Rank, Integer> entry : statistics.entrySet()) {
+            total += entry.getKey().getPrice() * entry.getValue();
+        }
+        return total;
+   }
 
 }
