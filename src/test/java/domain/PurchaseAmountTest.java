56bf9eb1428285f3a07b73561cb758ddde7fe9c4
Seong Jin <oroi2009@gmail.com>
2026-07-25 01:47:12 +0900

    test : PurchaseAmount 테스트 구현

---
+++ src/test/java/domain/PurchaseAmountTest.java
@@ -0,0 +1,20 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PurchaseAmountTest {
+    @Test
+    void 구매_금액이_1000원_미만이면_예외를_던진다() {
+        assertThatThrownBy(() -> new PurchaseAmount(999))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 구매_금액이_1000원_이상이면_정상적으로_생성된다() {
+        assertThatCode(() -> new PurchaseAmount(1000))
+                .doesNotThrowAnyException();
+    }
+}
