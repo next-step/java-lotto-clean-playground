83ae2a339a71ae058a757b9e6ef1fb2e0fb4fce0
jh-lee0 <justin030627@naver.com>
2026-07-24 18:02:10 +0900

    test(lottery-statistics): 통계 및 총 당첨금 계산 테스트 추가

---
+++ src/test/java/domain/LotteryStatisticsTest.java
@@ -0,0 +1,57 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LotteryStatisticsTest {
+    @Test
+    void 통계를_정상적으로_검사한다() {
+        // Given
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        Lotto lotto1 = new Lotto(List.of(1, 2, 3, 7, 8, 9));      // 3개 일치
+        Lotto lotto2 = new Lotto(List.of(1, 2, 3, 4, 7, 8));      // 4개 일치
+        Lotto lotto3 = new Lotto(List.of(7, 8, 9, 10, 11, 12));   // 일치 x
+
+        Lottos lottos = new Lottos(List.of(lotto1, lotto2, lotto3));
+
+        LotteryStatistics lotteryStatistics = new LotteryStatistics();
+
+        // When
+        lotteryStatistics.calculateStatistics(lottos, winningLotto);
+
+        // Then
+        Map<Rank, Integer> result = lotteryStatistics.getStatistics();
+
+        assertEquals(1, result.get(Rank.NONE));
+        assertEquals(1, result.get(Rank.THREE));
+        assertEquals(1, result.get(Rank.FOUR));
+        assertEquals(0, result.get(Rank.FIVE));
+        assertEquals(0, result.get(Rank.SIX));
+    }
+
+    @Test
+    void 총_당첨금을_정확히_계산한다() {
+        // Given
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        Lotto lotto1 = new Lotto(List.of(1, 2, 3, 7, 8, 9));      // 3개 일치
+        Lotto lotto2 = new Lotto(List.of(1, 2, 3, 4, 7, 8));      // 4개 일치
+        Lotto lotto3 = new Lotto(List.of(7, 8, 9, 10, 11, 12));   // 일치 x
+
+        Lottos lottos = new Lottos(List.of(lotto1, lotto2, lotto3));
+
+        LotteryStatistics lotteryStatistics = new LotteryStatistics();
+
+        // When
+        lotteryStatistics.calculateStatistics(lottos, winningLotto);
+        int totalPrize = lotteryStatistics.calculatePrize();
+
+        // Then
+        assertEquals(55000, totalPrize);
+    }
+}
