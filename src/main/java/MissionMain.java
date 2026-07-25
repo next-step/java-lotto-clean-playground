28168a31cbbfd47c0b9593dc28db7b975bd8185d
KDYoung5606 <2271323@hansung.ac.kr>
2026-07-25 23:33:00 +0900

    feat: MissionMain에서 당첨 통계 계산 흐름 연결
    - 당첨 번호 입력 → LottoWinningStatus 생성 → 당첨 통계/수익률 출력까지 이어지는 흐름을 조립

--- src/main/java/MissionMain.java
+++ src/main/java/MissionMain.java
@@ -1,12 +1,19 @@
 import domain.LottoSystem;
+import domain.LottoWinningStatus;
+import domain.WinningNumbers;
 import generator.RandomLottoNumberGenerator;
 import view.InputView;
 import view.ResultView;
 
 public class MissionMain {
     public static void main(String[] args) {
-        final var inputMoney = InputView.inputMoney();
+        final Integer inputMoney = InputView.inputMoney();
         LottoSystem lottoSystem = new LottoSystem(inputMoney, new RandomLottoNumberGenerator());
         ResultView.printLottoResult(lottoSystem.getPurchasedLottoNumbers());
+
+        final var winningNumbers = new WinningNumbers(InputView.inputWinningNumbers());
+        LottoWinningStatus lottoWinningStatus = new LottoWinningStatus(lottoSystem.getLottos(), winningNumbers);
+        ResultView.printWinningStatistics(lottoWinningStatus.getLottoCountByMatchNumber(), lottoWinningStatus.getPrizeMoneyTable(), inputMoney);
+        ResultView.printRateOfReturn(lottoWinningStatus.getPrizeRate(inputMoney));
     }
 }
