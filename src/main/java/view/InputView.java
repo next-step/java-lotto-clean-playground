be4ca21b1dfd3856eb8ebcff0e8c7e216055545b
KDYoung5606 <2271323@hansung.ac.kr>
2026-07-25 23:32:21 +0900

    feat(view): 당첨 번호 입력과 당첨 통계 출력 기능 추가
    - InputView에 콤마로 구분된 당첨 번호를 List<Integer>로 변환해 입력받는 기능 추가함
    - ResultView가 도메인의 일급 컬렉션 타입(PurchasedLottoNumbers, LottoCountByMatchNumber, PrizeMoneyTable)을 그대로 받아 당첨 통계와 수익률을 출력하도록 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,7 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -7,5 +9,21 @@ public class InputView {
         Scanner scanner = new Scanner(System.in);
         System.out.println("구입금액을 입력해주세요");
         return scanner.nextInt();
+    }
+
+    public static List<Integer> inputWinningNumbers(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("지난 주 당첨 번호를 입력하세요");
+        String inputWinningNumbers = scanner.nextLine();
+        return winningNumbers(inputWinningNumbers);
+    }
+
+    private static List<Integer> winningNumbers(String inputWinningNumbers){
+        List<String> element = List.of(inputWinningNumbers.split(","));
+        List<Integer> change = new ArrayList<>();
+        for (String s : element) {
+            change.add(Integer.parseInt(s.trim()));
+        }
+        return change;
     }
 }
