6dec026d4bf542eb4f6f4ad7fed81708399715bb
jh-lee0 <justin030627@naver.com>
2026-07-28 00:06:40 +0900

    refactor: LottoNumber 클래스를 record로 변경

--- src/main/java/domain/lotto/LottoNumber.java
+++ src/main/java/domain/lotto/LottoNumber.java
@@ -1,42 +1,16 @@
 package domain.lotto;
 
-import java.util.Objects;
-
-public class LottoNumber {
+public record LottoNumber(int value) {
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 45;
-    private final int value;
 
-    public LottoNumber(int value) {
+    public LottoNumber {
         validateNumberRange(value);
-        this.value = value;
     }
 
     public static void validateNumberRange(int number) {
         if (number < MIN_NUMBER || number > MAX_NUMBER) {
             throw new IllegalArgumentException("로또 번호는 1~45 사이여야 합니다.");
         }
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (!(obj instanceof LottoNumber)) {
-            return false;
-        }
-        LottoNumber other = (LottoNumber) obj;
-        return value == other.value;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
-    }
-
-    @Override
-    public String toString() {
-        return String.valueOf(value);
     }
 }
--- src/test/java/domain/LottoNumberTest.java
+++ src/test/java/domain/LottoNumberTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -26,21 +25,5 @@ class LottoNumberTest {
                 () -> new LottoNumber(number)
         );
         assertEquals("로또 번호는 1~45 사이여야 합니다.", exception.getMessage());
-    }
-
-    @Test
-    void 내부_값이_같으면_같다() {
-        LottoNumber number1 = new LottoNumber(7);
-        LottoNumber number2 = new LottoNumber(7);
-
-        assertEquals(number1, number2);
-    }
-
-    @Test
-    void 내부_값이_다르면_다르다() {
-        LottoNumber number1 = new LottoNumber(7);
-        LottoNumber number2 = new LottoNumber(8);
-
-        assertNotEquals(number1, number2);
     }
 }
