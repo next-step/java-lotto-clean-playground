bb9b1730c22de04502140e349555865e61ed64a6
KDYoung5606 <2271323@hansung.ac.kr>
2026-07-28 21:36:27 +0900

    feat(controller) : LottoController 생성 및 Main 역할 간소화

--- src/main/java/MissionMain.java
+++ src/main/java/MissionMain.java
@@ -1,19 +1,7 @@
-import domain.LottoSystem;
-import domain.LottoWinningStatus;
-import domain.WinningNumbers;
-import generator.RandomLottoNumberGenerator;
-import view.InputView;
-import view.ResultView;
+import controller.LottoController;
 
 public class MissionMain {
     public static void main(String[] args) {
-        final Integer inputMoney = InputView.inputMoney();
-        LottoSystem lottoSystem = new LottoSystem(inputMoney, new RandomLottoNumberGenerator());
-        ResultView.printLottoResult(lottoSystem.getPurchasedLottoNumbers());
-
-        final var winningNumbers = new WinningNumbers(InputView.inputWinningNumbers());
-        LottoWinningStatus lottoWinningStatus = new LottoWinningStatus(lottoSystem.getLottos(), winningNumbers);
-        ResultView.printWinningStatistics(lottoWinningStatus.getLottoCountByMatchNumber(), lottoWinningStatus.getPrizeMoneyTable(), inputMoney);
-        ResultView.printRateOfReturn(lottoWinningStatus.getPrizeRate(inputMoney));
+        LottoController.run();
     }
 }
---
+++ src/main/java/controller/LottoController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import domain.LottoSystem;
+import domain.LottoWinningStatus;
+import generator.RandomLottoNumberGenerator;
+import java.util.List;
+import view.InputView;
+import view.ResultView;
+
+public class LottoController {
+  public static void run(){
+    final Integer purchaseAmount = InputView.purchaseAmount();
+    final LottoSystem lottoSystem = new LottoSystem(purchaseAmount, new RandomLottoNumberGenerator());
+    ResultView.printPurchasedLottos(lottoSystem.purchasedLottoCount(), lottoSystem.purchasedLottoNumbers());
+
+    final List<Integer> winningNumbers = InputView.winningNumbers();
+    final LottoWinningStatus lottoWinningStatus = new LottoWinningStatus(winningNumbers, lottoSystem.purchasedLottos(), purchaseAmount);
+    ResultView.printMatchingNumbers(lottoWinningStatus.rankTable());
+    ResultView.printProfitRate(lottoWinningStatus.profitRate());
+  }
+}
