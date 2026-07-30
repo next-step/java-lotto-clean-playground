674e0c639ee72b7b3cc4f78cef90a0e10e3727d3
seungminbluebox <parksm1415@gmail.com>
2026-07-30 13:46:44 +0900

    feat(Model) : Lotto class 생성

---
+++ src/main/java/model/Lotto.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Comparator;
+import java.util.Random;
+
+public class Lotto {
+    private List<Integer> nums=new ArrayList<>();
+    private Random random=new Random();
+
+    public Lotto(){
+        for(int i=0;i<6;i++){
+            int randomNum=random.nextInt(45)+1;
+            this.nums.add(randomNum);
+        }
+        this.nums.sort(Comparator.naturalOrder());
+    }
+
+    @Override
+    public String toString(){
+        return nums.toString();
+    }
+}
