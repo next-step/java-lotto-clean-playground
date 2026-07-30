bf92db61dcb4d5442ce0b388fef13baf99f21710
seungminbluebox <parksm1415@gmail.com>
2026-07-30 13:46:26 +0900

    feat(View, 1단계 완료) : printLottos,printLotto 생성

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -1,5 +1,8 @@
 package view;
 
+import model.Lotto;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class View {
@@ -9,7 +12,17 @@ public class View {
         System.out.println("구입금액을 입력해 주세요.");
         int cost=scanner.nextInt();
         int ticketAmount=cost/1000;
-        System.out.printf("%d개를 구매했습니다.", ticketAmount);
+        System.out.printf("%d개를 구매했습니다.\n", ticketAmount);
         return ticketAmount;
+    }
+
+    public void printLottos(List<Lotto> lottos){
+        for(Lotto lotto : lottos){
+            printLotto(lotto);
+        }
+    }
+
+    public void printLotto(Lotto lotto){
+            System.out.println(lotto);
     }
 }
