cb3d9a586c52ea80927ac60a0ea272087186f5c9
seungminbluebox <parksm1415@gmail.com>
2026-07-30 17:38:10 +0900

    feat(Controller, 2단계) : 당첨되어 수익률 수익금 관련 메서드 생성

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,11 +1,13 @@
 package controller;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
+
 import model.Lotto;
 
 public class Controller {
     private int ticketAmount;
     private List<Lotto> lottos=new ArrayList<>();
+    final List<Integer> prizeCost=new ArrayList<>();
+
 
     public Controller(int ticketAmount){
         this.ticketAmount=ticketAmount;
@@ -17,4 +19,56 @@ public class Controller {
         }
         return lottos;
     }
+    public List<List<Integer>> findWinning(List<Integer> nums){
+        List<List<Integer>> matrix=new ArrayList<>();
+        for(int i=0;i<ticketAmount;i++){
+            Lotto lotto=lottos.get(i);
+            matrix.add(findRank(lotto,nums));
+        }
+        return matrix;
+    }
+
+    public List<Integer> findRank(Lotto lotto,List<Integer> nums){
+        List<Integer> inter=new ArrayList<>(( lotto.getLottoList()));
+        inter.retainAll(nums);
+        return inter;
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////
+
+    public Map<Integer,Integer> createWinCountMap(List<List<Integer>> martrix){
+        Map<Integer,Integer> staticWin=new HashMap<>();
+        for(int i=3;i<7;i++){
+            staticWin.put(i,0);
+        }
+        for(List<Integer> inter : martrix){
+            int size=inter.size();
+            staticWin.put(size,staticWin.getOrDefault(size,0)+1);
+        }
+        return  staticWin;
+    }
+
+    public float totalRatio(int cost,Map<Integer,Integer> staticWin){
+        prizeCost.add(0,0);
+        staticWin.forEach((matchCount,winCount)->{
+            totalPrize(matchCount,winCount,prizeCost);
+        });
+        float ratio=((float)prizeCost.get(0)/(float)cost);
+        return ratio;
+    }
+
+    public void totalPrize(int matchCount,int winCount,List<Integer> prizeCost){
+        if(matchCount==3){
+            prizeCost.set(0,prizeCost.get(0)+5000*winCount);
+        }
+        if(matchCount==4){
+            prizeCost.set(0,prizeCost.get(0)+50000*winCount);
+        }
+        if(matchCount==5){
+            prizeCost.set(0,prizeCost.get(0)+1500000*winCount);
+        }
+        if(matchCount==6){
+            prizeCost.set(0,prizeCost.get(0)+2000000000*winCount);
+        }
+    }
+
 }
