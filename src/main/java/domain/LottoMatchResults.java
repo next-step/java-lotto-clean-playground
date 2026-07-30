50c1ab16b561ea2d6e2eb624c3c218818c51e1a6
KDYoung5606 <2271323@hansung.ac.kr>
2026-07-31 01:56:44 +0900

    refactor(domain): LottoRankTable이 등수 집계를 직접 수행하도록 변경
    - List<LottoMatchResult>를 받아 생성자 내부에서 Map을 직접 구성하고,
      등수별 매칭된 횟수 및 총 상금 계산까지 스스로 수행하도록 변경

--- src/main/java/domain/LottoMatchResults.java
+++ src/main/java/domain/LottoMatchResults.java
@@ -1,9 +1,7 @@
 package domain;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class LottoMatchResults {
   private final List<LottoMatchResult> lottoMatchResults;
@@ -13,27 +11,11 @@ public class LottoMatchResults {
   public LottoMatchResults(List<LottoMatchResult> lottoMatchResults, int purchaseAmount) {
     this.lottoMatchResults = new ArrayList<>(lottoMatchResults);
     this.purchaseAmount = purchaseAmount;
-    this.lottoRankTable = new LottoRankTable(createRankTable());
-  }
-
-  public long countByRank(LottoRank lottoRank){
-    return lottoMatchResults.stream().filter(result->result.getRank() == lottoRank).count();
-  }
-
-  private long sumPrize() {
-    return lottoMatchResults.stream().mapToLong(LottoMatchResult::winningPrize).sum();
+    this.lottoRankTable = new LottoRankTable(this.lottoMatchResults);
   }
 
   public double profitRate(){
-    return (double) sumPrize() / purchaseAmount;
-  }
-
-  private Map<LottoRank, Long> createRankTable(){
-    Map<LottoRank, Long> rankCounts = new HashMap<>();
-    for(LottoRank lottoRank : LottoRank.values()){
-      rankCounts.put(lottoRank, countByRank(lottoRank));
-    }
-    return rankCounts;
+    return (double) lottoRankTable.sumPrize() / purchaseAmount;
   }
 
   public LottoRankTable rankTable(){
--- src/main/java/domain/LottoRankTable.java
+++ src/main/java/domain/LottoRankTable.java
@@ -1,16 +1,33 @@
 package domain;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class LottoRankTable {
   private final Map<LottoRank, Long> lottoRankTable;
 
-  public LottoRankTable(Map<LottoRank, Long> lottoRankTable) {
-    this.lottoRankTable = new HashMap<>(lottoRankTable);
+  public LottoRankTable(List<LottoMatchResult> lottoMatchResults) {
+    this.lottoRankTable = createRankTable(lottoMatchResults);
   }
 
   public long countOf(LottoRank lottoRank){
     return lottoRankTable.get(lottoRank);
+  }
+
+  private Map<LottoRank, Long> createRankTable(List<LottoMatchResult> lottoMatchResults){
+    Map<LottoRank, Long> rankCounts = new HashMap<>();
+    for(LottoRank lottoRank : LottoRank.values()){
+      rankCounts.put(lottoRank, countByRank(lottoRank, lottoMatchResults));
+    }
+    return rankCounts;
+  }
+
+  private long countByRank(LottoRank lottoRank, List<LottoMatchResult> lottoMatchResults){
+    return lottoMatchResults.stream().filter(result->result.getRank() == lottoRank).count();
+  }
+
+  public long sumPrize() {
+    return lottoRankTable.entrySet().stream().mapToLong(value -> value.getKey().getPrize() * value.getValue()).sum();
   }
 }
