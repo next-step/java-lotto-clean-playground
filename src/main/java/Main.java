b0f218d5a9c5b6767e88c145cb4501911c13d3a9
seungminbluebox <parksm1415@gmail.com>
2026-07-30 17:39:45 +0900

    feat(Main, 2단계) : 2단계 흐름에 맞는 코드 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,17 +2,38 @@ import controller.Controller;
 import model.Lotto;
 import view.View;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         View view=new View();
         int cost=view.inputCost();
+        int amount=view.printAmount(cost);
+        Map<Integer,Integer> staticWin=new HashMap<>();
         List<Lotto> lottos;
+        List<List<Integer>> matrix=new ArrayList<>();
 
-        Controller controller=new Controller(cost);
+        Controller controller=new Controller(amount);
         lottos=controller.genLotto();
         view.printLottos(lottos);
+        List<Integer> winningNums = view.inputWinningNums();
+
+        matrix=controller.findWinning(winningNums);
+        System.out.println(matrix);
+        ////////////////////////////////////////////////////////////////////////////////////////
+        staticWin=controller.createWinCountMap(matrix);
+
+        System.out.println("당첨통계");
+        System.out.println("---------");
+
+        float ratio=controller.totalRatio(cost,staticWin);
+
+        view.cycle(staticWin);
+
+        System.out.printf("총 수익률은 %f입니다",ratio);
 
     }
 }
