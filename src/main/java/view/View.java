68d75daed01e6b2fdd1f099ddb675553dc99ada0
seungminbluebox <parksm1415@gmail.com>
2026-07-30 12:17:28 +0900

    fix(View) : 입력금액기반 티켓개수 출력

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -8,7 +8,8 @@ public class View {
     public int inputCost() {
         System.out.println("구입금액을 입력해 주세요.");
         int cost=scanner.nextInt();
-        System.out.printf("입력하신 금액은 %d원 입니다.\n", cost);
-        return cost;
+        int ticketAmount=cost/1000;
+        System.out.printf("%d개를 구매했습니다.", ticketAmount);
+        return ticketAmount;
     }
 }
