39fa0e35d1db28b21986711e1422b70320c5dd3d
seungminbluebox <parksm1415@gmail.com>
2026-08-01 13:48:46 +0900

    feat(Main, 4단계) : 사용자 수동입력 흐름에 맞게 코드 수정 및 주석 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,23 +12,46 @@ public class Main {
         View view=new View();
         //구매금액입력
         int cost=view.inputCost();
-        int amount=view.printAmount(cost);
+
+        //수동로또 개수 입력
+        int passiveAmount=view.inputPassiveAmount();
+
+        //수동 개수만큼 로또 번호 입력
+        List<Lotto> lottos= view.inputPassiveLotto(passiveAmount);
+
+        //총갯수중 자동,수동 몇개인지 구분
+        int totalAmount=cost/1000;
+        int autoAmount=totalAmount-passiveAmount;
+
+        //수동 몇장, 자동몇장 구매했는지 출력
+        view.printAmount(autoAmount,passiveAmount);
+
+
+        // 당첨-입력간 중복갯수:몇개 형식의 Map생성
         Map<Integer,Integer> staticWin=new HashMap<>();
-        List<Lotto> lottos;
+
+        //각 lotto별 중복되는 숫자들의 List들을 모아놓은 List
         List<List<Integer>> matrix=new ArrayList<>();
 
-        Controller controller=new Controller(amount);
-        lottos=controller.genLotto();
+        //로직 담당 Controller 생성
+        Controller controller=new Controller(autoAmount,passiveAmount);
+
+        //수동로또 + 자동로또= 전체 로또 완성
+        lottos.addAll(controller.genLotto());
+
         //구매액만큼 로또 자동구매결과 출력
         view.printLottos(lottos);
+
         //당첨번호 입력
         List<Integer> winningNums = view.inputWinningNums();
         //보너스볼 입력
         int bonusBall=view.inputBonusBall();
 
-        matrix=controller.findWinning(winningNums,bonusBall);
+        matrix=controller.findWinning(lottos,winningNums,bonusBall);
+        System.out.println(matrix); //[[1, 2, 3, 4, 5, 6], [3], [2], [5], [4], [], [], [6], [1], []]
         ////////////////////////////////////////////////////////////////////////////////////////
         staticWin=controller.createWinCountMap(matrix,bonusBall);
+        System.out.println(staticWin); // {3=0, 4=0, 5=0, 6=0, 7=0}
 
         System.out.println("당첨통계");
         System.out.println("---------");
