601eb714f24d09e5ba23d420d6810014a54d893b
juhee0223 <pjuhee23@dankook.ac.kr>
2026-08-01 14:09:34 +0900

    feat: Lotto 클래스

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,6 +1,24 @@
 package domain;
 
+import java.util.Random;
+import java.util.TreeSet;
+
 public class Lotto {
-    //한 장에 들어있는 숫자들을 갖고있으면 됨
+    public static final int LOTTO_NUMBER_BOUND = 45;
+    public static final int LOTTO_NUBER_COUNT = 6;
+
+    TreeSet<Integer> randomNumberSet = new TreeSet<>();
+    Random random = new Random();
+
+    public Lotto() { // 로또 한장 생성자
+        setLottoNumber();
+    }
+
+    private void setLottoNumber(){
+        while (randomNumberSet.size() < LOTTO_NUBER_COUNT) {
+            randomNumberSet.add(random.nextInt(LOTTO_NUMBER_BOUND + 1));
+        }
+
+    }
 
 }
