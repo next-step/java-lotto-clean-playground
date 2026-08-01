05b1205f082d962e78e2b8f2742cb3c787bf44e1
KDYoung5606 <2271323@hansung.ac.kr>
2026-08-02 02:24:54 +0900

    refactor(controller): LottoWinningStatus 제거 후 controller가 조립과 단일 책임을 갖도록 변경

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,8 +1,10 @@
 package controller;
 
+import domain.LottoMatchResult;
+import domain.LottoMatchResults;
 import domain.LottoSystem;
-import domain.LottoWinningStatus;
 import domain.RandomLottoNumberGenerator;
+import domain.WinningNumbers;
 import java.util.List;
 import view.InputView;
 import view.ResultView;
@@ -10,14 +12,31 @@ import view.ResultView;
 public class LottoController {
   public static void run() {
     final int purchaseAmount = InputView.purchaseAmount();
+    final LottoSystem lottoSystem = purchaseLotto(purchaseAmount);
+    final WinningNumbers winningNumbers = createWinningNumbers();
+    final LottoMatchResults lottoMatchResults = createLottoMatchResults(lottoSystem, winningNumbers, purchaseAmount);
+    printLottoResult(lottoMatchResults);
+  }
+
+  private static LottoSystem purchaseLotto(int purchaseAmount) {
     final LottoSystem lottoSystem = new LottoSystem(purchaseAmount, new RandomLottoNumberGenerator());
     ResultView.printPurchasedLottos(lottoSystem.purchasedLottoCount(), lottoSystem.purchasedLottoNumbers());
+    return lottoSystem;
+  }
 
-    final List<Integer> winningNumbers = InputView.winningNumbers();
+  private static WinningNumbers createWinningNumbers() {
+    final List<Integer> winningNumberValues = InputView.winningNumbers();
     final int bonusNumber = InputView.bonusNumber();
+    return new WinningNumbers(winningNumberValues, bonusNumber);
+  }
 
-    final LottoWinningStatus lottoWinningStatus = new LottoWinningStatus(winningNumbers, lottoSystem.purchasedLottos(), purchaseAmount, bonusNumber);
-    ResultView.printMatchingNumbers(lottoWinningStatus.rankTable());
-    ResultView.printProfitRate(lottoWinningStatus.profitRate());
+  private static LottoMatchResults createLottoMatchResults(LottoSystem lottoSystem, WinningNumbers winningNumbers, int purchaseAmount) {
+    final List<LottoMatchResult> matchResults = lottoSystem.purchasedLottos().createLottoMatchResult(winningNumbers);
+    return new LottoMatchResults(matchResults, purchaseAmount);
+  }
+
+  private static void printLottoResult(LottoMatchResults lottoMatchResults) {
+    ResultView.printMatchingNumbers(lottoMatchResults.rankTable());
+    ResultView.printProfitRate(lottoMatchResults.profitRate());
   }
 }
