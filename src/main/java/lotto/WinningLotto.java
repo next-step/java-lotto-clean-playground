2f14ff0c0c702aea3fba1c6000ecfd5e5527b0cb
MunJaeWon <wodnjs3961@naver.com>
2026-08-01 21:40:58 +0900

    refactor: 당첨 번호 비교 결과 캡슐화

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -11,13 +11,23 @@ public class WinningLotto {
     }
 
     public LottoRank determineRank(Lotto lotto) {
-        int matchCount = lotto.countMatches(winningNumbers);
-        return LottoRank.from(matchCount, lotto.contains(bonusNumber));
+        LottoMatch match = match(lotto);
+        return LottoRank.from(match.matchCount(), match.bonusMatched());
+    }
+
+    private LottoMatch match(Lotto lotto) {
+        return new LottoMatch(
+                lotto.countMatches(winningNumbers),
+                lotto.contains(bonusNumber)
+        );
     }
 
     private void validateBonusNumber(Lotto winningNumbers, LottoNumber bonusNumber) {
         if (winningNumbers.contains(bonusNumber)) {
             throw new IllegalArgumentException("보너스 번호는 당첨 번호와 중복될 수 없습니다.");
         }
+    }
+
+    private record LottoMatch(int matchCount, boolean bonusMatched) {
     }
 }
