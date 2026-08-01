25a7a6d42eeb18f6be14aaae8db45a56728a171a
JisuHan <ivyjisu@gmail.com>
2026-08-02 01:37:54 +0900

    feat: 기본 로또 숫자 생성 기능 구현

---
+++ src/main/java/domain/LottoNumber.java
@@ -0,0 +1,27 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoNumber {
+    private static final List<Integer> NUMBERS = new ArrayList<>();
+    static {
+        for (int i = 1; i <= 45; i++) {
+            NUMBERS.add(i);
+        }
+    }
+
+    private final List<Integer> lottoNumbers;
+
+    public LottoNumber() {
+        List<Integer> numbers = new ArrayList<>(NUMBERS);
+        Collections.shuffle(numbers);
+        this.lottoNumbers = new ArrayList<>(numbers.subList(0, 6));
+        Collections.sort(this.lottoNumbers);
+    }
+
+    public List<Integer> getLottoNumbers() {
+        return lottoNumbers;
+    }
+}
