7bff8a674fc3d78a3052c5395d4c1ecdaa375951
장서인 <seoin1893@dankook.ac.kr>
2026-08-02 23:19:05 +0900

    feat: 4단계 AutoLotto, ManualLotto 구현

---
+++ src/main/java/domain/AutoLotto.java
@@ -0,0 +1,61 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class AutoLotto {
+
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
+
+    public static List<Lotto> generateAutoLotto(int autoLottoCount) { // 개수가 count인 로또 리스트 생성
+        List<Lotto> autoLotto = new ArrayList<>();
+
+        for(int i = 0; i < autoLottoCount; i++) {
+            List<LottoNumber> numbers = createNumbers();
+            shuffleNumbers(numbers);
+            autoLotto.add(new Lotto(getLottoNumbers(numbers)));
+        }
+
+        return autoLotto;
+    }
+
+
+    private static List<LottoNumber> createNumbers() { // 1~45 번호 준비
+        List<LottoNumber> numbers = new ArrayList<>();
+
+        for (int number = 1; number <= MAX_LOTTO_NUMBER; number++) {
+            numbers.add(new LottoNumber(number));
+        }
+
+        return numbers;
+    }
+
+
+    private static void shuffleNumbers(List<LottoNumber> numbers) { // 1~45 번호를 무작위로 섞음
+        Collections.shuffle(numbers);
+    }
+
+
+    private static List<LottoNumber> extractNumber(List<LottoNumber> numbers) { // 앞자리 6개 추출
+        List<LottoNumber> lottoNumbers = new ArrayList<>();
+        for (int index = 0; index < LOTTO_NUMBER_COUNT; index++) {
+            lottoNumbers.add(numbers.get(index));
+        }
+        return lottoNumbers;
+    }
+
+
+    private static void sortLotto(List<LottoNumber> lottoNumbers) { // 로또 번호 오름차순 정렬
+        Collections.sort(lottoNumbers);
+    }
+
+
+    private static List<LottoNumber> getLottoNumbers(List<LottoNumber> numbers) { // 로또 번호 반환
+        List<LottoNumber> lottoNumbers = extractNumber(numbers);
+        sortLotto(lottoNumbers);
+
+        return lottoNumbers;
+    }
+}
---
+++ src/main/java/domain/ManualLotto.java
@@ -0,0 +1,51 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ManualLotto {
+
+    public static List<Lotto> generateManualLotto(int manualLottoCount, List<String> values) {
+        List<Lotto> manualLotto = new ArrayList<>();
+        for(int i = 0; i < manualLottoCount; i++) {
+            manualLotto.add(convertStringToLottoNumber(splitString(values.get(i))));
+        }
+
+        return manualLotto;
+    }
+
+    private static String[] splitString(String input) { // string 객체를 쉼표 기준으로 나눔
+        String[] manualLottoNumbers = input.split(",");
+
+        manualLottoNumbers = Arrays.stream(manualLottoNumbers)
+                .map(String::trim).toArray(String[]:: new); // 공백 처리
+
+        long distinct = Arrays.stream(manualLottoNumbers).distinct().count(); // 중복 처리
+
+        if(manualLottoNumbers.length != distinct){
+            throw new IllegalArgumentException("로또는 중복될 수 없습니다.");
+        }
+
+        return manualLottoNumbers;
+    }
+
+
+    private static Lotto convertStringToLottoNumber(String[] manualLottoNumbers) { // string 배열를 LottoNumber 리스트로 변환
+        List<LottoNumber> lottoNumbers = new ArrayList<>();
+
+        try {
+            for (int i = 0; i < manualLottoNumbers.length; i++) {
+                int number = Integer.parseInt(manualLottoNumbers[i]);
+
+                lottoNumbers.add(new LottoNumber(number));
+            }
+        }
+
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException("로또 번호는 숫자로 입력해주세요.");
+        }
+
+        return new Lotto(lottoNumbers);
+    }
+}
