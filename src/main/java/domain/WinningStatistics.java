1925288859c69eb7b3f8ad907dea6b31a2180cab
JisuHan <ivyjisu@gmail.com>
2026-08-02 14:05:08 +0900

    feat: 당첨 번호 비교 및 등수별 당첨 통계 집계 기능 구현

---
+++ src/main/java/domain/WinningStatistics.java
@@ -0,0 +1,35 @@
+package domain;
+
+import java.util.List;
+
+public class WinningStatistics {
+    int fourthPlace = 0; // 3개 일치
+    int thirdPlace = 0; // 4개 일치
+    int secondPlace = 0; // 5개 일치
+    int firstPlace = 0; // 6개 일치
+    int temptCount = 0;
+    MatchCount matchCount = new MatchCount();
+
+    public void compareLottos (List<Integer> winningNumbers, Lottos lottos) {
+        for (LottoNumber lottoNumber : lottos.getLottos()) {
+            matchCount.comparingLotto(lottoNumber.getLottoNumbers(), winningNumbers);
+            temptCount = matchCount.getCount();
+            getPlace(temptCount);
+        }
+    }
+
+    public void getPlace(int temptCount) {
+        if (temptCount == 3) {
+            fourthPlace++;
+        }
+        if (temptCount == 4) {
+            thirdPlace++;
+        }
+        if (temptCount == 5) {
+            secondPlace++;
+        }
+        if (temptCount == 6) {
+            firstPlace++;
+        }
+    }
+}
