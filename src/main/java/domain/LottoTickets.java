4095f4f95c335e10da7379d42a4fe8d6f19db041
juhee0223 <pjuhee23@dankook.ac.kr>
2026-08-02 21:45:14 +0900

    feat: 수동 입력번호를 받아와서 LottoTickets에서 처리

--- src/main/java/domain/LottoTickets.java
+++ src/main/java/domain/LottoTickets.java
@@ -1,16 +1,27 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class LottoTickets {
     ArrayList<Lotto> lottoArrayList = new ArrayList<>();
 
-    public ArrayList<Lotto> makeLottos(int lottoTotalCount){
-        for (int i = 0; i < lottoTotalCount; i++) {
+    public void addUserSelectedLottos(List<String> userSelectedNumbersInput) {
+        for (String numbersString : userSelectedNumbersInput) {
+            List<Integer> numbers = Arrays.stream(numbersString.split(", "))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            lottoArrayList.add(new Lotto(numbers));
+        }
+    }
+
+    public void addAutoLottos(int autoCount) {
+        for (int i = 0; i < autoCount; i++) {
             lottoArrayList.add(new Lotto());
         }
-        return lottoArrayList;
     }
 
     public TreeSet<Integer> getLottoTreeSet(int lottoTicketNumber){
