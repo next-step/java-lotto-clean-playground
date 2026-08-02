29d459658dc58a0dd8cb6104c0647222bdec4284
장서인 <seoin1893@dankook.ac.kr>
2026-08-02 17:55:21 +0900

    feat: 3단계 - enum 생성

---
+++ src/main/java/domain/Rank.java
@@ -0,0 +1,54 @@
+package domain;
+
+public enum Rank {
+
+    FIRST(6, 2000000000),
+    SECOND(5,30000000),
+    THIRD(5, 1500000),
+    FOURTH(4, 50000),
+    FIFTH(3, 5000),
+    MISS(0, 0);
+
+    private final int matchCount;
+    private final long prize;
+
+    Rank(int matchCount, long prize) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+    }
+
+
+    public static Rank findRank(int matchCount, boolean bonusMatch) {
+        if (matchCount == 6) {
+            return FIRST;
+        }
+
+        if (matchCount == 5 && bonusMatch) {
+            return SECOND;
+        }
+
+        if (matchCount == 5) {
+            return THIRD;
+        }
+
+        if (matchCount == 4) {
+            return FOURTH;
+        }
+
+        if (matchCount == 3) {
+            return FIFTH;
+        }
+
+        return MISS;
+    }
+
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+
+    public long getPrize() {
+        return prize;
+    }
+}
