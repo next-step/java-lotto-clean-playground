c7b225217a13e12839527d706c910047fa210329
juhee0223 <pjuhee23@dankook.ac.kr>
2026-08-02 17:25:13 +0900

    feat: enum 수정 및 등수별 설명 추가

--- src/main/java/domain/LottoWinningTypePrize.java
+++ src/main/java/domain/LottoWinningTypePrize.java
@@ -3,22 +3,24 @@ package domain;
 import java.util.Arrays;
 import java.util.function.Function;
 
-public enum LottoWinningTypePrize {
-    PRIZE_3_MATCH(originMatchCount -> originMatchCount * 5000),
-    PRIZE_4_MATCH(originMatchCount -> originMatchCount * 50000),
-    PRIZE_5_MATCH(originMatchCount -> originMatchCount * 1500000),
-    PRIZE_5_MATCH_AND_1_BONUS_BALL_MATCH(originMatchCount -> originMatchCount * 30000000),
-    PRIZE_6_MATCH(originMatchCount -> originMatchCount * 2000000000),
-    NO_PRIZE(originMatchCount -> 0L);
+public enum LottoWinningType {
+    FIRST_PLACE("6개 일치 (2000000000원)- ", tickets -> tickets * 2000000000),
+    SECOND_PLACE("5개 일치, 보너스 볼 일치(30000000원)- ", tickets -> tickets * 30000000),
+    THIRD_PLACE("5개 일치 (1500000원)- ", tickets -> tickets * 1500000),
+    FOURTH_PLACE("4개 일치 (50000원)- ", tickets -> tickets * 50000),
+    FIFTH_PLACE("3개 일치 (5000원)- ", tickets -> tickets * 5000);
 
-    private final Function<Long, Long> expression;
+    private String winningDescription;
+    private Function<Double, Double> prizeExpression;
 
-    LottoWinningTypePrize(Function<Long, Long> expression) {
-        this.expression = expression;
+
+    LottoWinningType(String winningDescription, Function<Double, Double> prizeExpression) {
+        this.winningDescription = winningDescription;
+
     }
 
-    public long calculatePrize(int originMatchCount) {
-        return expression.apply((long) originMatchCount);
+    public long calculatePrize(double matchingTickets) {
+        return prizeExpression.apply((long) matchingTickets);
     }
 
     public static LottoWinningTypePrize findLottoWinningType(String winningType){
