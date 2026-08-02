27467927833b376c7db205acecf9e1dc80c09264
juhee0223 <pjuhee23@dankook.ac.kr>
2026-08-02 16:13:48 +0900

    feat: 당첨금이 없는 타입일때 처리

--- src/main/java/domain/LottoWinningTypePrize.java
+++ src/main/java/domain/LottoWinningTypePrize.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.util.Arrays;
 import java.util.function.Function;
 
 public enum LottoWinningTypePrize {
@@ -7,7 +8,8 @@ public enum LottoWinningTypePrize {
     PRIZE_4_MATCH(originMatchCount -> originMatchCount * 50000),
     PRIZE_5_MATCH(originMatchCount -> originMatchCount * 1500000),
     PRIZE_5_MATCH_AND_1_BONUS_BALL_MATCH(originMatchCount -> originMatchCount * 30000000),
-    PRIZE_6_MATCH(originMatchCount -> originMatchCount * 2000000000);
+    PRIZE_6_MATCH(originMatchCount -> originMatchCount * 2000000000),
+    NO_PRIZE(originMatchCount -> 0L);
 
     private final Function<Long, Long> expression;
 
@@ -16,10 +18,14 @@ public enum LottoWinningTypePrize {
     }
 
     public long calculatePrize(int originMatchCount) {
-        return expression.apply(originMatchCount);
+        return expression.apply((long) originMatchCount);
     }
 
-
-
+    public static LottoWinningTypePrize findLottoWinningType(String winningType){
+        return Arrays.stream(LottoWinningTypePrize.values())
+                .filter(lottoWinningTypePrize -> lottoWinningTypePrize.name().equals(winningType))
+                .findAny()
+                .orElse(NO_PRIZE);
+    }
 
 }
