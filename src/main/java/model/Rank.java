ab8dc812cbd35aebcfbb2a86f86ffd36791fd2f0
seungminbluebox <parksm1415@gmail.com>
2026-08-03 18:28:19 +0900

    feat(Rank) : 등수관련 enum추가

---
+++ src/main/java/model/Rank.java
@@ -0,0 +1,42 @@
+package model;
+
+public enum Rank {
+    FIFTH(3,5_000),
+    FOURTH(4, 50_000),
+    THIRD(5, 1_500_000),
+    SECOND(5, 30_000_000),
+    FIRST(6, 2_000_000_000);
+
+    private int matchCount;
+    private int prize;
+
+    Rank(int matchCount,int prize){
+        this.matchCount=matchCount;
+        this.prize=prize;
+    }
+
+    public static Rank of(int matchCount,boolean bonusMatched){
+        if(matchCount==6){
+            return FIRST;
+        }
+        if(matchCount==5&&bonusMatched){
+            return SECOND;
+        }
+        if(matchCount==5){
+            return THIRD;
+        }
+        if(matchCount==4){
+            return FOURTH;
+        }
+        if(matchCount==3){
+            return FIFTH;
+        }
+        return null;
+    }
+
+
+    public int getPrize(){
+        return prize;
+    }
+
+}
