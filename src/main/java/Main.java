fa49f9fa458ea46471e859e8b5ed3fe9eccd52ea
seungminbluebox <parksm1415@gmail.com>
2026-08-03 18:32:30 +0900

    fix(Main) : 수동로또 중복출력 해결, 개행문자 적용

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
-import controller.Controller;
+import controller.LottosGenerator;
+import controller.Result;
+import controller.WinFinder;
 import model.Lotto;
-import view.View;
+import view.InputView;
+import view.OutputView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,58 +12,57 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        View view=new View();
+        InputView inputView=new InputView();
+        OutputView outputView=new OutputView();
         //구매금액입력
-        int cost=view.inputCost();
+        int cost=inputView.inputCost();
 
         //수동로또 개수 입력
-        int passiveAmount=view.inputPassiveAmount();
+        int passiveAmount=inputView.inputPassiveAmount();
 
         //수동 개수만큼 로또 번호 입력
-        List<Lotto> lottos= view.inputPassiveLotto(passiveAmount);
+        List<Lotto> lottos= inputView.inputPassiveLotto(passiveAmount);
 
         //총갯수중 자동,수동 몇개인지 구분
         int totalAmount=cost/1000;
         int autoAmount=totalAmount-passiveAmount;
 
         //수동 몇장, 자동몇장 구매했는지 출력
-        view.printAmount(autoAmount,passiveAmount);
+        outputView.printAmount(autoAmount,passiveAmount);
 
 
-        // 당첨-입력간 중복갯수:몇개 형식의 Map생성
-        Map<Integer,Integer> staticWin=new HashMap<>();
+        // 당첨-입력간 [중복갯수:해당 중복갯수가 몇장] 형식의 Map생성
+        Map<Integer,Integer> staticWin;
 
         //각 lotto별 중복되는 숫자들의 List들을 모아놓은 List
-        List<List<Integer>> matrix=new ArrayList<>();
+        List<List<Integer>> matrix;
 
         //로직 담당 Controller 생성
-        Controller controller=new Controller(autoAmount,passiveAmount);
+        LottosGenerator lottosGenerator=new LottosGenerator(autoAmount,lottos);
 
         //수동로또 + 자동로또= 전체 로또 완성
-        lottos.addAll(controller.genLotto());
+        lottos=lottosGenerator.genLotto();
 
         //구매액만큼 로또 자동구매결과 출력
-        view.printLottos(lottos);
+        outputView.printLottos(lottos);
 
         //당첨번호 입력
-        List<Integer> winningNums = view.inputWinningNums();
+        List<Integer> winningNums = inputView.inputWinningNums();
         //보너스볼 입력
-        int bonusBall=view.inputBonusBall();
+        int bonusBall=inputView.inputBonusBall();
 
-        matrix=controller.findWinning(lottos,winningNums,bonusBall);
-        System.out.println(matrix); //[[1, 2, 3, 4, 5, 6], [3], [2], [5], [4], [], [], [6], [1], []]
-        ////////////////////////////////////////////////////////////////////////////////////////
-        staticWin=controller.createWinCountMap(matrix,bonusBall);
-        System.out.println(staticWin); // {3=0, 4=0, 5=0, 6=0, 7=0}
+        WinFinder winFinder=new WinFinder(winningNums,bonusBall);
 
-        System.out.println("당첨통계");
-        System.out.println("---------");
+        matrix=winFinder.findWinning(lottos,totalAmount);
 
-        float ratio=controller.totalRatio(cost,staticWin);
+        staticWin=winFinder.createWinCountMap(matrix);
 
-        view.cycle(staticWin);
+        Result result=new Result();
 
-        System.out.printf("총 수익률은 %f입니다",ratio);
+        float ratio=result.totalRatio(cost,staticWin);
 
+        outputView.cycle(staticWin);
+
+        outputView.printRatio(ratio);
     }
 }
