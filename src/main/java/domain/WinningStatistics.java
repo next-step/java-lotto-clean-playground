a2e257b55852861893e5ba8b84d9c6b21e6d4fc2
JisuHan <ivyjisu@gmail.com>
2026-08-03 16:00:51 +0900

    refactor: Map과 Enum 기반의 로또 당첨 집계 로직으로 변경

--- src/main/java/domain/WinningStatistics.java
+++ src/main/java/domain/WinningStatistics.java
@@ -1,54 +1,26 @@
 package domain;
 
-import java.util.List;
+import java.util.*;
 
 public class WinningStatistics {
-    private int fourthPlace = 0; // 3개 일치
-    private int thirdPlace = 0; // 4개 일치
-    private int secondPlace = 0; // 5개 일치
-    private int firstPlace = 0; // 6개 일치
-    private int bonusSecondPlace = 0; // 5개 일치 + 보너스 볼 일치
-    private int temptCount = 0;
+    Map<Rank, WinnerNum> winningStatistics = new HashMap<>();
+
     MatchCount matchCount = new MatchCount();
+    public WinningStatistics() {
+        for (Rank rank : Rank.values()) {
+            winningStatistics.put(rank, new WinnerNum(0));
+        }
+    }
 
     public void compareLottos (List<Integer> winningNumbers, Lottos lottos, int bonusNumber) {
         for (LottoNumber lottoNumber : lottos.getLottos()) {
             matchCount.comparingLotto(lottoNumber.getLottoNumbers(), winningNumbers);
-            temptCount = matchCount.getCount();
-            setPlace(temptCount, lottoNumber.getLottoNumbers(), bonusNumber);
+            Rank rank = Rank.getRank(matchCount.getCount(), matchCount.hasBonusNumber(lottoNumber.getLottoNumbers(), bonusNumber));
+            winningStatistics.get(rank).increase();
         }
     }
 
-    private void setPlace(int temptCount, List<Integer> lottoNumber, int bonusNumber) {
-        if (temptCount == 3) {fourthPlace++;}
-        if (temptCount == 4) {thirdPlace++;}
-        if (temptCount == 5) {
-            bonusBallChecker(lottoNumber, bonusNumber);
-        }
-        if (temptCount == 6) {firstPlace++;}
-    }
-
-    private void bonusBallChecker(List<Integer> lottoNumber, int bonusNumber) {
-        if (matchCount.hasBonusNumber(lottoNumber, bonusNumber)) {
-            bonusSecondPlace++;
-            return;
-        }
-        secondPlace++;
-    }
-
-    public int getFourthPlace() {
-        return fourthPlace;
-    }
-    public int getThirdPlace() {
-        return thirdPlace;
-    }
-    public int getSecondPlace() {
-        return secondPlace;
-    }
-    public int getFirstPlace() {
-        return firstPlace;
-    }
-    public int getBounusSecondPlace() {
-        return bonusSecondPlace;
+    public Map<Rank, WinnerNum> getWinningStatistics() {
+        return winningStatistics;
     }
 }
