d37a959b37da7c09bfbd4cf79752689cde8ae1a9
장서인 <seoin1893@dankook.ac.kr>
2026-08-04 20:28:13 +0900

    feat (Lotto) : 로또 숫자 개수 검증, 로또 숫자 중복 검증 기능 추가

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -4,10 +4,27 @@ import java.util.List;
 
 public class Lotto {
 
+    private static final int LOTTO_NUMBER_COUNT = 6;
+
     private final List<LottoNumber> lotto;
 
     public Lotto(List<LottoNumber> lotto) {
+        verifyNumberCount(lotto);
+        verifyOverlap(lotto);
         this.lotto = lotto;
+    }
+
+    private void verifyNumberCount(List<LottoNumber> lotto) { // 로또 숫자 개수 검증
+        if(lotto.size() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_COUNT + "개의 숫자를 입력해주세요.");
+        }
+    }
+
+    private void verifyOverlap(List<LottoNumber> lotto) { // 로또 숫자 중복 검증
+        long distinct = lotto.stream().distinct().count();
+        if(lotto.size() != distinct){
+            throw new IllegalArgumentException("로또는 중복될 수 없습니다.");
+        }
     }
 
     public List<LottoNumber> getLotto() { // lotto 번호 반환
