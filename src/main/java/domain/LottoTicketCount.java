1dd83543e3e7bee7aac8f2156d832a5a7813c467
juhee0223 <pjuhee23@dankook.ac.kr>
2026-08-04 10:41:41 +0900

    feat: 구입금액 1000원 이상의 1000원단위로 제한

--- src/main/java/domain/LottoTicketCount.java
+++ src/main/java/domain/LottoTicketCount.java
@@ -5,8 +5,17 @@ public class LottoTicketCount {
     private int lottoTicketCount;
 
     public int convertLottoPriceToTicketCount(int totalLottoPrice) {
+        validatePurchaseAmount(totalLottoPrice);
         lottoTicketCount = totalLottoPrice / PRICE_PER_ONE_LOTTO_TICKET;
         return lottoTicketCount;
     }
 
+    private void validatePurchaseAmount(int price) {
+        if (price < PRICE_PER_ONE_LOTTO_TICKET) {
+            throw new IllegalArgumentException("구입 금액은 " + PRICE_PER_ONE_LOTTO_TICKET + "원 이상이어야 합니다.");
+        }
+        if (price % PRICE_PER_ONE_LOTTO_TICKET != 0) {
+            throw new IllegalArgumentException("구입 금액은 " + PRICE_PER_ONE_LOTTO_TICKET + "원 단위로 입력해야 합니다.");
+        }
+    }
 }
