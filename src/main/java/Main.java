4787db9e9aedf0450e05788cab2894536c1e32ad
seungminbluebox <parksm1415@gmail.com>
2026-08-06 00:13:18 +0900

    fix(Main) : 수정된 코드에 맞게 흐름 수정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,47 +1,50 @@
 import controller.LottosGenerator;
+import controller.PassiveLottoGenerator;
 import controller.Result;
-import controller.WinFinder;
+import controller.WinCounter;
+import controller.WinningLotto;
 import model.Lotto;
+import model.Rank;
 import view.InputView;
 import view.OutputView;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
+
     public static void main(String[] args) {
-        InputView inputView=new InputView();
-        OutputView outputView=new OutputView();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
         //구매금액입력
-        int cost=inputView.inputCost();
+        int cost = inputView.inputCost();
 
         //수동로또 개수 입력
-        int passiveAmount=inputView.inputPassiveAmount();
+        int passiveAmount = inputView.inputPassiveAmount();
 
+        PassiveLottoGenerator passiveLottoGenerator = new PassiveLottoGenerator();
         //수동 개수만큼 로또 번호 입력
-        List<Lotto> lottos= inputView.inputPassiveLotto(passiveAmount);
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < passiveAmount; i++) {
+            List<Integer> nums = inputView.inputPassiveLotto();
+            Lotto lotto = passiveLottoGenerator.genPassiveLotto(nums);
+            lottos.add(lotto);
+        }
 
         //총갯수중 자동,수동 몇개인지 구분
-        int totalAmount=cost/1000;
-        int autoAmount=totalAmount-passiveAmount;
+        int totalAmount = cost / 1000;
+        int autoAmount = totalAmount - passiveAmount;
 
         //수동 몇장, 자동몇장 구매했는지 출력
-        outputView.printAmount(autoAmount,passiveAmount);
-
-
-        // 당첨-입력간 [중복갯수:해당 중복갯수가 몇장] 형식의 Map생성
-        Map<Integer,Integer> staticWin;
-
-        //각 lotto별 중복되는 숫자들의 List들을 모아놓은 List
-        List<List<Integer>> matrix;
+        outputView.printAmount(autoAmount, passiveAmount);
 
         //로직 담당 Controller 생성
-        LottosGenerator lottosGenerator=new LottosGenerator(autoAmount,lottos);
+        LottosGenerator lottosGenerator = new LottosGenerator();
 
         //수동로또 + 자동로또= 전체 로또 완성
-        lottos=lottosGenerator.genLotto();
+        lottos.addAll(lottosGenerator.genLottos(autoAmount));
 
         //구매액만큼 로또 자동구매결과 출력
         outputView.printLottos(lottos);
@@ -49,19 +52,19 @@ public class Main {
         //당첨번호 입력
         List<Integer> winningNums = inputView.inputWinningNums();
         //보너스볼 입력
-        int bonusBall=inputView.inputBonusBall();
+        int bonusBall = inputView.inputBonusBall();
+        Lotto winLotto = new Lotto(winningNums);
 
-        WinFinder winFinder=new WinFinder(winningNums,bonusBall);
+        WinningLotto winningLotto = new WinningLotto(winLotto, bonusBall);
+        WinCounter winCounter = new WinCounter();
 
-        matrix=winFinder.findWinning(lottos,totalAmount);
+        EnumMap<Rank, Integer> counts = winCounter.countWins(lottos, winningLotto);
 
-        staticWin=winFinder.createWinCountMap(matrix);
+        Result result = new Result(counts);
 
-        Result result=new Result();
+        float ratio = result.totalRatio(cost);
 
-        float ratio=result.totalRatio(cost,staticWin);
-
-        outputView.cycle(staticWin);
+        outputView.cycle(counts);
 
         outputView.printRatio(ratio);
     }
