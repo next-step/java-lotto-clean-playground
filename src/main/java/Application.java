ce392275634f3bd17b3240f448aabdd1bff9f26a
johaeunn <jheun04@naver.com>
2026-08-05 21:24:43 +0900

    refactor: Application 실행 흐름을 의미 단위로 분리

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -17,6 +17,14 @@ public class Application {
 
         PurchaseAmount purchaseAmount = new PurchaseAmount(inputView.readPurchaseAmount());
 
+        Lottos lottos = purchaseLottos(inputView, outputView, purchaseAmount);
+
+        WinningLotto winningLotto = createWinningLotto(inputView);
+
+        printStatistics(outputView, lottos, winningLotto, purchaseAmount);
+    }
+
+    private static Lottos purchaseLottos(InputView inputView, OutputView outputView, PurchaseAmount purchaseAmount) {
         int manualLottoCount = inputView.readManualLottoCount();
         int autoLottoCount = purchaseAmount.calculateAutoLottoCount(manualLottoCount);
 
@@ -27,11 +35,18 @@ public class Application {
         outputView.printPurchasedCount(manualLottoCount, autoLottoCount);
         outputView.printLottos(lottos);
 
+        return lottos;
+    }
+
+    private static WinningLotto createWinningLotto(InputView inputView) {
         WinningNumbers winningNumbers = new WinningNumbers(inputView.readWinningNumbers());
         LottoNumber bonusNumber = new LottoNumber(inputView.readBonusNumber());
 
-        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+        return new WinningLotto(winningNumbers, bonusNumber);
+    }
 
+    private static void printStatistics(OutputView outputView, Lottos lottos,
+                                        WinningLotto winningLotto, PurchaseAmount purchaseAmount) {
         LottoStatistics lottoStatistics = new LottoStatistics(lottos, winningLotto);
         outputView.printLottoStatistics(purchaseAmount, lottoStatistics);
     }
