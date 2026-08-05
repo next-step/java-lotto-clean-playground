423146b5f0fbd9551037ff12bb8e18c1ba56ec53
juhee0223 <pjuhee23@dankook.ac.kr>
2026-08-05 14:53:16 +0900

    refactor: Lotto 생성자 내부에서 검증추가

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -20,6 +20,12 @@ public class Lotto {
         if (userSelectedNumbers.size() != LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException("로또 번호는" + LOTTO_NUMBER_COUNT + "개여야 합니다.");
         }
+        for (Integer number : userSelectedNumbers) {
+            if (number < LOTTO_NUMBER_LOWER_BOUND || number > LOTTO_NUMBER_BOUND) {
+                throw new IllegalArgumentException("로또 번호는 " + LOTTO_NUMBER_LOWER_BOUND
+                        + "부터 " + LOTTO_NUMBER_BOUND + " 사이의 숫자여야 합니다.");
+            }
+        }
         this.randomNumberSet.addAll(userSelectedNumbers);
         if (this.randomNumberSet.size() != LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
@@ -30,7 +36,7 @@ public class Lotto {
         return this.randomNumberSet;
     }
 
-    private void setLottoNumber(){
+    private void setLottoNumber() {
         while (randomNumberSet.size() < LOTTO_NUMBER_COUNT) {
             randomNumberSet.add(random.nextInt(LOTTO_NUMBER_LOWER_BOUND, LOTTO_NUMBER_BOUND + 1));
         }
