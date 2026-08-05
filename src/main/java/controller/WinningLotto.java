9df8b08ccfd0a5f5cbdebff2bd5412e1436c027a
seungminbluebox <parksm1415@gmail.com>
2026-08-05 21:00:56 +0900

    fix(WinningLotto) : enum활용해 불필요한 메서드제거

---
+++ src/main/java/controller/WinningLotto.java
@@ -0,0 +1,30 @@
+package controller;
+
+import model.Lotto;
+import model.Rank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WinningLotto {
+    private final Lotto winlotto;
+    private final int bonusBall;
+
+    public WinningLotto(Lotto winlotto, int bonusBall){
+        if(winlotto.contain(bonusBall)){
+            throw new IllegalArgumentException("보너스볼을 포함한 로또는 우승할수 없습니다");
+        }
+        this.winlotto=winlotto;
+        this.bonusBall=bonusBall;
+    }
+
+    public Rank findRank(Lotto lotto){
+            int matchCount = lotto.countMatch(winlotto);
+            boolean bonusMatched = lotto.contain(bonusBall);
+
+            return Rank.of(matchCount, bonusMatched);
+        }
+    }
+
