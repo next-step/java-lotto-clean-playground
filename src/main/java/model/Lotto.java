faccc661794fb87da71fe05bd3b8f527ce6df8e9
seungminbluebox <parksm1415@gmail.com>
2026-08-05 15:55:58 +0900

    fix(Lotto) : setter삭제 및 생성자 단순화

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -7,18 +7,9 @@ import java.util.Random;
 
 public class Lotto {
     private List<Integer> nums=new ArrayList<>();
-    private Random random=new Random();
 
-    public Lotto(){
-        for(int i=0;i<6;i++){
-            int randomNum=random.nextInt(45)+1;
-
-            while (nums.contains(randomNum)){
-                randomNum=random.nextInt(45)+1;
-            }
-            this.nums.add(randomNum);
-        }
-        this.nums.sort(Comparator.naturalOrder());
+    public Lotto(List<Integer> nums){
+        this.nums=nums;
     }
 
     public List<Integer> getLottoList(){
@@ -26,15 +17,7 @@ public class Lotto {
     }
 
     public void setLotto(List<Integer> nums){
-        if (nums.size()!=6){
-            throw new ArrayIndexOutOfBoundsException("로또번호는 6자리를 넘을수 없습니다");
-        }
 
-        for(int num:nums){
-            if(num>45||num<1){
-                throw new IllegalArgumentException("숫자는 1에서 45만 가능합니다.");
-            }
-        }
 
         this.nums=nums;
     }
