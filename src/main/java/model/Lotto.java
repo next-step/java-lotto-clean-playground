f3aea6519c9f79821c3a3e8c6aa8c4ec882e661e
seungminbluebox <parksm1415@gmail.com>
2026-08-05 17:11:40 +0900

    fix(Lotto) : 3가지 예외 생성자레벨에서 처리

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -1,14 +1,12 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Comparator;
-import java.util.Random;
+import java.util.*;
 
 public class Lotto {
     private List<Integer> nums=new ArrayList<>();
 
     public Lotto(List<Integer> nums){
+        validateNums(nums);
         this.nums=nums;
     }
 
@@ -16,10 +14,31 @@ public class Lotto {
         return nums;
     }
 
-    public void setLotto(List<Integer> nums){
+    public void validateNums(List<Integer> nums){
+        validateSizeOfNums(nums);
+        validateRangeOfNums(nums);
+        validateDuplNums(nums);
+    }
 
+    public void validateSizeOfNums(List<Integer> nums){
+        if (nums.size()!=6){
+            throw new ArrayIndexOutOfBoundsException("로또번호는 6자리를 넘을수 없습니다");
+        }
+    }
 
-        this.nums=nums;
+    public void validateRangeOfNums(List<Integer> nums){
+        for(int num:nums){
+            if(num>45||num<1){
+                throw new IllegalArgumentException("숫자는 1에서 45만 가능합니다.");
+            }
+        }
+    }
+    public void validateDuplNums(List<Integer> nums){
+        int originalSize=nums.size();
+        int removeDuplicatedNumberSize=new HashSet<>(nums).size();
+        if(originalSize!=removeDuplicatedNumberSize){
+            throw new IllegalArgumentException("중복 숫자 입력은 불가능합니다");
+        }
     }
 
     @Override
