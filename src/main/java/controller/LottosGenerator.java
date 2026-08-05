ff84348d49e383c9a91b3fbc0bc4e5bbd985237a
seungminbluebox <parksm1415@gmail.com>
2026-08-05 15:56:59 +0900

    fix(LottosGenerator) : 로또번호 생성시 이 객체에서 범위검사

--- src/main/java/controller/LottosGenerator.java
+++ src/main/java/controller/LottosGenerator.java
@@ -3,22 +3,42 @@ package controller;
 import model.Lotto;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Random;
 
 public class LottosGenerator {
     private int autoAmount;
     private List<Lotto> lottos=new ArrayList<>();
+    private Random random=new Random();
+
 
     public LottosGenerator(int autoAmount, List<Lotto> lottos){
         this.autoAmount=autoAmount;
         this.lottos=lottos;
     }
 
-    public List<Lotto> genLotto(){
+    public List<Lotto> genLottos(){
         for(int i=0;i<autoAmount;i++){
-            lottos.add(new Lotto());
+            lottos.add(genLotto());
         }
 
         return lottos;
     }
+
+    public Lotto genLotto(){
+        List<Integer> nums=new ArrayList<>();
+        for(int i=0;i<6;i++){
+            int randomNum=random.nextInt(45)+1;
+
+            while (nums.contains(randomNum)){
+                randomNum=random.nextInt(45)+1;
+            }
+            nums.add(randomNum);
+        }
+        nums.sort(Comparator.naturalOrder());
+        return new Lotto(nums);
+    }
+
+
 }
