332eb611ced1ddf1a834823f3286e4d30d2036cc
seungminbluebox <parksm1415@gmail.com>
2026-08-05 16:10:10 +0900

    fix(InputView) : Scanner 필드변수로 통합

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,15 +5,17 @@ import model.Lotto;
 import java.util.*;
 
 public class InputView {
+    private Scanner scanner = new Scanner(System.in);
+
     public int inputCost() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("구입금액을 입력해 주세요.");
-        return scanner.nextInt();
+
+        int v=scanner.nextInt();
+        scanner.nextLine(); // 남은 개행 소비
+        return v;
     }
 
     public List<Integer> inputWinningNums(){
-        Scanner scanner = new Scanner(System.in);
-
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
 
         List<Integer> nums= Arrays.stream(scanner.nextLine().split(", "))
@@ -23,25 +25,23 @@ public class InputView {
         return nums;
     }
     public int inputBonusBall(){
-        Scanner scanner = new Scanner(System.in);
-
         System.out.println("보너스 볼을 입력해 주세요.");
 
-        return scanner.nextInt();
+        int v=scanner.nextInt();
+        scanner.nextLine(); // 남은 개행 소비
+        return v;
     }
 
     public int inputPassiveAmount(){
-        Scanner scanner = new Scanner(System.in);
-
         System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
 
-        return scanner.nextInt();
+        int v=scanner.nextInt();
+        scanner.nextLine(); // 남은 개행 소비
+        return v;
     }
 
     public List<Integer> inputPassiveLotto(){
         System.out.println("수동으로 구매할 번호를 입력해 주세요.");
-
-        Scanner scanner = new Scanner(System.in);
 
         List<Integer> nums=Arrays.stream(scanner.nextLine().split(", "))
                 .map(Integer::parseInt)
